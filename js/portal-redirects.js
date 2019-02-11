e56d16abf94478e6231dddafc40fda1aaacb2d94
Jason Kaplan <jason.kaplan88@gmail.com>
2019-02-11 16:55:21 -0600

    F1 Redirects Initial Commit

---
+++ js/portal-redirects.js
@@ -0,0 +1,15 @@
+if(window.location == 'https://bookrooms.formula1.com/v6/fowt'){
+    window.location.href = "https://bookrooms.formula1.com/v6?siteid=45249&theme=standard";
+}
+
+if(window.location == 'https://bookrooms.formula1.com/v6/staff'){
+    window.location.href = "https://bookrooms.formula1.com/v6?siteid=47326&theme=standard";
+}
+
+if(window.location == 'https://bookrooms.formula1.com/v6/travel'){
+    window.location.href = "https://bookrooms.formula1.com/v6?siteid=46455&theme=standard";
+}
+
+if(window.location == 'https://bookrooms.formula1.com/v6/' || window.location == 'https://bookrooms.formula1.com/v6'){
+    window.location.href = "https://bookrooms.formula1.com/v6?siteid=46455&theme=standard";
+}

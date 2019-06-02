df04799a37769880ac0eb5ed317f5e0466800882
Jason Kaplan <jason.kaplan88@gmail.com>
2019-06-02 12:15:31 -0500

    Added Contact Us to header and formatted

--- portals/child-portals/52317/52317.js
+++ portals/child-portals/52317/52317.js
@@ -19,6 +19,7 @@ function waitForElementToLoad(elementWaitingFor) {
             if (mutation.type === 'childList') {
                 noLRGPortal.updateAttribute('src', 'https://static.hotelsforhope.com/portals/child-portals/52317/images/logo-dark.png', '.logo img');
                 noLRGPortal.updateAttribute('href', 'https://www.bahraingp.com/', '.logo');
+                noLRGPortal.createHTML('<a href="https://events.hotelsforhope.com/v6/support?siteId=52317" target="_blank">Contact Us</a>', '#language', 'beforeBegin');
                 observer.disconnect();
             }
         }

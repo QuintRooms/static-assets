daaaf68bd121c7ba8db783a8f355279925d8341f
Jason Kaplan <jason.kaplan88@gmail.com>
2019-06-27 11:44:14 -0500

    Distance calculation front end initial commit

---
+++ js/calculate-distance.js
@@ -0,0 +1,120 @@
+class Event {
+
+    constructor(params, venueName, unit, from_lat, from_long) {
+        this.params = [];
+        this.venueName = venueName;
+        this.unit = unit;
+        this.from_lat = from_lat;
+        this.from_long = from_long;
+
+        this.init();
+    }
+
+    init() {
+        this.getFromLatLong();
+        this.getVenueName();
+        this.getUnit();
+        this.getPropertyLatLong().then(() => {
+            this.updateDistance();
+        });
+    }
+
+    getFromLatLong() {
+        this.from_lat = document.querySelector('meta[name="aLatitude"]');
+        this.from_long = document.querySelector('meta[name="aLongitude"]');
+
+        if (this.from_lat) {
+            this.from_lat = this.from_lat.getAttribute('content');
+        }
+
+        if (this.from_long) {
+            this.from_long = this.from_long.getAttribute('content');
+        }
+    }
+
+    async getPropertyLatLong(params) {
+        let self = this;
+        let property = document.querySelectorAll('.ArnProperty');
+
+        property.forEach(function(element, index) {
+            let to_lat = element.getAttribute('latitude');
+            to_lat = parseFloat(to_lat);
+            to_lat = to_lat.toFixed(4);
+            element.setAttribute('latitude', to_lat);
+
+            let to_long = element.getAttribute('longitude');
+            to_long = parseFloat(to_long);
+            to_long = to_long.toFixed(4);
+            element.setAttribute('longitude', to_long);
+
+            if (to_lat && to_long) {
+                self.params.push([
+                    [self.from_lat, self.from_long],
+                    [to_lat, to_long]
+                ]);
+            }
+        });
+    }
+
+    getVenueName() {
+        this.venueName = document.querySelector('meta[name="SearchLocation"]').getAttribute('content');
+    }
+
+    getUnit() {
+        let initialUnits = document.querySelector('.units').textContent;
+
+        if (initialUnits.includes('miles')) {
+            this.unit = 'miles';
+        }
+
+        if (initialUnits.includes('kilometers')) {
+            this.unit = 'kilometers';
+        }
+    }
+
+    async updateDistance() {
+        let self = this;
+        let distanceElement = document.querySelectorAll('.distanceLabel');
+
+        let interval = 200;
+        let promise = Promise.resolve();
+
+        self.params.forEach(function(param) {
+            promise = promise.then(function() {
+                let url = 'https://distance.hotelsforhope.com?from_lat=' + param[0][0] + '&from_long=' + param[0][1] + '&to_lat=' + param[1][0] + '&to_long=' + param[1][1];
+
+                let response = fetch(url).then((response) => {
+                    let data = response.json();
+                    console.log(data);
+                    return data;
+                }).then((data) => {
+                    distanceElement.forEach(function(element) {
+                        let parent = element.closest('.ArnProperty');
+
+                        if (data['to_lat'] == parent.getAttribute('latitude') && data['to_long'] == parent.getAttribute('longitude')) {
+                            if (self.unit == 'miles') {
+                                element.textContent = data['mi'] + ' ' + self.unit + ' to ' + self.venueName;
+                            }
+
+                            if (self.unit == 'kilometers') {
+                                element.textContent = data['km'] + ' ' + self.unit + ' to ' + self.venueName;
+                            }
+                        }
+                    });
+                }).catch(() => {
+                    console.log('There was an error trying to make your request');
+                });
+
+                return new Promise(function(resolve) {
+                    setTimeout(resolve, interval);
+                });
+            });
+        });
+
+        promise.then(function() {
+            console.log('Request loop finished.');
+        });
+    }
+}
+
+let event = new Event();

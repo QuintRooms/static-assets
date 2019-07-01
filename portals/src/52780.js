4b4e5fa25d15e574c3744aa86279a27639056153
Jason Kaplan <jason.kaplan88@gmail.com>
2019-07-01 14:12:13 -0500

    C3 Travel Initial Commit

---
+++ portals/src/52780.js
@@ -0,0 +1,5 @@
+import '../styles/styles.css';
+import '../child-portals/cug/52780/52780.css';
+import 'whatwg-fetch';
+import '@babel/polyfill';
+import CUGPortal from '../child-portals/cug/52780/52780.js';

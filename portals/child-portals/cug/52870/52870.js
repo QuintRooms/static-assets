9d9c01290ff81682c15535e0b50ab117088a971a
Jason Kaplan <jason.kaplan88@gmail.com>
2019-07-12 10:36:17 -0500

    Remove dist js files test

--- portals/child-portals/cug/52870/52870.js
+++ portals/child-portals/cug/52870/52870.js
@@ -263,6 +263,3 @@ window.onload = function() {
     cugPortal.updateText('.RootBody .modifySearch', 'C3 Travel, whether you\'re travelling for personal stay or business, we\'ve got you covered');
     cugPortal.updateAttribute('#travelPlusTab', '_blank', 'target');
 }
-
-
-console.log('dev autodeploy test tres');

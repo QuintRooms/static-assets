75fb1801971b736ef3c59c4409a93afb5ce81ece
Jason Kaplan <jason.kaplan88@gmail.com>
2019-07-16 16:42:50 -0500

    Dutch GP

--- portals/child-portals/noLRG/52407/52407.js
+++ portals/child-portals/noLRG/52407/52407.js
@@ -22,7 +22,7 @@ function waitForElementToLoad(elementWaitingFor) {
             if (mutation.type === 'childList') {
                 noLRGPortal.updateAttribute('.logo img', 'https://static.hotelsforhope.com/portals/child-portals/noLRG/' + noLRGPortal.site_id + '/images/logo.png', 'src');
                 noLRGPortal.updateAttribute('.logo', 'https://www.circuitzandvoort.nl/en/', 'href');
-                noLRGPortal.createHTML('#language', '<a class="hidden-xs" href="https://events.hotelsforhope.com/v6/support?siteId=' + noLRGPortal.site_id + '" target="_blank">Group Request</a><a class="hidden-xs" href="https://form.jotform.com/91966384058167" target="_blank">Contact Us</a>', 'beforeBegin');
+                noLRGPortal.createHTML('#language', '<a class="hidden-xs" href="https://form.jotform.com/91966384058167" target="_blank">Group Request</a> <a class="hidden-xs" href="https://events.hotelsforhope.com/v6/support?siteId=' + noLRGPortal.site_id + '" target="_blank">Contact Us</a>', 'beforeBegin');
 
                 if(window.matchMedia('(max-width: 565px)').matches){
                     noLRGPortal.updateText('#language .translateMe', 'Language');

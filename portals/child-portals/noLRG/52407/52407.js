37b7dbb5fa3976ae5b04b43594bc675c58870e21
Jason Kaplan <jason.kaplan88@gmail.com>
2019-07-16 16:38:06 -0500

    Dutch GP Updates

--- portals/child-portals/noLRG/52407/52407.js
+++ portals/child-portals/noLRG/52407/52407.js
@@ -4,12 +4,12 @@ let noLRGPortal = new NoLRGPortal();
 
 noLRGPortal.updateText('title', 'Dutch Grand Prix Rooms');
 noLRGPortal.updateRoomDescription('.RoomDescription', 'Dutch Grand Prix Exclusive Rate');
-noLRGPortal.updateText('#theMarketingOptInAjax', 'I want to receive the latest information by email from F1® including relevant news, surveys, offers, and promotions.');
+noLRGPortal.updateText('#theMarketingOptInAjax label', 'Opt in to receive communication from the event and it\'s partners');
 
 jQuery(document).on('ratesReadyEvent', function() {
     setTimeout(function() {
         noLRGPortal.ratesReadyEventMethods();
-        noLRGPortal.updateText('.event-rate', 'Exclusive Rates');
+        noLRGPortal.updateText('.event-rate', 'Official Hotel');
     }, 1);
 });
 
@@ -22,7 +22,7 @@ function waitForElementToLoad(elementWaitingFor) {
             if (mutation.type === 'childList') {
                 noLRGPortal.updateAttribute('.logo img', 'https://static.hotelsforhope.com/portals/child-portals/noLRG/' + noLRGPortal.site_id + '/images/logo.png', 'src');
                 noLRGPortal.updateAttribute('.logo', 'https://www.circuitzandvoort.nl/en/', 'href');
-                noLRGPortal.createHTML('#language', '<a class="hidden-xs" href="https://events.hotelsforhope.com/v6/support?siteId=' + noLRGPortal.site_id + '" target="_blank">Contact Us</a>', 'beforeBegin');
+                noLRGPortal.createHTML('#language', '<a class="hidden-xs" href="https://events.hotelsforhope.com/v6/support?siteId=' + noLRGPortal.site_id + '" target="_blank">Group Request</a><a class="hidden-xs" href="https://form.jotform.com/91966384058167" target="_blank">Contact Us</a>', 'beforeBegin');
 
                 if(window.matchMedia('(max-width: 565px)').matches){
                     noLRGPortal.updateText('#language .translateMe', 'Language');

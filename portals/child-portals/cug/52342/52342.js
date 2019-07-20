46629171fcfcaac970fd198cd723151178f65400
Jason Kaplan <jason.kaplan88@gmail.com>
2019-07-20 10:38:50 -0500

    Room Steals URL Fix

--- portals/child-portals/cug/52342/52342.js
+++ portals/child-portals/cug/52342/52342.js
@@ -3,7 +3,7 @@ import CUGPortal from '../../../js/portal-setup.js';
 let cugPortal = new CUGPortal();
 
 cugPortal.updateText('title', 'Room Steals');
-noLRGPortal.updateAttribute('#favicon', 'https://static.hotelsforhope.com/portals/child-portals/noLRG/' + noLRGPortal.site_id + '/images/favicon.png', 'href');
+cugPortal.updateAttribute('#favicon', 'https://static.hotelsforhope.com/portals/child-portals/cug/' + cugPortal.site_id + '/images/favicon.png', 'href');
 
 
 jQuery(document).on('ratesReadyEvent', function() {

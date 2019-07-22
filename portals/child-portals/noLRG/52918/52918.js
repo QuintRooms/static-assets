73a68041c8e88a787719e46a43bedab8ce8b74b1
Jason Kaplan <jason.kaplan88@gmail.com>
2019-07-22 14:41:13 -0500

    Dutch GP Custom Property Tags

--- portals/child-portals/noLRG/52918/52918.js
+++ portals/child-portals/noLRG/52918/52918.js
@@ -28,6 +28,29 @@ jQuery(document).on('ratesReadyEvent', function() {
         noLRGPortal.updateText('.event-rate', 'Official Hotel');
         noLRGPortal.translateText('.perNight', 'per night');
         noLRGPortal.translateText('.openMapBtn', 'open map');
+
+        noLRGPortal.addCustomTag('Train Access', 24832);
+        noLRGPortal.addCustomTag('Train Access', 276769);
+        noLRGPortal.addCustomTag('Train Access', 199659);
+        noLRGPortal.addCustomTag('Train Access', 31992);
+        noLRGPortal.addCustomTag('Train Access', 3415520);
+        noLRGPortal.addCustomTag('Train Access', 360867);
+        noLRGPortal.addCustomTag('Train Access', 651049);
+        noLRGPortal.addCustomTag('Train Access', 1701460);
+        noLRGPortal.addCustomTag('Train Access', 801928);
+        noLRGPortal.addCustomTag('Train Access', 284789);
+        noLRGPortal.addCustomTag('Train Access', 48520);
+        noLRGPortal.addCustomTag('Train Access', 224220);
+        noLRGPortal.addCustomTag('Train Access', 234002);
+        noLRGPortal.addCustomTag('Train Access', 1985366);
+        noLRGPortal.addCustomTag('Train Access', 3341285);
+        noLRGPortal.addCustomTag('Train Access', 228776);
+        noLRGPortal.addCustomTag('Train Access', 199667);
+        noLRGPortal.addCustomTag('Train Access', 32238);
+        noLRGPortal.addCustomTag('Train Access', 224621);
+        noLRGPortal.addCustomTag('Train Access', 234784);
+        noLRGPortal.addCustomTag('Train Access', 172363);
+        noLRGPortal.addCustomTag('Train Access', 32240);
     }, 1);
 });
 

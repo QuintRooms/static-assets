028c0d6ed47c931b12f01bc2154ebd4fb98194f2
Jack Wolfe <jackwolfe@utexas.edu>
2019-07-29 14:52:37 -0500

    Life is Beautiful - Artists & Vendors

---
+++ portals/src/51662.js
@@ -0,0 +1,5 @@
+import '../styles/styles.css';
+import '../child-portals/noLRG/51662/51662.css';
+import 'whatwg-fetch';
+import '@babel/polyfill';
+import NoLRGPortal from '../child-portals/noLRG/51662/51662.js';

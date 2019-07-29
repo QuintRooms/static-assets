7c31dedb3491c61a800c4428b8f34d296e8fb781
Jack Wolfe <jackwolfe@utexas.edu>
2019-07-29 15:47:22 -0500

    Life is Beautiful

--- portals/child-portals/noLRG/51662/51662.js
+++ portals/child-portals/noLRG/51662/51662.js
@@ -5,8 +5,8 @@ let noLRGPortal = new NoLRGPortal();
 noLRGPortal.updateText('title', 'Music City Food + Wine Rooms');
 noLRGPortal.updateRoomDescription('.RoomDescription', 'Music City Food + Wine Exclusive Rate');
 
-jQuery(document).on('ratesReadyEvent', function() {
-    setTimeout(function() {
+jQuery(document).on('ratesReadyEvent', function () {
+    setTimeout(function () {
         noLRGPortal.ratesReadyEventMethods();
         noLRGPortal.updateText('.event-rate', 'Exclusive Rates');
     }, 1);
@@ -14,12 +14,16 @@ jQuery(document).on('ratesReadyEvent', function() {
 
 function waitForElementToLoad(elementWaitingFor) {
     let element = document.querySelector(elementWaitingFor);
-    let config = { attributes: false, childList: true, subtree: false };
+    let config = {
+        attributes: false,
+        childList: true,
+        subtree: false
+    };
 
     function callback(mutationsList, observer) {
         for (let mutation of mutationsList) {
             if (mutation.type === 'childList') {
-                noLRGPortal.updateAttribute('.logo img', 'https://static.hotelsforhope.com/portals/child-portals/noLRG/' + noLRGPortal.site_id + '/images/logo.png', 'src');
+                noLRGPortal.updateAttribute('.logo img', 'https://dev-static.hotelsforhope.com/portals/child-portals/noLRG/' + '51662' + '/images/logo.png', 'src');
                 noLRGPortal.updateAttribute('.logo', 'https://www.musiccityfoodandwinefestival.com/', 'href');
                 observer.disconnect();
             }
@@ -29,4 +33,4 @@ function waitForElementToLoad(elementWaitingFor) {
     observer.observe(element, config);
 };
 
-waitForElementToLoad('header'); 
+waitForElementToLoad('header');

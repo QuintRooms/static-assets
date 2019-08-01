039df20d849a28a576ee8bb4619723177a4f8fb7
Jason Kaplan <jason.kaplan88@gmail.com>
2019-08-01 11:58:09 -0500

    Lolla

--- portals/child-portals/noLRG/52975/52975.js
+++ portals/child-portals/noLRG/52975/52975.js
@@ -5,8 +5,8 @@ let noLRGPortal = new NoLRGPortal();
 noLRGPortal.updateText('title', 'Lollapalooza Rooms');
 noLRGPortal.updateRoomDescription('.RoomDescription', 'Lollapalooza Exclusive Rate');
 
-jQuery(document).on('ratesReadyEvent', function () {
-    setTimeout(function () {
+jQuery(document).on('ratesReadyEvent', function() {
+    setTimeout(function() {
         noLRGPortal.ratesReadyEventMethods();
         noLRGPortal.updateText('.event-rate', 'Exclusive Rates');
     }, 1);
@@ -25,6 +25,7 @@ function waitForElementToLoad(elementWaitingFor) {
             if (mutation.type === 'childList') {
                 noLRGPortal.updateAttribute('.logo img', 'https://dev-static.hotelsforhope.com/portals/child-portals/noLRG/' + noLRGPortal.site_id + '/images/logo.png', 'src');
                 noLRGPortal.updateAttribute('.logo', 'https://www.lollapalooza.com/', 'href');
+                createHTML('.logo', '<span class="event-dates">August 1-4, 2019 - Grant Park, Chicago</span>', 'afterEnd');
                 observer.disconnect();
             }
         }

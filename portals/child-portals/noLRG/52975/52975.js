bdbb39b035f65ea28df68c4d1f851bdb23791c32
Jason Kaplan <jason.kaplan88@gmail.com>
2019-08-01 12:40:10 -0500

    Lolla and Dist Updates

---
+++ portals/child-portals/noLRG/52975/52975.js
@@ -0,0 +1,39 @@
+import NoLRGPortal from '../../../js/portal-setup.js';
+
+let noLRGPortal = new NoLRGPortal();
+
+noLRGPortal.updateText('title', 'Lollapalooza Rooms');
+noLRGPortal.updateRoomDescription('.RoomDescription', 'Lollapalooza Exclusive Rate');
+noLRGPortal.updateText('#theMarketingOptInAjax label', 'Opt in to receive communication from the event and its partners.');
+
+
+jQuery(document).on('ratesReadyEvent', function() {
+    setTimeout(function() {
+        noLRGPortal.ratesReadyEventMethods();
+        noLRGPortal.updateText('.event-rate', 'Exclusive Rates');
+    }, 1);
+});
+
+function waitForElementToLoad(elementWaitingFor) {
+    let element = document.querySelector(elementWaitingFor);
+    let config = {
+        attributes: false,
+        childList: true,
+        subtree: false
+    };
+
+    function callback(mutationsList, observer) {
+        for (let mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                noLRGPortal.updateAttribute('.logo img', 'https://dev-static.hotelsforhope.com/portals/child-portals/noLRG/' + noLRGPortal.site_id + '/images/logo.png', 'src');
+                noLRGPortal.updateAttribute('.logo', 'https://www.lollapalooza.com/', 'href');
+                noLRGPortal.createHTML('.logo', '<span class="event-dates">Jul 30 - Aug 2, 2020 - Grant Park, Chicago</span>', 'afterEnd');
+                observer.disconnect();
+            }
+        }
+    }
+    let observer = new MutationObserver(callback);
+    observer.observe(element, config);
+};
+
+waitForElementToLoad('header');

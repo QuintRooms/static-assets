943564f9a6bc1ab5b7be470a933981d41a625332
Jason Kaplan <jason.kaplan88@gmail.com>
2019-08-01 11:13:08 -0500

    Lolla 2020 Initial Commit

---
+++ portals/src/52975.js
@@ -0,0 +1,5 @@
+import '../styles/styles.css';
+import '../child-portals/noLRG/52975/52975.css';
+import 'whatwg-fetch';
+import '@babel/polyfill';
+import NoLRGPortal from '../child-portals/noLRG/52975/52975.js';

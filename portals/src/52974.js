643eca4977c298cda3bb2a71490ca32ac6b10a36
Jack Wolfe <jackwolfe@utexas.edu>
2019-08-05 13:25:37 -0500

    Rockweave

---
+++ portals/src/52974.js
@@ -0,0 +1,5 @@
+import '../styles/styles.css';
+import '../child-portals/noLRG/52974/52974.css';
+import 'whatwg-fetch';
+import '@babel/polyfill';
+import NoLRGPortal from '../child-portals/noLRG/52974/52974.js';

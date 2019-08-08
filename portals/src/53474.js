8b5bb707775e892d2b1d634cfc36c2f6b8d250c7
Jack Wolfe <jackwolfe@utexas.edu>
2019-08-08 13:00:20 -0500

    Tortuga

---
+++ portals/src/53474.js
@@ -0,0 +1,5 @@
+import '../styles/styles.css';
+import '../child-portals/noLRG/53474/53474.css';
+import 'whatwg-fetch';
+import '@babel/polyfill';
+import NoLRGPortal from '../child-portals/noLRG/53474/53474.js';

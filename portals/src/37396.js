9d70d8a883d153034e467ee11de99c5580c757a3
Jack Wolfe <jackwolfe@utexas.edu>
2019-08-12 10:55:17 -0500

    Barrett-Jackson

---
+++ portals/src/37396.js
@@ -0,0 +1,5 @@
+import '../styles/styles.css';
+import '../child-portals/noLRG/37396/37396.css';
+import 'whatwg-fetch';
+import '@babel/polyfill';
+import NoLRGPortal from '../child-portals/noLRG/37396/37396.js';

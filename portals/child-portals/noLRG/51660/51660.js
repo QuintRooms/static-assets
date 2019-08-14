eaea800103c5f98f15e18aba189093dbff0209a6
Jack Wolfe <jackwolfe@utexas.edu>
2019-08-14 14:46:21 -0500

    Miami Beach

---
+++ portals/child-portals/noLRG/51660/51660.js
@@ -0,0 +1,38 @@
+import NoLRGPortal from '../../../js/portal-setup.js';
+
+let noLRGPortal = new NoLRGPortal();
+
+noLRGPortal.updateRoomDescription('.RoomDescription', "Miami Beach Pop Festival Exclusive Rate");
+
+jQuery(document).on('ratesReadyEvent', function () {
+    setTimeout(function () {
+        noLRGPortal.ratesReadyEventMethods();
+        noLRGPortal.updateText('.event-rate', 'Exclusive Rates');
+        noLRGPortal.addCustomTag('Host Hotel', 41188);
+    }, 1);
+});
+
+function waitForElementToLoad(elementWaitingFor) {
+    let element = document.querySelector(elementWaitingFor);
+    let config = {
+        attributes: false,
+        childList: true,
+        subtree: false
+    };
+
+    function callback(mutationsList, observer) {
+        for (let mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                noLRGPortal.updateAttribute('.logo img', 'https://static.hotelsforhope.com/portals/child-portals/noLRG/51660/images/logo.png', 'src');
+                noLRGPortal.updateAttribute('.logo', 'https://miamibeachpop.com/', 'href');
+                observer.disconnect();
+            }
+        }
+    }
+    let observer = new MutationObserver(callback);
+    observer.observe(element, config);
+};
+
+waitForElementToLoad('header');
+
+noLRGPortal.updateText('#theMarketingOptInAjax label', 'Opt in to receive communication from the event and its partners.');

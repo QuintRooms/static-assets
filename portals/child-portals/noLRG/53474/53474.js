deccee7fe1f2534f9c4c9b2218e24334a4443f45
Jason Kaplan <jason.kaplan88@gmail.com>
2019-08-14 12:38:48 -0500

    Tortuga Tweak

--- portals/child-portals/noLRG/53474/53474.js
+++ portals/child-portals/noLRG/53474/53474.js
@@ -38,4 +38,4 @@ waitForElementToLoad('header');
 noLRGPortal.updateText('#theMarketingOptInAjax label', 'Opt in to receive communication from the event and its partners.');
 
 // Custom Banner Ad
-noLRGPortal.createHTML('.SearchHotels .ArnLeftSearchContainer', '<a class="ad-unit" target="_blank" href="http://rocktheocean.com/"><img src="https://static.hotelsforhope.com/portals/child-portals/noLRG/'+ noLRGPortal.site_id +'/images/ad.png"></a>', 'beforeEnd');
+noLRGPortal.createHTML('.SearchHotels .ArnLeftSearchContainer', '<a class="ad" target="_blank" href="http://rocktheocean.com/"><img src="https://static.hotelsforhope.com/portals/child-portals/noLRG/'+ noLRGPortal.site_id +'/images/ad.png"></a>', 'beforeEnd');

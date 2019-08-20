93a5656c7ce0e27e8090d906c9e089a66acc7942
Jason Kaplan <jason.kaplan88@gmail.com>
2019-08-20 15:56:31 -0500

    Austin Marathon Updates

--- portals/child-portals/noLRG/53763/53763.js
+++ portals/child-portals/noLRG/53763/53763.js
@@ -2,8 +2,8 @@ import NoLRGPortal from '../../../js/portal-setup.js';
 
 let noLRGPortal = new NoLRGPortal();
 
-noLRGPortal.updateText('title', 'Dell Technologies Match Play Rooms');
-noLRGPortal.updateRoomDescription('.RoomDescription', 'Dell Technologies Match Play Exclusive Rates');
+noLRGPortal.updateText('title', 'Austin Marathon Rooms');
+noLRGPortal.updateRoomDescription('.RoomDescription', 'Austin Marathon Exclusive Rates');
 
 jQuery(document).on('ratesReadyEvent', function() {
     setTimeout(function() {
@@ -20,7 +20,7 @@ function waitForElementToLoad(elementWaitingFor) {
         for (let mutation of mutationsList) {
             if (mutation.type === 'childList') {
                 noLRGPortal.updateAttribute('.logo img', 'https://dev-static.hotelsforhope.com/portals/child-portals/noLRG/' + noLRGPortal.site_id + '/images/logo.png', 'src');
-                noLRGPortal.updateAttribute('.logo', 'https://www.pgatour.com/tournaments/wgc-dell-technologies-match-play.html', 'href');
+                noLRGPortal.updateAttribute('.logo', 'https://youraustinmarathon.com/', 'href');
                 observer.disconnect();
             }
         }

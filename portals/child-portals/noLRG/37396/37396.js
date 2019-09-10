0dcad28264e8963eeb3b325a4b3183809ff42a1a
Jason Kaplan <jason.kaplan88@gmail.com>
2019-09-10 15:08:34 -0500

    Barrett Jackson Hilton Tags

--- portals/child-portals/noLRG/37396/37396.js
+++ portals/child-portals/noLRG/37396/37396.js
@@ -21,6 +21,7 @@ jQuery(document).on('ratesReadyEvent', function() {
 
         if (noLRGPortal.site_id == 49551) {
             noLRGPortal.addCustomTag('<div style="border-bottom: 1px solid rgba(255, 255, 255, .4);">Host Hotel</div><div>Complimentary Shuttle</div>', 20);
+            addHiltonTags();
         }
 
     }, 1);
@@ -37,9 +38,9 @@ function waitForElementToLoad(elementWaitingFor) {
     function callback(mutationsList, observer) {
         for (let mutation of mutationsList) {
             if (mutation.type === 'childList') {
-                noLRGPortal.updateAttribute('.logo img', 'https://dev-static.hotelsforhope.com/portals/child-portals/noLRG/37396/images/logo.png', 'src');
+                noLRGPortal.updateAttribute('.logo img', 'https://static.hotelsforhope.com/portals/child-portals/noLRG/37396/images/logo.png', 'src');
                 noLRGPortal.updateAttribute('.logo', 'https://www.barrett-jackson.com/', 'href');
-                document.querySelector('body').insertAdjacentHTML('beforeEnd', '<a href="https://www.grandcanyon.org/" class="side-banner" target="_blank"><img src="https://dev-static.hotelsforhope.com/portals/child-portals/noLRG/37396/images/leftbanner.jpg"></a>');
+                document.querySelector('body').insertAdjacentHTML('beforeEnd', '<a href="https://www.grandcanyon.org/" class="side-banner" target="_blank"><img src="https://static.hotelsforhope.com/portals/child-portals/noLRG/37396/images/leftbanner.jpg"></a>');
                 observer.disconnect();
 
             }
@@ -52,3 +53,22 @@ function waitForElementToLoad(elementWaitingFor) {
 waitForElementToLoad('header');
 
 noLRGPortal.updateText('#theMarketingOptInAjax label', 'Opt in to receive communication from the event and its partners.');
+
+function addHiltonTags() {
+    let hiltonPropIds = [236618, 16, 31, 21, 12937, 267891, 257288, 1220075, 1268837, 175722, 65, 188905, 16460, 16343, 12715, 71, 16797, 16417, 277232, 16454, 16470, 165275, 182101, 659056, 3349002, 50725, 282039, 16523, 1269346, 1367176, 1239970, 272052, 188634, 3469078, 378245, 3309155, 277416, 2302525, 3167140, 16837, 859570, 215220, 2048227];
+
+    hiltonPropIds.forEach(function(prop) {
+        if (document.querySelector('#theArnProperty' + prop)) {
+            document.querySelector('#theArnProperty' + prop + ' .ArnContainer').style.background = 'url("https://static.hotelsforhope.com/portals/child-portals/noLRG/37396/images/hilton-logo.svg") no-repeat 100% 25px / 65%';
+
+            if (window.matchMedia("(max-width: 500px)").matches) {
+                document.querySelector('#theArnProperty' + prop + ' .ArnContainer').style.background = 'url("https://static.hotelsforhope.com/portals/child-portals/noLRG/37396/images/hilton-logo.svg") 0 85% / 100% no-repeat';
+            }
+
+            noLRGPortal.addCustomTag('<div style="border-bottom: 1px solid rgba(255, 255, 255, .4);">Hilton Hotel</div>', prop);
+            if (document.querySelector('#theArnProperty' + prop + '> .sash')) {
+                document.querySelector('#theArnProperty' + prop + '> .sash .event-rate').textContent = 'Hilton Deals';
+            }
+        }
+    });
+}

8db48531acda55cff360684cd179be00a56441ed
Jason Kaplan <jason.kaplan88@gmail.com>
2019-09-18 14:34:35 -0500

    Innings Fest Header Update

--- portals/child-portals/noLRG/51442/51442.js
+++ portals/child-portals/noLRG/51442/51442.js
@@ -21,8 +21,8 @@ function waitForElementToLoad(elementWaitingFor) {
             if (mutation.type === 'childList') {
                 noLRGPortal.updateAttribute('.logo img', 'https://static.hotelsforhope.com/portals/child-portals/noLRG/' + noLRGPortal.site_id + '/images/logo.png', 'src');
                 noLRGPortal.updateAttribute('.logo', 'https://www.inningsfestival.com/', 'href');
-                noLRGPortal.createHTML('header', '<span class="header-info"><span>Feb. 29th-Mar. 1st</span><span>2020</span></span>', 'afterBegin');
-                noLRGPortal.createHTML('header', '<span class="header-info"><span>Tempe Beach Park</span><span>&amp; Arts Park</span></span>', 'beforeEnd');
+                noLRGPortal.createHTML('header', '<span class="header-info"><img src="https://static.hotelsforhope.com/portals/child-portals/noLRG/51442/images/dash.png"><span>Feb. 29th-Mar. 1st</span><span>2020</span><img src="https://static.hotelsforhope.com/portals/child-portals/noLRG/51442/images/dash.png"></span>', 'afterBegin');
+                noLRGPortal.createHTML('header', '<span class="header-info"><img src="https://static.hotelsforhope.com/portals/child-portals/noLRG/51442/images/dash.png"><span>Tempe Beach Park</span><span>&amp; Arts Park</span><img src="https://static.hotelsforhope.com/portals/child-portals/noLRG/51442/images/dash.png"></span>', 'beforeEnd');
                 observer.disconnect();
             }
         }

2dea15069ec2c91fd041b56504532ce14528b00e
Jason Kaplan <jason.kaplan88@gmail.com>
2019-10-03 12:36:47 -0500

    Vietnam GP

--- portals/child-portals/noLRG/53739/53739.js
+++ portals/child-portals/noLRG/53739/53739.js
@@ -2,8 +2,8 @@ import NoLRGPortal from '../../../js/portal-setup.js';
 
 let noLRGPortal = new NoLRGPortal();
 
-noLRGPortal.updateText('title', 'Hanoi Street Circuit Rooms');
-noLRGPortal.updateRoomDescription('.RoomDescription', 'Hanoi Street Circuit Exclusive Rate');
+noLRGPortal.updateText('title', 'Formula 1 Vietnam Grand Prix Rooms');
+noLRGPortal.updateRoomDescription('.RoomDescription', 'Formula 1 Vietnam Grand Prix Exclusive Rate');
 
 jQuery(document).on('ratesReadyEvent', function() {
     setTimeout(function() {
@@ -19,9 +19,10 @@ function waitForElementToLoad(elementWaitingFor) {
     function callback(mutationsList, observer) {
         for (let mutation of mutationsList) {
             if (mutation.type === 'childList') {
-                noLRGPortal.updateAttribute('.logo img', 'https://static.hotelsforhope.com/portals/child-portals/noLRG/' + noLRGPortal.site_id + '/images/logo.png', 'src');
-                noLRGPortal.updateAttribute('.logo', 'https://events.hotelsforhope.com/v6/?siteid=' + noLRGPortal.site_id, 'href');
-                noLRGPortal.createHTML('#language', '<a href="https://events.hotelsforhope.com/v6/support?siteId=' + noLRGPortal.site_id + '" target="_blank">Contact Us</a>', 'beforeBegin');
+                noLRGPortal.updateAttribute('.logo img', 'https://static.hotelsforhope.com/portals/child-portals/noLRG/53739/images/logo-white.png', 'src');
+                noLRGPortal.updateAttribute('.logo', 'https://events.hotelsforhope.com/v6/?siteid=53739', 'href');
+                noLRGPortal.createHTML('#language', '<a href="https://form.jotform.com/92754977819176" target="_blank">Group Request</a><a href="https://events.hotelsforhope.com/v6/support?siteId=53739" target="_blank">Contact Us</a>', 'beforeBegin');
+                document.querySelector('select#language').insertAdjacentHTML('beforeEnd', '<option value="vietnamese">Tiếng Việt (Vietnamese)</option>');
                 observer.disconnect();
             }
         }

576080b817be8f6854c70b439e3b484b824409b0
Jason Kaplan <jason.kaplan88@gmail.com>
2019-10-29 10:25:01 -0500

    Quint Events Landing Page Initial Commit

---
+++ portals/src/56837.js
@@ -0,0 +1,5 @@
+import '../styles/styles.css';
+import '../child-portals/noLRG/56837/56837.css';
+import 'whatwg-fetch';
+import '@babel/polyfill';
+import NoLRGPortal from '../child-portals/noLRG/56837/56837.js';

b801589f575ccfb141feff8d859421784e7ecc92
Jason Kaplan <jason.kaplan88@gmail.com>
2019-10-31 10:06:17 -0500

    Deposit Policy to All Sites

--- portals/src/53739.js
+++ portals/src/53739.js
@@ -3,3 +3,4 @@ import '../child-portals/noLRG/53739/53739.css';
 import 'whatwg-fetch';
 import '@babel/polyfill';
 import NoLRGPortal from '../child-portals/noLRG/53739/53739.js';
+import '../../js/calculate-distance.js';

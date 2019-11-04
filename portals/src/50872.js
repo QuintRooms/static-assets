25a4e39760dd91c2ed72c470d2318a00c0525810
Jason Kaplan <jason.kaplan88@gmail.com>
2019-11-04 13:54:14 -0600

    Shaky Boots Initial Commit

---
+++ portals/src/50872.js
@@ -0,0 +1,5 @@
+import '../styles/styles.css';
+import '../child-portals/noLRG/50872/50872.css';
+import 'whatwg-fetch';
+import '@babel/polyfill';
+import NoLRGPortal from '../child-portals/noLRG/50872/50872.js';

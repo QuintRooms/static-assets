10cc7f686b014bfb44be8901a6467a3ebc039763
Jason Kaplan <jason.kaplan88@gmail.com>
2020-01-21 16:57:05 -0600

    test

--- ares/js/build.js
+++ ares/js/build.js
@@ -30,6 +30,7 @@ export default class BasePortal {
                 this.appendToParent('#theMarketingOptInAjax', '#theConfirmCheckboxesAjax');
 
                 this.updateRoomDescription();
+                this.createImageSlider();
             });
         });
     }

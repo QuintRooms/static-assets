629706efe9e9aff125649080b484b34235907841
Jason Kaplan <jason.kaplan88@gmail.com>
2020-01-21 16:08:13 -0600

    Config Test

--- ares/js/site_configs/39624/39624.js
+++ ares/js/site_configs/39624/39624.js
@@ -1,15 +1,18 @@
 import BasePortal from '../../build.js';
 
-class ChildPortal extends BasePortal{
-    constructor(){
+class ChildPortal extends BasePortal {
+    constructor() {
         super();
         console.log('CUGPortal.constructor() fired.');
         this.init();
     }
 
-    init(){
-        super.getSiteConfigJSON();
+    init() {
         console.log('CUGPortal.init() fired.');
+        super.getSiteID()
+            .then(() => {
+                super.getSiteConfigJSON();
+            });
     }
 }
 

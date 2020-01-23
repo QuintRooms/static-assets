e65d5cd56b747db64f72376c9515c7b856164b9d
Jason Kaplan <jason.kaplan88@gmail.com>
2020-01-23 16:22:21 -0600

    test

--- ares/js/build.js
+++ ares/js/build.js
@@ -414,7 +414,7 @@ export default class BasePortal {
         star_elements.forEach(function(star) {
             let number_of_stars = star.textContent;
             let num = number_of_stars.replace(/\D/g, "");
-            let star_svg = '<svg height="25" width="23" class="star rating" data-rating="1"><polygon points="9.9, 1.1, 3.3, 21.78, 19.8, 8.58, 0, 8.58, 16.5, 21.78" style="fill: #faaf18"/></svg>';
+            let star_svg = '<svg height="21" width="20" class="star rating" data-rating="1"><polygon points="9.9, 1.1, 3.3, 21.78, 19.8, 8.58, 0, 8.58, 16.5, 21.78" style="fill: #faaf18"/></svg>';
 
             if (num == 1) {
                 star.innerHTML = star_svg;

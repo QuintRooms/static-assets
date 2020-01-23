eca278880fbd8810e65ff155137be35cffab0d60
Jason Kaplan <jason.kaplan88@gmail.com>
2020-01-23 16:39:54 -0600

    test

--- ares/js/build.js
+++ ares/js/build.js
@@ -26,7 +26,7 @@ export default class BasePortal {
                 this.updateText('.ArnLeftListContainer > span.translateMe', 'Search');
 
                 // single prop detail methods
-                if (this.page_name == '.property-detail') {
+                if (this.page_name == 'property-detail') {
                     this.updateText('.SinglePropDetail .OptionsPricing a', 'Rooms');
                     this.updateText('.SinglePropDetail .Details a', 'General Info');
                     this.updateText('.SinglePropDetail .Map a', 'Map');

ee9b7608e8ab37506e564b942cf8509fec5b82ff
Jason Kaplan <jason.kaplan88@gmail.com>
2020-01-27 14:22:31 -0600

    test

--- ares/js/build.js
+++ ares/js/build.js
@@ -298,6 +298,7 @@ export default class BasePortal {
      *@param string selector - DOM selector
      */
     updateRoomDescription() {
+        console.log('updateRoomDescription() fired.', this.site_config_json)
         let room_description_el = document.querySelectorAll('.RoomDescription');
         if (!document.querySelector('.SinglePropDetail') || !room_description_el || ! this.site_config_json['site_type'] == "lodging") {
             console.log('updateRoomDescription() should return here.')

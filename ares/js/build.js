38d8f86d08588dcaca950b2e5e2e02e9c19edde4
Jason Kaplan <jason.kaplan88@gmail.com>
2020-02-14 14:45:01 -0600

    test

--- ares/js/build.js
+++ ares/js/build.js
@@ -824,12 +824,15 @@
              this.updateHTML(`.RoomNumber-${reservation_count} > legend`, 'Billing Info');
              this.updateHTML(`.RoomNumber-${reservation_count} .paymentMethods`, '<span class="creditcards"><img src="https://dev-static.hotelsforhope.com/ares/images/creditcards.png" alt="Credit Cards"></span>');
              this.createHTML('<legend>Credit Card Info</legend>', `.RoomNumber-${reservation_count} .guestBillingAddress`, 'beforeBegin');
-             
+
              if (reservation_count >= 2) {
-                console.log(reservation_count)
-                 document.querySelector(`.RoomNumber-${reservation_count} #theCopyInfoAjax`).addEventListener('click', () => {
-                     document.querySelector(`.RoomNumber-${reservation_count} > legend`).textContent = 'Billing info';
-                 });
+                 console.log(reservation_count)
+                 setTimeout(() => {
+
+                     document.querySelector(`.RoomNumber-${reservation_count} #theCopyInfoAjax`).addEventListener('click', () => {
+                         document.querySelector(`.RoomNumber-${reservation_count} > legend`).textContent = 'Billing info';
+                     });
+                 }, 2000)
              }
          });
      }

d2f8e060550b09b5afcb52217f333f1fd4fdcbec
Jason Kaplan <jason.kaplan88@gmail.com>
2020-02-14 11:03:49 -0600

    test

--- ares/js/build.js
+++ ares/js/build.js
@@ -809,6 +809,7 @@ export default class BasePortal {
             ], `billing-details-container${reservation_count}`, `#theBookingPage td.GuestForms > fieldset:nth-child(${reservation_count}) #theBillingAddressAjax${reservation_count - 1}`, 'afterEnd');
 
             this.moveOrphanedElementsIntoNewWrapper([document.querySelector(`#theBookingPage td.GuestForms > fieldset:nth-child(${reservation_count}) #theCardVerificationAjax`), document.querySelector(`#theBookingPage td.GuestForms > fieldset:nth-child(${reservation_count}) #theCardExpirationFieldsAjax`)], `security-code-exp-container${reservation_count}`, `#theCreditCardBillingNameAjax${reservation_count - 1}`, 'afterEnd');
+            console.log(reservation_count)
             document.querySelector(`#billing-details-container${reservation_count}`).classList.add('billing-details-container');
             console.log('805:', document.querySelector(`#billing-details-container${reservation_count}`).classList.add('billing-details-container'))
             document.querySelector(`#security-code-exp-container${reservation_count}`).classList.add('security-code-exp-container');

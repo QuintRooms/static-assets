def18b849b6f12941162559278157100beb76233
Jason Kaplan <jason.kaplan88@gmail.com>
2020-02-18 11:09:39 -0600

    Breeders' Cup

---
+++ portals/child-portals/noLRG/59788/59788.js
@@ -0,0 +1,31 @@
+import NoLRGPortal from '../../../js/portal-setup.js';
+
+let noLRGPortal = new NoLRGPortal();
+
+jQuery(document).on('ratesReadyEvent', function() {
+    setTimeout(function() {
+        noLRGPortal.ratesReadyEventMethods();
+    }, 1);
+});
+
+function waitForElementToLoad(elementWaitingFor) {
+    let element = document.querySelector(elementWaitingFor);
+    let config = { attributes: false, childList: true, subtree: false };
+
+    function callback(mutationsList, observer) {
+        for (let mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                noLRGPortal.updateAttribute('.logo img', 'https://static.hotelsforhope.com/portals/child-portals/noLRG/59788/images/logo.png', 'src');
+                noLRGPortal.updateAttribute('.logo', 'https://www.okroger.ai/', 'href');
+
+                observer.disconnect();
+            }
+        }
+    }
+    let observer = new MutationObserver(callback);
+    observer.observe(element, config);
+};
+
+waitForElementToLoad('header');
+
+document.querySelector('body').insertAdjacentHTML('beforeEnd', '<link rel="stylesheet" type="text/css" href="https://static.hotelsforhope.com/portals/child-portals/noLRG/59788/59788.css">');

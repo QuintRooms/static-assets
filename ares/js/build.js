82b612f04664d2b838e0b1c960dbc45fb6de9b8e
Jason Kaplan <jason.kaplan88@gmail.com>
2020-02-20 12:28:25 -0600

    test

--- ares/js/build.js
+++ ares/js/build.js
@@ -1084,7 +1084,7 @@
          window.addEventListener('click', (e) => {
              console.log('window clicked')
              console.log(e)
-             if (e.target != document.querySelector('#currency-label') || e.target != document.querySelector('.show-currencies-container') && document.querySelector('.show-currencies-container')) {
+             if (e.target != document.querySelector('#currency-label') && e.target != document.querySelector('.show-currencies-container') && document.querySelector('.show-currencies-container')) {
                  console.log('window clicked inside conditional')
                  currencies_container.classList.toggle('show-currencies-container');
              }

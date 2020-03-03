a35527cc056dd66394168494cb88710a2df24be5
Matt <Sartain>
2020-03-03 17:33:26 -0600

    created stay 22 CUG

---
+++ portals/child-portals/cug/60100/60100.js
@@ -0,0 +1,31 @@
+import CUGPortal from '../../../js/portal-setup.js';
+
+let cugPortal = new CUGPortal();
+
+jQuery(document).on('ratesReadyEvent', function() {
+    setTimeout(function() {
+        cugPortal.ratesReadyEventMethods();   
+    }, 1);
+});
+
+function waitForElementToLoad(elementWaitingFor) {
+    let element = document.querySelector(elementWaitingFor);
+    let config = { attributes: false, childList: true, subtree: false };
+
+    function callback(mutationsList, observer) {
+        for (let mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                cugPortal.updateAttribute('.logo img', 'https://dev-static.hotelsforhope.com/portals/child-portals/cug/60100/images/logo.svg', 'src');
+                cugPortal.updateAttribute('.logo', 'https://www.stay22.com/', 'href');
+
+                observer.disconnect();
+            }
+        }
+    }
+    let observer = new MutationObserver(callback);
+    observer.observe(element, config);
+};
+
+waitForElementToLoad('header');
+
+document.querySelector('body').insertAdjacentHTML('beforeEnd', '<link rel="stylesheet" type="text/css" href="https://dev-static.hotelsforhope.com/portals/child-portals/cug/60100/60100.css">');
---
+++ portals/src/60100.js
@@ -0,0 +1,3 @@
+import 'whatwg-fetch';
+import '@babel/polyfill';
+import NoLRGPortal from '../child-portals/cug/60100/60100.js';

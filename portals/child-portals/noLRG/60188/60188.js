617d7cb7553579d0470f9bb50a12062a83c145fd
Jason Kaplan <jason.kaplan88@gmail.com>
2020-03-12 16:00:15 -0500

    H4H Housing Template - Coronavirus function refactor

--- portals/child-portals/noLRG/60188/60188.js
+++ portals/child-portals/noLRG/60188/60188.js
@@ -36,38 +36,28 @@ document.querySelector('body').insertAdjacentHTML('beforeEnd', '<link rel="style
 noLRGPortal.updateAttribute('.RootBody', 'background-image: url(https://dev-static.hotelsforhope.com/portals/child-portals/noLRG/60188/images/banner.png)', 'style');
 
 
-function showCoronavirusInfoBanner(){
+function showCoronavirusInfoBanner() {
 
-    if(localStorage.getItem('covidAlertBanner') === 'closed')return;
+    if (localStorage.getItem('covidAlertBanner') === 'closed') return;
 
-    let body = document.body;
-    let banner = document.createElement('div');
-    let content = document.createElement('div');
-    let infoText = document.createElement('h1');
-    let link = document.createElement('a');
-    let closeBanner = document.createElement('button');
-    let linkImage = document.createElement('svg');
+    document.body.insertAdjacentHTML('afterBegin', `
+        <div class="info-banner">
+            <div class="message-content">
+                <h1>Travel Information:</h1>
+                <a class="details-link" href="https://www.hotelsforhope.com/covid19/" target="_blank">
+                    <h1>COVID-19</h1> 
+                    <svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="clone" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-clone fa-w-16 fa-3x" width="18px">
+                        <path fill="currentColor" d="M464 0H144c-26.51 0-48 21.49-48 48v48H48c-26.51 0-48 21.49-48 48v320c0 26.51 21.49 48 48 48h320c26.51 0 48-21.49 48-48v-48h48c26.51 0 48-21.49 48-48V48c0-26.51-21.49-48-48-48zM362 464H54a6 6 0 0 1-6-6V150a6 6 0 0 1 6-6h42v224c0 26.51 21.49 48 48 48h224v42a6 6 0 0 1-6 6zm96-96H150a6 6 0 0 1-6-6V54a6 6 0 0 1 6-6h308a6 6 0 0 1 6 6v308a6 6 0 0 1-6 6z" class="">
+                        </path>
+                    </svg>
+                </a>
+            </div>
+            <button class="close-banner-button close-alert">X</button>
+        </div>
+    `);
 
-    linkImage = '<svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="clone" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-clone fa-w-16 fa-3x" width="18px"><path fill="currentColor" d="M464 0H144c-26.51 0-48 21.49-48 48v48H48c-26.51 0-48 21.49-48 48v320c0 26.51 21.49 48 48 48h320c26.51 0 48-21.49 48-48v-48h48c26.51 0 48-21.49 48-48V48c0-26.51-21.49-48-48-48zM362 464H54a6 6 0 0 1-6-6V150a6 6 0 0 1 6-6h42v224c0 26.51 21.49 48 48 48h224v42a6 6 0 0 1-6 6zm96-96H150a6 6 0 0 1-6-6V54a6 6 0 0 1 6-6h308a6 6 0 0 1 6 6v308a6 6 0 0 1-6 6z" class=""></path></svg>';
-
-    banner.classList.add('info-banner');
-    content.classList.add('message-content');
-    link.classList.add('details-link');
-    closeBanner.classList.add('close-alert');
-    infoText.textContent =  'Travel Information:';
-    link.innerHTML = `<h1>COVID-19</h1> ${linkImage}`;
-    link.href = 'https://www.hotelsforhope.com/covid19/';
-    link.setAttribute('target', '_blank');
-    closeBanner.textContent = 'X';
-    
-    banner.appendChild(content);
-    content.appendChild(infoText);
-    content.appendChild(link);
-    banner.appendChild(closeBanner);
-    body.prepend(banner)
-
-    closeBanner.addEventListener('click', function(evt){
-        banner.style.display = 'none';
+    document.querySelector('.close-banner-button').addEventListener('click', function(evt) {
+        document.querySelector('.info-banner').style.display = 'none';
         window.localStorage.setItem('covidAlertBanner', 'closed')
     });
 }

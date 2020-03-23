b9512542e2cf9ef424faf9057a7e3c9f12f6f7c1
Matt Sartain <matthewsartain@me.com>
2020-03-23 17:13:25 -0500

    Image SLider - changes NodeList to have correct images

--- components/image-slideshow/slideshow.js
+++ components/image-slideshow/slideshow.js
@@ -54,21 +54,18 @@ let props = [718107, 507388, 2332133, 2028020, 1258650, 313962, 282860, 224842, 
             }
         };
 
-
         /* can't find the appropriate place to call fire this code off - I'm currently putting it in the console. 
 
         Before I can refactor I need to have it removing the right images to be able to see the effect it's having on the counter */
 
         carouselImages = document.querySelectorAll('.carousel-slide img');
         for(let j = 0; j < carouselImages.length; j++){
-            console.log(j)
             if(carouselImages[j].naturalWidth < 400){
-                console.log(carouselImages[j])
                 console.log(carouselImages[j].naturalWidth)
                 carouselImages[j].parentNode.removeChild(carouselImages[j]);
             }
-            carouselImages = document.querySelectorAll('.carousel-slide img');
         }   
+        carouselImages = document.querySelectorAll('.carousel-slide img');
     
 
     async function createCarousel() {

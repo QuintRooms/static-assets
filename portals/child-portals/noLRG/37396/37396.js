5387e4f563b0ec9b64e5bbf737ca4417bd9676b7
Matt Sartain <matthewsartain@me.com>
2020-04-14 12:57:17 -0500

    Barrett Jackson landing page updates

--- portals/child-portals/noLRG/37396/37396.js
+++ portals/child-portals/noLRG/37396/37396.js
@@ -1,84 +1,207 @@
-import NoLRGPortal from '../../../js/portal-setup.js';
+import NoLRGPortal from '../../../js/portal-setup';
 
-let noLRGPortal = new NoLRGPortal();
+const no_lrg_portal = new NoLRGPortal();
 
-noLRGPortal.updateRoomDescription('.RoomDescription', 'Barrett-Jackson Rooms');
+no_lrg_portal.updateRoomDescription(
+    '.RoomDescription',
+    'Barrett-Jackson Rooms'
+);
 
-jQuery(document).on('ratesReadyEvent', function() {
-    setTimeout(function() {
-        noLRGPortal.ratesReadyEventMethods();
-        noLRGPortal.updateText('.event-rate', 'Exclusive Rates');
-        noLRGPortal.addCustomTag('Host Hotel', 41188);
+jQuery(document).on('ratesReadyEvent', () => {
+    setTimeout(() => {
+        no_lrg_portal.ratesReadyEventMethods();
+        no_lrg_portal.updateText('.event-rate', 'Exclusive Rates');
+        no_lrg_portal.addCustomTag('Host Hotel', 41188);
 
-        if (noLRGPortal.site_id == 37396) {
-            noLRGPortal.addCustomTag('Free Tram to Mandalay Bay', 564);
-            noLRGPortal.addCustomTag('Free Tram to Mandalay Bay', 22619);
+        if (no_lrg_portal.site_id === '37396') {
+            no_lrg_portal.addCustomTag('Free Tram to Mandalay Bay', 564);
+            no_lrg_portal.addCustomTag('Free Tram to Mandalay Bay', 22619);
         }
 
-        if (noLRGPortal.site_id == 37395) {
-            noLRGPortal.addCustomTag('Shuttle to Mohegan Sun', 191384);
+        if (no_lrg_portal.site_id === '37395') {
+            no_lrg_portal.addCustomTag('Shuttle to Mohegan Sun', 191384);
         }
 
-        if (noLRGPortal.site_id == 49551) {
-            noLRGPortal.addCustomTag('<div style="border-bottom: 1px solid rgba(255, 255, 255, .4);">Host Hotel</div><div>Complimentary Shuttle</div>', 20);
-            noLRGPortal.addCustomTag('<div style="border-bottom: 1px solid rgba(255, 255, 255, .4);">Hilton Hotel</div><div>Complimentary Shuttle</div>', 16460);
-            noLRGPortal.addCustomTag('<div>2 Bedroom Suites w/ 2 Kings</div>', 257);
+        if (no_lrg_portal.site_id === '49551') {
+            no_lrg_portal.addCustomTag(
+                '<div style="border-bottom: 1px solid rgba(255, 255, 255, .4);">Host Hotel</div><div>Complimentary Shuttle</div>',
+                20
+            );
+            no_lrg_portal.addCustomTag(
+                '<div style="border-bottom: 1px solid rgba(255, 255, 255, .4);">Hilton Hotel</div><div>Complimentary Shuttle</div>',
+                16460
+            );
+            no_lrg_portal.addCustomTag(
+                '<div>2 Bedroom Suites w/ 2 Kings</div>',
+                257
+            );
 
             addHiltonTags();
-            noLRGPortal.updateText('.event-rate', 'Exclusive Barrett-Jackson Rates');
+            no_lrg_portal.updateText(
+                '.event-rate',
+                'Exclusive Barrett-Jackson Rates'
+            );
         }
 
-        if (noLRGPortal.site_id == 49552) {
-            noLRGPortal.addCustomTag('<div>Marriott Oceanfront Open Feb 2020</div>', 3486114);
-            noLRGPortal.updateText('.event-rate', 'Exclusive Barrett-Jackson Rates');
-
+        if (no_lrg_portal.site_id === '49552') {
+            no_lrg_portal.addCustomTag(
+                '<div>Marriott Oceanfront Open Feb 2020</div>',
+                3486114
+            );
+            no_lrg_portal.updateText(
+                '.event-rate',
+                'Exclusive Barrett-Jackson Rates'
+            );
         }
 
-        if (noLRGPortal.site_id == 60292){
-            noLRGPortal.addCustomTag('<div>New Hotel Open 2020</div>', 3486114)
+        if (no_lrg_portal.site_id === '60292') {
+            no_lrg_portal.addCustomTag(
+                '<div>New Hotel Open 2020</div>',
+                3486114
+            );
         }
-
     }, 1);
 });
 
 function waitForElementToLoad(elementWaitingFor) {
-    let element = document.querySelector(elementWaitingFor);
-    let config = {
+    const element = document.querySelector(elementWaitingFor);
+    const config = {
         attributes: false,
         childList: true,
-        subtree: false
+        subtree: false,
     };
 
     function callback(mutationsList, observer) {
-        for (let mutation of mutationsList) {
+        for (const mutation of mutationsList) {
             if (mutation.type === 'childList') {
-                noLRGPortal.updateAttribute('.logo img', 'https://static.hotelsforhope.com/portals/child-portals/noLRG/37396/images/logo.png', 'src');
-                noLRGPortal.updateAttribute('.logo', 'https://www.barrett-jackson.com/', 'href');
-                document.querySelector('body').insertAdjacentHTML('beforeEnd', '<a href="https://www.grandcanyon.org/" class="side-banner" target="_blank"><img src="https://static.hotelsforhope.com/portals/child-portals/noLRG/37396/images/leftbanner.jpg"></a>');
+                no_lrg_portal.updateAttribute(
+                    '.logo img',
+                    'https://static.hotelsforhope.com/portals/child-portals/noLRG/37396/images/logo.png',
+                    'src'
+                );
+                no_lrg_portal.updateAttribute(
+                    '.logo',
+                    'https://www.barrett-jackson.com/',
+                    'href'
+                );
+                document
+                    .querySelector('body')
+                    .insertAdjacentHTML(
+                        'beforeEnd',
+                        '<a href="https://www.grandcanyon.org/" class="side-banner" target="_blank"><img src="https://static.hotelsforhope.com/portals/child-portals/noLRG/37396/images/leftbanner.jpg"></a>'
+                    );
                 observer.disconnect();
             }
         }
     }
-    let observer = new MutationObserver(callback);
+    const observer = new MutationObserver(callback);
     observer.observe(element, config);
-};
+}
 
 waitForElementToLoad('header');
 
-noLRGPortal.updateText('#theMarketingOptInAjax label', 'Opt in to receive communication from the event and its partners.');
+no_lrg_portal.updateText(
+    '#theMarketingOptInAjax label',
+    'Opt in to receive communication from the event and its partners.'
+);
 
 function addHiltonTags() {
-    let hiltonPropIds = [236618, 16, 31, 21, 12937, 267891, 257288, 1220075, 1268837, 175722, 65, 188905, 16460, 16343, 12715, 71, 16797, 16417, 277232, 16454, 16470, 165275, 182101, 659056, 3349002, 50725, 282039, 16523, 1269346, 1367176, 1239970, 272052, 188634, 3469078, 378245, 3309155, 277416, 2302525, 3167140, 16837, 859570, 215220, 2048227, 13770, 13499, 67, 20050, 20252, 415537, 6, 619759, 24, 446580, 25, 3349688, 2048225, 1243044, 1758007, 851719, 20132, 180778, 244634, 20130, 277730, 277731, 187648, 20133, 20164, 262395, 19706, 3011273, 16452, 220004, 2048226, 3468713];
+    const hilton_prop_ids = [
+        236618,
+        16,
+        31,
+        21,
+        12937,
+        267891,
+        257288,
+        1220075,
+        1268837,
+        175722,
+        65,
+        188905,
+        16460,
+        16343,
+        12715,
+        71,
+        16797,
+        16417,
+        277232,
+        16454,
+        16470,
+        165275,
+        182101,
+        659056,
+        3349002,
+        50725,
+        282039,
+        16523,
+        1269346,
+        1367176,
+        1239970,
+        272052,
+        188634,
+        3469078,
+        378245,
+        3309155,
+        277416,
+        2302525,
+        3167140,
+        16837,
+        859570,
+        215220,
+        2048227,
+        13770,
+        13499,
+        67,
+        20050,
+        20252,
+        415537,
+        6,
+        619759,
+        24,
+        446580,
+        25,
+        3349688,
+        2048225,
+        1243044,
+        1758007,
+        851719,
+        20132,
+        180778,
+        244634,
+        20130,
+        277730,
+        277731,
+        187648,
+        20133,
+        20164,
+        262395,
+        19706,
+        3011273,
+        16452,
+        220004,
+        2048226,
+        3468713,
+    ];
 
-    hiltonPropIds.forEach(function(prop) {
-        if (document.querySelector('#theArnProperty' + prop)) {
-            document.querySelector('#theArnProperty' + prop + ' .ArnContainer').style.background = 'url("https://static.hotelsforhope.com/portals/child-portals/noLRG/37396/images/hilton-logo.svg") no-repeat 100% 25px / 65%';
+    hilton_prop_ids.forEach((prop) => {
+        if (document.querySelector(`#theArnProperty${prop}`)) {
+            document.querySelector(
+                `#theArnProperty${prop} .ArnContainer`
+            ).style.background =
+                'url("https://static.hotelsforhope.com/portals/child-portals/noLRG/37396/images/hilton-logo.svg") no-repeat 100% 25px / 65%';
 
-            if (window.matchMedia("(max-width: 500px)").matches) {
-                document.querySelector('#theArnProperty' + prop + ' .ArnContainer').style.background = 'url("https://static.hotelsforhope.com/portals/child-portals/noLRG/37396/images/hilton-logo.svg") 0 85% / 100% no-repeat';
+            if (window.matchMedia('(max-width: 500px)').matches) {
+                document.querySelector(
+                    `#theArnProperty${prop} .ArnContainer`
+                ).style.background =
+                    'url("https://static.hotelsforhope.com/portals/child-portals/noLRG/37396/images/hilton-logo.svg") 0 85% / 100% no-repeat';
             }
 
-            noLRGPortal.addCustomTag('<div style="border-bottom: 1px solid rgba(255, 255, 255, .4);">Hilton Hotel</div>', prop);
+            no_lrg_portal.addCustomTag(
+                '<div style="border-bottom: 1px solid rgba(255, 255, 255, .4);">Hilton Hotel</div>',
+                prop
+            );
             // if (document.querySelector('#theArnProperty' + prop + '> .sash')) {
             //     document.querySelector('#theArnProperty' + prop + '> .sash .event-rate').textContent = 'Hilton Deals';
             // }
@@ -86,15 +209,41 @@ function addHiltonTags() {
     });
 }
 
-document.querySelector('body').insertAdjacentHTML('beforeEnd', '<link rel="stylesheet" type="text/css" href="https://static.hotelsforhope.com/portals/child-portals/noLRG/37396/37396.css">');
+document
+    .querySelector('body')
+    .insertAdjacentHTML(
+        'beforeEnd',
+        '<link rel="stylesheet" type="text/css" href="https://static.hotelsforhope.com/portals/child-portals/noLRG/37396/37396.css">'
+    );
 
-function getPropIds() {
-    let props = document.querySelectorAll('.propId');
-    let propIds = [];
-    props.forEach(function(prop) {
-        propIds.push(prop.textContent);
-    });
-    console.log(propIds);
-}
+// function getPropIds() {
+//     const props = document.querySelectorAll('.propId');
+//     const prop_ids = [];
+//     props.forEach(function (prop) {
+//         prop_ids.push(prop.textContent);
+//     });
+//     console.log(prop_ids);
+// }
 
 // getPropIds();
+
+function updateLandingPage() {
+    if (!no_lrg_portal.site_id === '60292') return;
+    let markup;
+    try {
+        fetch(
+            'static-assets/portals/child-portals/noLRG/37396/html-BJ/60292.html'
+        ).then((response) => {
+            markup = response.text();
+            console.log(markup);
+            return markup;
+        });
+    } catch (error) {
+        return error;
+    }
+
+    document
+        .querySelector('div.landing-page-container')
+        .insertAdjacentHTML('afterbegin', markup);
+}
+updateLandingPage();

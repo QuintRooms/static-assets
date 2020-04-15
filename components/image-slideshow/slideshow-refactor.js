b8ae3d2a63e64d3fb252838ddf7eadef9ffe84b0
Matt Sartain <matthewsartain@me.com>
2020-04-15 10:53:35 -0500

    Image Slideshow - adds block comments and removes commented code

--- components/image-slideshow/slideshow-refactor.js
+++ components/image-slideshow/slideshow-refactor.js
@@ -1,53 +1,57 @@
-// let props = [718107, 507388, 2332133, 2028020, 1258650, 313962, 282860, 224842, 445045, 371294, 388067, 341911, 409543, 405529, 300314, 342899, 312202, 1466441, 515326, 18845, 361301, 41357, 615332, 20837, 191708, 1231557, 359000, 191722, 712504, 496155, 29433, 275750];
-
-// function replaceImageSlideshow(){
-// document.querySelector('.ArnPhotoContainer').style.display = 'none';
-
-const propId = document.querySelector('meta[name="aPropertyId"]').content;
+const prop_id = document.querySelector('meta[name="aPropertyId"]').content;
 let counter = 0;
-let propImages;
-let carouselImages;
-// let propId = '16664';
+let prop_images;
+let carousel_images;
 
+/**
+ *@descriptionmakes the initial fetch to the api to grab all the images for the property then changes the image url to '_804480' for improved quality
+ */
 async function getPropImages() {
     try {
-        const data = await fetch(
-            `https://api.travsrv.com/api/content/findpropertyinfo?&username=h4h&password=hDzYz9HHwcJDDthPK&propertyid=${propId}`,
-            {
-                method: 'GET',
-            }
-        ).then((response) => response.json());
+        const data = await fetch(`https://api.travsrv.com/api/content/findpropertyinfo?&username=h4h&password=hDzYz9HHwcJDDthPK&propertyid=${prop_id}`, {
+            method: 'GET',
+        }).then((response) => response.json());
         return data.Images.map((e) => e.ImagePath.replace(/_300/, '_804480'));
     } catch (error) {
         return error;
     }
 }
 
+/**
+ *@description Every time a new image is put into the images array, it checks that its size is over 400 and removes it from the array if it isn't.
+ @param Number image number in array
+*/
 function removeSmallImages(i) {
-    const currentImage = document.querySelector(`#image-${i}`);
-    currentImage.onload = () => {
-        if (currentImage.naturalWidth < 400) {
-            propImages.splice(i, 1);
-            currentImage.parentNode.remove();
+    const current_image = document.querySelector(`#image-${i}`);
+    current_image.onload = () => {
+        if (current_image.naturalWidth < 400) {
+            prop_images.splice(i, 1);
+            current_image.parentNode.remove();
         }
     };
 }
 
-function setImageLoopIterator() {
+/**
+ *@description sets the iterator for the populateImages function, this will allow the next 5 images to load when you are within 2 images of the current end of the array.
+ */
+function set_image_loop_iterator() {
     const value = counter === 0 ? counter : counter + 2;
     return value;
 }
 
+/**
+ *@description whenever the current image is within two of the end of the current array lenght, populateImages will load the next 5 images and insert them into the DOM.
+ */
 function populateImages() {
-    carouselImages = document.querySelectorAll('.carousel-slide img');
-    for (let i = setImageLoopIterator; i < counter + 5; i += 1) {
-        if (i === propImages.length) return;
+    carousel_images = document.querySelectorAll('.carousel-slide img');
+    for (let i = set_image_loop_iterator; i < counter + 5; i += 1) {
+        if (i === prop_images.length) return;
 
         document.querySelector('.carousel-slide').insertAdjacentHTML(
             'beforeend',
             `
                 <div class="image-wrapper">
-                    <img id="image-${i}" src=${propImages[i]}>
+                    <img id="image-${i}" src=${prop_images[i]}>
                 </div>
             `
         );
@@ -55,50 +59,52 @@ function populateImages() {
     }
 }
 
+/**
+ *@description creates the slideshow container, next and prev buttons and calls other functions to get the images and insert them.
+ */
 async function createPropImageSlideshow() {
-    propImages = await getPropImages();
+    prop_images = await getPropImages();
     document.body.insertAdjacentHTML(
         'afterend',
         `
             <div class="carousel-container">
                 <div class="carousel-slide">
                 </div>
-                <a id="previousBtn">&#10094;</a>
-                <a id="nextBtn">&#10095;</a>
+                <a id="previous_btn">&#10094;</a>
+                <a id="next_btn">&#10095;</a>
             </div>
         `
     );
     populateImages();
 }
 
+/**
+ *@description sets up the event listeners for the next and prev buttons as well as the action for the buttons to move each image by the size of that image either to the left or right.
+ */
 async function createCarousel() {
     await createPropImageSlideshow();
 
-    const carouselSlide = document.querySelector('.carousel-slide');
-    const previousBtn = document.querySelector('#previousBtn');
-    const nextBtn = document.querySelector('#nextBtn');
+    const carousel_slide = document.querySelector('.carousel-slide');
+    const previous_btn = document.querySelector('#previous_btn');
+    const next_btn = document.querySelector('#next_btn');
 
-    nextBtn.addEventListener('click', () => {
-        carouselImages = document.querySelectorAll('.carousel-slide img');
-        if (counter === carouselImages.length - 1) return;
+    next_btn.addEventListener('click', () => {
+        carousel_images = document.querySelectorAll('.carousel-slide img');
+        if (counter === carousel_images.length - 1) return;
         counter += 1;
-        const size = carouselImages[counter].clientWidth;
-        carouselSlide.style.transform = `translateX(${-size * counter}px)`;
-        if (counter === carouselImages.length - 1) {
+        const size = carousel_images[counter].clientWidth;
+        carousel_slide.style.transform = `translateX(${-size * counter}px)`;
+        if (counter === carousel_images.length - 1) {
             populateImages();
         }
     });
 
-    previousBtn.addEventListener('click', () => {
+    previous_btn.addEventListener('click', () => {
         if (counter <= 0) return;
         counter -= 1;
-        const size = carouselImages[counter].clientWidth;
-        carouselSlide.style.transform = `translateX(${-size * counter}px)`;
+        const size = carousel_images[counter].clientWidth;
+        carousel_slide.style.transform = `translateX(${-size * counter}px)`;
     });
 }
 
 createCarousel();
-
-// function eventListener(el, event, cb){
-//     el.addEventListener(event, cb)
-// }

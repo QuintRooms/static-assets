42515f87c494748599926e9853dec7df4d002d74
Matt Sartain <matthewsartain@me.com>
2020-04-21 13:33:43 -0500

    Eslint rule - adds 'guard-for-in'
    set it to off so we can use for in loops without the eslint required if statement inside

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,7 @@ module.exports = {
             },
         ],
         'camelcase': 'off',
+        'guard-for-in': 'off',
         'class-methods-use-this': 'off', // remove this and fix 'this' errors
         'consistent-return': 'off',
         'no-use-before-define': 'warn',

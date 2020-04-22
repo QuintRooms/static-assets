8459041487146a12ccee8972f3ea583535d13e5f
Jason Kaplan <jason.kaplan88@gmail.com>
2020-04-22 16:56:22 -0500

    Voodoo Initial Commit

--- ares/webpack.config.js
+++ ares/webpack.config.js
@@ -1,27 +1,30 @@
-const webpack = require('webpack');
+require('webpack');
 
 module.exports = {
     entry: {
-        11111: './src/11111.js',
-        39624: './src/39624.js',
-        60265: './src/60265.js'
+        11111: './src/11111.js', // local portal
+        39624: './src/39624.js', // CUG Template
+        60265: './src/60265.js', //  Housing Template
+        60792: './src/60792.js', // Voodoo
     },
     output: {
         filename: '[name].js',
-        path: __dirname + '/dist'
+        path: `${__dirname}/dist`,
     },
     module: {
-        rules: [{
-            test: /\.(js)$/,
-            exclude: /node_modules/,
-            loader: "babel-loader"
-        }]
+        rules: [
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            },
+        ],
     },
-    devtool: 'hidden-source-map',
-    plugins: [
-        new webpack.SourceMapDevToolPlugin({
-            filename: '[name].map',
-            append: '\n//# sourceMappingURL=https://dev-static.hotelsforhope.com/ares/dist/[url]'
-        })
-    ]
+    // devtool: 'hidden-source-map',
+    // plugins: [
+    //     new webpack.SourceMapDevToolPlugin({
+    //         filename: '[name].map',
+    //         append: '\n//# sourceMappingURL=https://dev-static.hotelsforhope.com/ares/dist/[url]'
+    //     })
+    // ]
 };

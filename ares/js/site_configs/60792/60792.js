93a8cee123a5c2f93c8a53079ce29ea9bf675926
Jason Kaplan <jason.kaplan88@gmail.com>
2020-04-28 09:24:12 -0500

    Removed references to class in Voodoo LRG function

--- ares/js/site_configs/60792/60792.js
+++ ares/js/site_configs/60792/60792.js
@@ -8,3 +8,19 @@ class ChildPortal extends BasePortal {
 }
 
 new ChildPortal();
+
+// This is temporary - only to show Michelle LRG messaging
+function addLRGDetails() {
+    const properties = document.querySelectorAll('.ArnPropertyTierOne');
+    properties.forEach((property) => {
+        property.querySelector('.arnPrice').insertAdjacentHTML(
+            'afterEnd',
+            `
+<a href="https://events.hotelsforhope.com/v6/low-rate-guarantee?siteid=60792&amp;theme=standard" target="_blank" class="lowest-rate-link">Lowest Rate. <span>Guaranteed.</span></a>
+
+            `
+        );
+    });
+}
+
+addLRGDetails();

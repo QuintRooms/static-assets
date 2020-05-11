3d5b71332b1dcf9a3452f2f302e01884d7674638
Matt Sartain <matthewsartain@me.com>
2020-05-11 12:29:56 -0500

    updates conditionals for lat/lng search-results on search-results page

--- ares/js/build.js
+++ ares/js/build.js
@@ -1491,7 +1491,7 @@ export default class BasePortal {
                 };
 
                 if (lat_lng) build_url(lat_lng.lat, lat_lng.lng);
-                else if (!lat_lng) build_url(default_lat_lng.lat, default_lat_lng.lng);
+                else if (default_lat_lng) build_url(default_lat_lng.lat, default_lat_lng.lng);
                 else if (!lat_lng && !default_lat_lng && this.page_name === 'search-results') build_url(original_params_url.get('latitude'), original_params_url.get('longitude'));
 
                 window.location.href = url;

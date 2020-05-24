863e0015e718d2fedade8ec7728b8a9c48e9175f
Jason Kaplan <jason.kaplan88@gmail.com>
2020-05-24 16:13:21 -0500

    Moving Room Steals to the CUG test site

--- ares/site_configs/cug_template-39624/39624.js
+++ ares/site_configs/cug_template-39624/39624.js
@@ -1,4 +1,8 @@
 import BasePortal from '../../js/build';
+import Utilities from '../../js/utilities';
+// import RoomSteals from '../../../js/roomsteals-api';
+
+const utilities = new Utilities();
 
 class ChildPortal extends BasePortal {
     constructor() {
@@ -6,5 +10,28 @@ class ChildPortal extends BasePortal {
         super.init();
     }
 }
+
+utilities.updateHTML(
+    '#theMarketingOptInAjax label',
+    'I want to receive the latest information by email from Room Steals including event reminders, relevant news, surveys, offers, and promotions.'
+);
+
+utilities.createHTML(
+    `<footer>
+        <span>
+            <a href="https://roomsteals.com/terms" target="_blank">Terms &amp; Conditions</a>
+            <span>|</span>
+            <a href="https://hotels.roomsteals.com/v6/privacy-policy" target="_blank">Privacy Policy</a>
+            <span>|</span>
+            <a href="https://roomsteals.getrewardful.com/signup" target="_blank">Affiliate Program</a>
+        </span>
+        <span>
+            &copy; 2020. All Rights Reserved. <br>
+            RoomSteals.com
+        </span>
+    </footer>`,
+    '.ArnSupportBottom',
+    'beforeBegin'
+);
 
 new ChildPortal();

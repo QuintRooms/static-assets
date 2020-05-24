4b8666d1c53da62ac6b821c8f9f15c86e69bdc62
Jason Kaplan <jason.kaplan88@gmail.com>
2020-05-24 16:19:09 -0500

    Room Steals json update and moved to json from dev server in build.js

--- ares/arn_html/includes/config.php
+++ ares/arn_html/includes/config.php
@@ -1,6 +1,8 @@
 <?php 
-	$site_id = "49553";
+	$site_id = '52342';
+	$site_name = 'room_steals';
 	$cug = false; 
-	$dist_name = "shaky_knees_21-49553.js";
-	$directory_name = "shaky_knees_21-49553";
+
+	$dist_name = $site_name . '-' . $site_id . '.js';
+	$directory_name = $site_name . '-' . $site_id;
 ?>

1a2242456f35b31a78d1d63de9f2c3a75fafd811
Matt Sartain <matthewsartain@me.com>
2020-05-27 12:52:52 -0500

    Adds Barrett-Jackson Las Vegas

---
+++ ares/src/60089.js
@@ -0,0 +1,1 @@
+import '../site_configs/bj_las_vegas_20-60089/60089';
--- ares/webpack.config.js
+++ ares/webpack.config.js
@@ -2,17 +2,19 @@ const webpack = require('webpack');
 
 module.exports = {
     entry: {
-        'local_portal-11111': './src/11111.js', // local portal
-        'cug_template-39624': './src/39624.js', // CUG Template
-        'shaky_knees_21-49553': './src/49553.js', // Shaky Knees
-        'room_steals-52342': './src/52342.js', // Room Steals
-        'tortuga_20-53474': './src/53474.js', // Tortuga
-        'housing_template-60265': './src/60265.js', // Housing Template
-        'voodoo_21-60792': './src/60792.js', // Voodoo
-        'ares_child-60279': './src/60279.js', // Ares Child Test
-        'ares_child-60278': './src/60278.js', // Ares Child Test
-        'lolla_21-60828': './src/60828.js', // lollapalooza 2021
-        'lolla_20-52975': './src/52975.js', // lollapalooza 2020
+        // 'local_portal-11111': './src/11111.js', // local portal
+        // 'cug_template-39624': './src/39624.js', // CUG Template
+        // 'shaky_knees_21-49553': './src/49553.js', // Shaky Knees
+        // 'room_steals-52342': './src/52342.js', // Room Steals
+        // 'tortuga_20-53474': './src/53474.js', // Tortuga
+        // 'housing_template-60265': './src/60265.js', // Housing Template
+        // 'voodoo_21-60792': './src/60792.js', // Voodoo
+        // 'ares_child-60279': './src/60279.js', // Ares Child Test
+        // 'ares_child-60278': './src/60278.js', // Ares Child Test
+        // 'lolla_21-60828': './src/60828.js', // lollapalooza 2021
+        // 'lolla_20-52975': './src/52975.js', // lollapalooza 2020
+        // 'acl_20-53336': './src/53336.js', // acl 2020
+        'bj_las_vegas_20-60089': './src/60089.js', // BJ Las Vegas 2020
     },
     output: {
         filename: '[name].js',

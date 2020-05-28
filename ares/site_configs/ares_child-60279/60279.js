9ab61d1bf63c3bafc6c3d0372b93da1f3346e33f
Matt Sartain <matthewsartain@me.com>
2020-05-28 11:51:32 -0500

    Testing ACL on Ares child 60279

--- ares/site_configs/ares_child-60279/60279.js
+++ ares/site_configs/ares_child-60279/60279.js
@@ -1,4 +1,7 @@
 import BasePortal from '../../js/build';
+import Utilities from '../../js/utilities';
+
+const utilities = new Utilities();
 
 class ChildPortal extends BasePortal {
     constructor() {
@@ -8,3 +11,16 @@ class ChildPortal extends BasePortal {
 }
 
 new ChildPortal();
+
+async function addWeekendButtons() {
+    await utilities.waitForSelectorInDOM('header');
+    document.querySelector('header').insertAdjacentHTML(
+        'beforeend',
+        `<div class="event-week pull-right">
+            <a id="week-one" target="_blank" href="https://aclfestival.hotelsforhope.com/group-event?id=32970">Book Weekend One</a>
+            <a id="week-two" target="_blank" href="https://aclfestival.hotelsforhope.com/group-event?id=37465">Book Weekend Two</a>
+        </div>`
+    );
+}
+
+addWeekendButtons();

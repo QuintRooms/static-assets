d17381eba647d354281c0769d61a343f167fb9a8
Jason Kaplan <jason.kaplan88@gmail.com>
2020-06-03 10:53:57 -0500

    Tested and working on test sites

--- ares/arn_html/includes/config.php
+++ ares/arn_html/includes/config.php
@@ -1,6 +1,6 @@
 <?php 
-	$site_id = '60871';
-	$site_name = 'hof_marathon_21';
+	$site_id = '60278';
+	$site_name = 'ares_child';
 	$cug = false; 
 
 	$dist_name = $site_name . '-' . $site_id . '.js';

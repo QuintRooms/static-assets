ba3d5dd51769075e8fb389bf24706439462bd4cb
Matt Sartain <matthewsartain@me.com>
2020-06-11 16:31:49 -0500

    Adds Austin marathon production build

--- ares/js/entry-points.js
+++ ares/js/entry-points.js
@@ -24,20 +24,21 @@ module.exports = () => {
             'austin_food_and_wine_20-60290': './src/60290.js', // AFW 2020
             'akon_21-60284': './src/60284.js', // A-Kon 2021
             'innings_21-60093': './src/60093.js', // Innings Fest 2021
+            'austin_marathon_21-59243': './src/59243.js', // Austin Marathon 2021
         };
 
         // develop environment
     } else if (process.env.environment === 'develop') {
         entry_points = {
-            'ares_child-60278': './src/60278.js', // Ares Child Test
-            'cug_template-39624': './src/39624.js', // CUG Template
+            // 'ares_child-60278': './src/60278.js', // Ares Child Test
+            // 'cug_template-39624': './src/39624.js', // CUG Template
             'ares_child-60279': './src/60279.js', // Ares Child Test
         };
 
         // local environment
     } else if (process.env.environment === 'local') {
         entry_points = {
-            'room_steals-52342': './src/52342.js', // Room Steals
+            // 'room_steals-52342': './src/52342.js', // Room Steals
         };
     }
 
---
+++ ares/src/59243.js
@@ -0,0 +1,1 @@
+import '../site_configs/austin_marathon_21-59243/59243';

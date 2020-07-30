3c0ef8052d409dccf5be41e7f532d3a9206c9629
Jason <jason.kaplan@hotelsforhope.com>
2020-07-30 16:45:06 -0500

    Encodes resbeat rewards url and moves method to resbeat.js

--- ares/site_configs/resbeat_test-62011/62011.js
+++ ares/site_configs/resbeat_test-62011/62011.js
@@ -14,8 +14,6 @@ class ChildPortal extends Resbeat {
     }
 
     init() {
-        this.addLinkToRewardsPlatform();
-
         if (document.querySelector('.SearchHotels')) {
             utilities.waitForTextInDOM('.ArnSearchHeader', 'Update Search').then(() => {
                 this.boldLastWord(['.ArnPrimarySearchOuterContainer div.ArnSearchHeader']);
@@ -47,34 +45,6 @@ class ChildPortal extends Resbeat {
 
             string.innerHTML = `${text.substring(0, last_word_length)} <b>${last_word}</b>`;
         });
-    }
-
-    getMemberUsername() {
-        const member_metatag_content = utilities.getMetaTagContent('memberMetaTag');
-        let member_json = '';
-
-        if (!member_metatag_content) return;
-
-        member_json = JSON.parse(member_metatag_content);
-
-        if (!member_json.MemberUsername) return;
-
-        return member_json.MemberUsername;
-    }
-
-    async addLinkToRewardsPlatform() {
-        await utilities.waitForSelectorInDOM('header #commands');
-
-        const member_username = this.getMemberUsername();
-        const encoded_query_string = btoa(`member_username=${member_username}`);
-        const header_links = document.querySelector('header #commands');
-
-        if (!document.querySelector('.MemberAuthenticated') || !header_links) return;
-
-        header_links.insertAdjacentHTML(
-            'afterBegin',
-            `<a href="https://rb-redirect.hotelsforhope.com/users/redirect/${encoded_query_string}" target="_blank">RES<b>BEAT</b> Rewards</a>`
-        );
     }
 }
 

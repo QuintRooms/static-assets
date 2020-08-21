c4b46b393e09eff8b1283c6bb488f430e05cccf6
Matt Sartain <matthewsartain@me.com>
2020-08-21 11:14:08 -0500

    adds var for file path

--- ares/js/generate-scss-files.js
+++ ares/js/generate-scss-files.js
@@ -1,20 +1,45 @@
 const fs = require('fs');
 const path = require('path');
 
+const scss_vars = `$header_background: ;
+$logo_flex_position: ;
+$logo_max_width: ;
+$background_color: ;
+$secondary_background_color: ;
+$primary_color: ;
+$secondary_color: ;
+$primary_text_color: ;
+$secondary_text_color: ;
+$border_color: ;
+$button_hover_background_color: ;
+$button_hover_text_color: ;
+$button_hover_border_color: ;
+$google_font_name: '';
+`;
+
+function appendFile(filePath, dataToWrite, fileVar) {
+    fs.appendFile(filePath, dataToWrite, (err) => {
+        if (err) throw err;
+        console.log(`- - - - - scss file made in ${fileVar}/styles - - - - - -`);
+    });
+}
+
 const directory_path = path.join(__dirname, '../site_configs');
 fs.readdir(directory_path, function (err, files) {
     if (err) {
         return console.log(`Unable to scan directory: ${err}`);
     }
     files.forEach((file) => {
-        fs.readdir(`${process.cwd()}/site_configs/${file}`, (error) => {
+        const file_path = `${process.cwd()}/site_configs/${file}/`;
+
+        fs.readdir(file_path, (error) => {
             if (error) return;
-            if (!fs.existsSync(`${process.cwd()}/site_configs/${file}/styles`)) {
-                fs.mkdirSync(`${process.cwd()}/site_configs/${file}/styles`);
+            if (!fs.existsSync(`${file_path}/styles`)) {
+                fs.mkdirSync(`${file_path}/styles`);
                 console.log(`- - - - - Directory made in ${file} - - - - - -`);
-            } else if (!fs.existsSync(`${process.cwd()}/site_configs/${file}/styles/${file.slice(-5)}`)) {
-                fs.writeFileSync(`${process.cwd()}/site_configs/${file}/styles/${file.slice(-5)}.scss`);
-                console.log(`- - - - - scss file made in ${file}/styles - - - - - -`);
+                appendFile(`${file_path}/styles/${file.slice(-5)}.scss`, scss_vars, file);
+            } else if (!fs.existsSync(`${file_path}/styles/${file.slice(-5)}.scss`)) {
+                appendFile(`${file_path}/styles/${file.slice(-5)}.scss`, scss_vars, file);
             }
         });
     });

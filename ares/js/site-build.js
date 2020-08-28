041f3f8e08262dbc6fe07581029f6936e2c2e93a
Matt Sartain <matthewsartain@me.com>
2020-08-28 15:45:59 -0500

    creates new instance of RegExp to hold var

--- ares/js/site-build.js
+++ ares/js/site-build.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 
 let site_name;
 let site_id;
-// let resbeat_client;
+let resbeat_client;
 
 function waitForFile(filePath) {
     return new Promise((resolve) => {
@@ -21,55 +21,10 @@ function waitForFile(filePath) {
     });
 }
 
-function getSiteVars() {
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-    });
+// TODO write function to add new line to 'local' object in EntryPoins.js with new site id and name
+// function addToEntryPoints(){
 
-    console.log(' - - - - - - - - - - - - - - - - - - - - - - - - - -');
-    rl.question('\nPlease enter the site ID:  ', (id) => {
-        rl.question('\nPlease enter the site name:  ', (name) => {
-            rl.question('\nIs this site for a Resbeat client? (Y/N):  ', (response) => {
-                console.log(response);
-                console.log(typeof response);
-                // resbeat_client = response === 'y';
-                site_id = id;
-                site_name = name;
-                console.log(`\nThank you, building files for ${site_name}-${site_id} now.`);
-                console.log('\n - - - - - - - - - - - - - - - - - - - - - - - - - -');
-                rl.close();
-            });
-        });
-    });
-    rl.on('close', () => {
-        buildSiteDir();
-    });
-}
-
-function buildSiteDir() {
-    const source = `${process.cwd()}/site_configs/template_site`;
-    const destination = `${process.cwd()}/site_configs/${site_name}-${site_id}`;
-    fsx.copy(source, destination, (err) => {
-        if (err) {
-            console.log('An error occured while copying the folder.');
-            return console.error(err);
-        }
-        nameFiles();
-    });
-}
-
-function buildSrcFile() {
-    const src_content = `import '../site_configs/${site_name}-${site_id}/js/${site_id}';
-import '../site_configs/${site_name}-${site_id}/styles/${site_id}.scss';
-`;
-    const directory_path = `${process.cwd()}/src`;
-    fs.appendFile(`${directory_path}/${site_id}.js`, src_content, (err) => {
-        if (err) throw err;
-    });
-    editChildJs();
-    editScss();
-}
+// }
 
 async function editScss() {
     const directory_path = `${process.cwd()}/site_configs/${site_name}-${site_id}`;
@@ -85,7 +40,68 @@ async function editScss() {
     });
 }
 
+function editConfigForResbeat(data, path) {
+    let config_data = data;
+    const resbeat_data = [
+        {
+            old: 'plCZXR0GZ7J1',
+            new: 'pl58QCMXHS4C',
+        },
+        {
+            old: 'b9763a419845b59957b8cc5c9b13440c',
+            new: '1514caaca583b1ed25dad3b0c6addf0a',
+        },
+        {
+            old: 'is_resbeat_client: false',
+            new: 'is_resbeat_client: true',
+        },
+        {
+            old: "confirmation_email_from: 'Hotels for Hope'",
+            new: "confirmation_email_from: 'RESBEAT'",
+        },
+        {
+            old: "site_type: 'lodging'",
+            new: "site_type: 'cug'",
+        },
+        {
+            old: 'is_cug: false',
+            new: 'is_cug: true',
+        },
+        {
+            old: "percent_off_text: 'off'",
+            new: 'percent_off_text: "BEAT \'EM BY"',
+        },
+    ];
+
+    resbeat_data.forEach((el) => {
+        const old_string = `${el.old}`;
+        const regex = new RegExp(old_string, 'g');
+        config_data = config_data.replace(regex, `${el.new}`);
+        fs.writeFile(`${path}`, config_data, (er) => {
+            if (er) throw er;
+        });
+    });
+    console.log(`\n - Resbeat config variables have been added to ${site_name}'s config.`);
+}
+
+async function editConfig() {
+    let formatted_data;
+    const path = `${process.cwd()}/site_configs/${site_name}-${site_id}/js/${site_id}-config.js`;
+    await waitForFile(`${path}`);
+    fs.readFile(`${path}`, 'utf8', (err, data) => {
+        if (err) throw err;
+        formatted_data = data.replace(/site_id = 11111;/g, `site_id = ${site_id};`).replace(/template/g, `${site_name}`);
+        fs.writeFile(`${path}`, formatted_data, (error) => {
+            if (error) throw error;
+            console.log('\n - Config JS file updated with new site ID and name');
+            if (resbeat_client) editConfigForResbeat(formatted_data, path);
+        });
+    });
+}
+
 async function editChildJs() {
+    // TODO add ternary for if Resbeat client to then pull in resbeat.js
+    // TODO if Resbeat client, 'extends Resbeat'
     const directory_path = `${process.cwd()}/site_configs/${site_name}-${site_id}`;
     await waitForFile(`${directory_path}/js/${site_id}.js`);
     fs.readFile(`${directory_path}/js/${site_id}.js`, 'utf8', (err, data) => {
@@ -99,48 +115,19 @@ async function editChildJs() {
     });
 }
 
-async function editConfig() {
-    const directory_path = `${process.cwd()}/site_configs/${site_name}-${site_id}`;
-    await waitForFile(`${directory_path}/js/${site_id}-config.js`);
-    fs.readFile(`${directory_path}/js/${site_id}-config.js`, 'utf8', (err, data) => {
+function buildSrcFile() {
+    const src_content = `import '../site_configs/${site_name}-${site_id}/js/${site_id}';
+import '../site_configs/${site_name}-${site_id}/styles/${site_id}.scss';
+`;
+    const directory_path = `${process.cwd()}/src`;
+    fs.appendFile(`${directory_path}/${site_id}.js`, src_content, (err) => {
         if (err) throw err;
-        const formatted_data = data.replace(/site_id = 11111;/g, `site_id = ${site_id};`).replace(/template/g, `${site_name}`);
-        fs.writeFile(`${directory_path}/js/${site_id}-config.js`, formatted_data, (error) => {
-            if (error) throw error;
-            console.log('\n - Config JS file updated with new site ID and name');
-        });
     });
-    // if (resbeat_client) editConfigForResbeat();
+    editChildJs();
+    editScss();
 }
 
-// function editConfigForResbeat() {
-//     console.log('inside resbeat func');
-//     const resbeat_algolia_app_id = data.replace(/plCZXR0GZ7J1/g, 'pl58QCMXHS4C');
-//     const resbeat_algolia_api_key = data.replace(/b9763a419845b59957b8cc5c9b13440c/g, '1514caaca583b1ed25dad3b0c6addf0a');
-//     fs.writeFile(`${directory_path}/js/${site_id}-config.js`, resbeat_algolia_app_id, (er) => {
-//         if (er) throw er;
-//     });
-//     fs.writeFile(`${directory_path}/js/${site_id}-config.js`, resbeat_algolia_api_key, (er) => {
-//         if (er) throw er;
-//     });
-//     fs.writeFile(`${directory_path}/js/${site_id}-config.js`, data.replace(/is_resbeat_client: false/g, 'is_resbeat_client: true'), (er) => {
-//         if (er) throw er;
-//     });
-//     fs.writeFile(`${directory_path}/js/${site_id}-config.js`, data.replace(/confirmation_email_from: 'Hotels for Hope'/g, `confirmation_email_from: 'RESBEAT'`), (er) => {
-//         if (er) throw er;
-//     });
-//     fs.writeFile(`${directory_path}/js/${site_id}-config.js`, data.replace(/site_type: lodging/g, `site_type: cug`), (er) => {
-//         if (er) throw er;
-//     });
-//     fs.writeFile(`${directory_path}/js/${site_id}-config.js`, data.replace(/is_cug: false/g, `is_cug: true`), (er) => {
-//         if (er) throw er;
-//     });
-//     fs.writeFile(`${directory_path}/js/${site_id}-config.js`, data.replace(/percent_off_text: 'off'/g, `percent_off_text: "BEAT 'EM BY"`), (er) => {
-//         if (er) throw er;
-//     });
-// }
-
-function nameFiles() {
+function renameFiles() {
     const directory_path = `${process.cwd()}/site_configs/${site_name}-${site_id}`;
     fs.readdir(directory_path, (err) => {
         if (err) {
@@ -157,6 +144,44 @@ function nameFiles() {
             if (error) throw error;
         });
         buildSrcFile();
+    });
+}
+
+function buildSiteDir() {
+    const source = `${process.cwd()}/site_configs/template_site`;
+    const destination = `${process.cwd()}/site_configs/${site_name}-${site_id}`;
+    fsx.copy(source, destination, (err) => {
+        if (err) {
+            console.log('An error occured while copying the folder.');
+            return console.error(err);
+        }
+        renameFiles();
+    });
+}
+
+function getSiteVars() {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+    console.log(' - - - - - - - - - - - - - - - - - - - - - - - - - -');
+    rl.question('\nPlease enter the site ID:  ', (id) => {
+        rl.question('\nPlease enter the site name:  ', (name) => {
+            rl.question('\nIs this site for a Resbeat client? (Y/N):  ', (response) => {
+                console.log(response);
+                console.log(typeof response);
+                resbeat_client = response === 'y';
+                site_id = id;
+                site_name = name;
+                console.log(`\nThank you, building files for ${site_name}-${site_id} now.`);
+                console.log('\n - - - - - - - - - - - - - - - - - - - - - - - - - -');
+                rl.close();
+            });
+        });
+    });
+    rl.on('close', () => {
+        buildSiteDir();
     });
 }
 

eee662fcd1dd26714f8eccae40c139c2ce13fcb5
Matt Sartain <matthewsartain@me.com>
2020-09-01 16:37:53 -0500

    adds header and X to popup for weekend 2

--- ares/js/components/multiple-event-weekends.js
+++ ares/js/components/multiple-event-weekends.js
@@ -8,6 +8,15 @@ export default function popUp(config) {
             `
             <div class="modal-overlay show-modal">
                 <div id="weekend-popup" class="modal-container">
+                    <div class="modal-header">
+                        <h3>Weekend Two?</h3>
+                        <span class="close-modal">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 13 13">
+                                <polyline stroke="#333" fill="transparent" points="1 1,6.5 6.5,12 1"></polyline>
+                                <polyline stroke="#333" fill="transparent" points="1 12,6.5 6.5,12 12"></polyline>
+                            </svg>
+                        </span>
+                    </div>
                     <span>Do you want to view exclusive hotel rates for weekend two?</span>
                     <a target="_blank" href="">Check Rates</a>
                     <a id="close-popup">Don't show this message again</a>
@@ -23,7 +32,7 @@ export default function popUp(config) {
                 justify-content: center;
                 width: 30vw;
                 height: 20vh;
-                border: 2px solid #ccc;
+                border: 2px solid ${config.primary_color};
             }
 
             #close-popup {
@@ -33,10 +42,17 @@ export default function popUp(config) {
             #close-popup:hover {
                 text-decoration: underline
             }
+            .modal-header {
+                width: 100%;
+            }
             </style>
             `
         );
         document.querySelector('#close-popup').addEventListener('click', () => {
+            document.body.classList.toggle('fixed');
+            document.querySelector('.modal-overlay').style.display = 'none';
+        });
+        document.querySelector('.close-modal').addEventListener('click', () => {
             document.body.classList.toggle('fixed');
             document.querySelector('.modal-overlay').style.display = 'none';
         });

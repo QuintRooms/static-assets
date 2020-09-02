3ca918b468b6e21e8baf256a4ea893eef712e467
Matt Sartain <matthewsartain@me.com>
2020-09-02 13:33:11 -0500

    moves Bahrain into Sass branch and adds files

--- ares/site_configs/f1_bahrain_20-63341/js/63341.js
+++ ares/site_configs/f1_bahrain_20-63341/js/63341.js
@@ -1,8 +1,8 @@
-import BasePortal from '../../js/build';
+import BasePortal from '../../../js/build';
 import SiteConfig from './63341-config';
-import f1Styles from '../../js/f1';
-import Distance from '../../js/calculate-distance';
-import popUp from '../../js/components/multiple-event-weekends';
+import f1Styles from '../../../js/f1';
+import Distance from '../../../js/calculate-distance';
+import popUp from '../../../js/components/multiple-event-weekends';
 
 const site_config = new SiteConfig();
 

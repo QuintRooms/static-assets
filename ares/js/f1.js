9290f0b143ed16b32deba00022825e5553344188
Matt Sartain <matthewsartain@me.com>
2020-09-17 11:03:29 -0500

    adds f1.scss with header styles

--- ares/js/f1.js
+++ ares/js/f1.js
@@ -13,6 +13,7 @@ export default async function f1Styles(siteId) {
     async function addHeader(id) {
         const html = await utilities.fetchHTMLFromFile(`${env_path.path}/html/f1/f1-header.html`);
         const races_contact =
+            // TODO update below site id when moved to new site id
             siteId === '46451'
                 ? `
         <ul id="races-contact">

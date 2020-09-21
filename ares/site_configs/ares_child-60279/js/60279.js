3b27d5a501b8864d940b8a34e801e3970cdc693a
Matt Sartain <matthewsartain@me.com>
2020-09-21 11:49:59 -0500

    adds method to set today's date in checkin popup

--- ares/site_configs/ares_child-60279/js/60279.js
+++ ares/site_configs/ares_child-60279/js/60279.js
@@ -1,12 +1,34 @@
 import BasePortal from '../../../js/build';
 import SiteConfig from './60279-config';
 import Distance from '../../../js/calculate-distance';
+import Utilities from '../../../js/utilities';
+
+const dayjs = require('dayjs');
+
+const utilities = new Utilities();
 
 const site_config = new SiteConfig();
 class ChildPortal extends BasePortal {
     constructor() {
         super(site_config);
         super.init();
+        this.init();
+    }
+
+    init() {
+        this.prepopulateSearchDate();
+    }
+
+    async prepopulateSearchDate() {
+        if (utilities.getPageName !== 'search-results') return;
+
+        await utilities.waitForSelectorInDOM('#theQuickCheckIn');
+
+        const check_in_input = document.querySelector('#theDatePrompt #theQuickCheckIn');
+
+        if (!check_in_input) return;
+
+        check_in_input.value = dayjs().format('MM/DD/YYYY');
     }
 }
 

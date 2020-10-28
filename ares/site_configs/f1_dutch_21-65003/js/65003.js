2048762de99b34297480e643b497c73dc17ae7e7
Matt Sartain <matthewsartain@me.com>
2020-10-28 17:07:34 -0500

    F1 Dutch - build for dev with rates coming soon component

--- ares/site_configs/f1_dutch_21-65003/js/65003.js
+++ ares/site_configs/f1_dutch_21-65003/js/65003.js
@@ -2,6 +2,7 @@ import BasePortal from '../../../js/build';
 import SiteConfig from './65003-config';
 import f1Styles from '../../../js/f1';
 import Distance from '../../../js/calculate-distance';
+import exclusiveRatesComingPopup from '../../../js/components/exclusive-rates-coming/rates-coming';
 
 const site_config = new SiteConfig();
 
@@ -13,6 +14,7 @@ class ChildPortal extends BasePortal {
 }
 
 f1Styles(site_config.site_id);
+exclusiveRatesComingPopup('Dutch GP', '6th November');
 
 new ChildPortal();
 new Distance();

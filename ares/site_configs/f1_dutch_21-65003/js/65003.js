31ae80e37bf73500ab0534a077bccbc347cfc07f
Matt Sartain <matthewsartain@me.com>
2020-10-29 12:04:59 -0500

    F1 Dutch - updates popup

--- ares/site_configs/f1_dutch_21-65003/js/65003.js
+++ ares/site_configs/f1_dutch_21-65003/js/65003.js
@@ -2,7 +2,7 @@ import BasePortal from '../../../js/build';
 import SiteConfig from './65003-config';
 import f1Styles from '../../../js/f1';
 import Distance from '../../../js/calculate-distance';
-import exclusiveRatesComingPopup from '../../../js/components/exclusive-rates-coming/rates-coming';
+import popup from '../../../js/components/on-load-popup/popup';
 
 const site_config = new SiteConfig();
 
@@ -14,7 +14,7 @@ class ChildPortal extends BasePortal {
 }
 
 f1Styles(site_config.site_id);
-exclusiveRatesComingPopup('Dutch GP', '6th November');
+popup('Check back on 6th November for additional and exclusive Dutch GP hotel rates.');
 
 new ChildPortal();
 new Distance();

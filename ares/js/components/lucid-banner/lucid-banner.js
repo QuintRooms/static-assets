dfae9c92e58bf49eb6e1235123ebd56dd182eeaf
Matt Sartain <matthewsartain@me.com>
2020-11-03 16:42:07 -0600

    Lucid banner - adds tablet and mobile text

--- ares/js/components/lucid-banner/lucid-banner.js
+++ ares/js/components/lucid-banner/lucid-banner.js
@@ -5,8 +5,9 @@ const utilities = new Utilities();
 /**
  *@description Adds a banner with a CTA to the Lucid Travel form for group bookings of more than 10 rooms.
  *@param string - Event name. Either site_config.event_name or 'Event Name'
+ *@param string - Link to jotForm.
  */
-export default async function lucidBanner(event) {
+export default async function renderLucidBanner(event, link) {
     if (document.querySelector('.RootBody') || document.querySelector('.SearchHotels')) {
         await utilities.waitForSelectorInDOM('header');
 
@@ -15,11 +16,12 @@ export default async function lucidBanner(event) {
             `
             <div class="lucid-banner">
                 <div class="lucid-content">
-                    <span id="desktop">Need 10+ rooms for ${event} or another event? We're here to help you secure more great rates.</span>     
+                    <span id="desktop">Need 10+ rooms for ${event} or another event? We're here to help you secure great rates.</span>     
+                    <span id="tablet">Need 10+ rooms for ${event}?</span>     
                     <span id="mobile">Need 10+ rooms?</span>     
                 </div>
                 <div class="lucid-button">
-                    <a id="lucid-link" target="_blank" href="">Contact Us</a>
+                    <a id="lucid-link" target="_blank" href="${link}">Contact Us</a>
                 </div>
             </div>
             <style>

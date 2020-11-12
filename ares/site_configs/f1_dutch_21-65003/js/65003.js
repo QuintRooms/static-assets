12b374d1c99f98113ba6160a5e0602d4296d188d
Matt Sartain <matthewsartain@me.com>
2020-11-12 12:21:13 -0600

    Dutch F1 GP - removes popup and adds Distance sorting

--- ares/site_configs/f1_dutch_21-65003/js/65003.js
+++ ares/site_configs/f1_dutch_21-65003/js/65003.js
@@ -1,8 +1,7 @@
 import BasePortal from '../../../js/build';
 import SiteConfig from './65003-config';
 import f1Styles from '../../../clients/formula-1/js/f1';
-// import Distance from '../../../js/calculate-distance';
-import popup from '../../../js/components/on-load-popup/popup';
+import Distance from '../../../js/calculate-distance';
 
 const site_config = new SiteConfig();
 
@@ -14,7 +13,6 @@ class ChildPortal extends BasePortal {
 }
 
 f1Styles(site_config.site_id);
-popup('Check back on 13th November for additional and exclusive Dutch GP hotel rates.');
 
 new ChildPortal();
-// new Distance();
+new Distance();

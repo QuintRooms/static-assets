a689d08fb921ca343cd867988c820af0f9dc0352
Matt Sartain <matthewsartain@me.com>
2020-11-16 16:08:43 -0600

    testing func 2

--- ares/js/func.test.js
+++ ares/js/func.test.js
@@ -1,6 +1,6 @@
 // import sum from './func';
 const sum = require('./func');
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
+test('adds 3 + 3 to equal 6', () => {
+    expect(sum(3, 3)).toBe(6);
 });
---
+++ ares/js/func2.test.js
@@ -0,0 +1,5 @@
+const subtract = require('./func2');
+
+test('subtracts 5 from 10 to equal 5', () => {
+    expect(subtract(10, 5)).toBe(5);
+});

f05e26bb4442e936181203eafa4387ec39f3d072
Matt Sartain <matthewsartain@me.com>
2020-11-18 16:01:03 -0600

    utilities tests - adds past date test

--- ares/js/utilities.test.js
+++ ares/js/utilities.test.js
@@ -3,7 +3,16 @@ import Utilities from './utilities';
 const utilities = new Utilities();
 
 describe('percentDiff', () => {
-    test('finds 50% difference between 10 and 20', () => {
+    test('Finds 50% difference between 10 and 20', () => {
         expect(utilities.percentDiff(20, 10)).toBe(50);
+    });
+});
+
+describe('checkForPastDate', () => {
+    test('Returns true for date in the past', () => {
+        expect(utilities.checkForPastDate('2020-09-27')).toBeTruthy();
+    });
+    test('Returns false for date in the future', () => {
+        expect(utilities.checkForPastDate('2021-09-27')).toBeFalsy();
     });
 });

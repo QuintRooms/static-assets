6e1eae8fb1d6684533cc5bf8ad7a5b3d0fd19ecc
Matt Sartain <matthewsartain@me.com>
2020-11-30 12:05:47 -0600

    Utilities testing - adds working tests

--- ares/js/utilities.test.js
+++ ares/js/utilities.test.js
@@ -5,6 +5,10 @@ fetchMock.enableMocks();
 
 const utilities = new Utilities();
 
+beforeEach(() => {
+    fetch.resetMocks();
+});
+
 describe('percentDiff', () => {
     test('Finds 50% difference between 10 and 20', () => {
         expect(utilities.percentDiff(20, 10)).toBe(50);
@@ -20,16 +24,23 @@ describe('checkForPastDate', () => {
     });
 });
 
-// describe('fetchHTMLFromFile', () => {
-//     const url = 'https://dev-static.hotelsforhope.com/ares/clients/formula_1/html/f1-header.html';
+describe('fetchHTMLFromFile', () => {
+    const url = 'https://dev-static.hotelsforhope.com/ares/clients/formula_1/html/f1-header.html';
 
-//     test('Returns HTML from a local file', async () => {
-//         expect(utilities.fetchHTMLFromFile(url)).toBeTruthy();
-//     });
-//     test('Is only called one time', async () => {
-//         expect(utilities.fetchHTMLFromFile(url)).toHaveBeenCalledTimes(1);
-//     });
-//     test('Was called with a path to html on dev server', async () => {
-//         expect(utilities.fetchHTMLFromFile(url)).toHaveBeenCalledWith('https://dev-static.hotelsforhope.com/ares/clients/formula_1/html/f1-header.html');
-//     });
-// });
+    it('Returns HTML from a local file', async () => {
+        fetch.mockResponseOnce('12345');
+        const html_data = await utilities.fetchHTMLFromFile(url);
+
+        expect(html_data).toEqual('12345');
+        expect(html_data).toEqual(expect.not.stringContaining(' '));
+        expect(fetch).toHaveBeenCalledTimes(1);
+        expect(fetch).toHaveBeenCalledWith('https://dev-static.hotelsforhope.com/ares/clients/formula_1/html/f1-header.html');
+    });
+
+    it('catches errors and returns the url and the error', async () => {
+        fetch.mockReject(new Error('Not 2xx response'));
+        // const html_data = await utilities.fetchHTMLFromFile(url);
+        expect(await utilities.fetchHTMLFromFile('https://dev-static.hotelsforhope.com/ares/clients/formula_1/html/f1-header.html')).toEqual('Error: Not 2xx response');
+        expect(fetch).toHaveBeenCalledTimes(1);
+    });
+});

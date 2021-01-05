9cc80695c8cc6d54df0face43edbef59895256ad
Matt Sartain <matthewsartain@me.com>
2021-01-05 11:44:13 -0600

    Resbeat retail - updates logo outbound url

--- ares/site_configs/retail_resbeat_public-51474/js/51474-config.js
+++ ares/site_configs/retail_resbeat_public-51474/js/51474-config.js
@@ -42,7 +42,7 @@ export default function siteConfig() {
         fav_icon_url: `${env_path.path}/site_configs/${directory_name}/img/favicon.png`,
         header: {
             logo_file_location: `${env_path.path}/site_configs/${directory_name}/img/logo.svg`,
-            logo_outbound_url: 'https://hotels.resbeat.com/v6/register',
+            logo_outbound_url: 'https://hotels.resbeat.com/v6/register?cta_referral=logo',
         },
         map_marker_image_url: `${env_path.path}/site_configs/${directory_name}/img/favicon.png`,
         theme: 'light',

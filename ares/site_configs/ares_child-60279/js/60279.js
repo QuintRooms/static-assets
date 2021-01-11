500b5ac5b239e51e0b93ccea5b73b618325d84fe
Matt Sartain <matthewsartain@me.com>
2021-01-11 15:24:21 -0600

    ares child - updates site to use Google places autocomplete

--- ares/site_configs/ares_child-60279/js/60279.js
+++ ares/site_configs/ares_child-60279/js/60279.js
@@ -1,38 +1,38 @@
 import BasePortal from '../../../js/build';
 import SiteConfig from './60279-config';
-import Distance from '../../../js/calculate-distance';
-import Utilities from '../../../js/utilities';
+// import Distance from '../../../js/calculate-distance';
+// import Utilities from '../../../js/utilities';
 
-const dayjs = require('dayjs');
+// const dayjs = require('dayjs');
 
-const utilities = new Utilities();
+// const utilities = new Utilities();
 
 const site_config = new SiteConfig();
 class ChildPortal extends BasePortal {
     constructor() {
         super(site_config);
         super.init();
-        this.init();
+        // this.init();
     }
 
-    init() {
-        this.prepopulateSearchDate();
-    }
+    // init() {
+    //     this.prepopulateSearchDate();
+    // }
 
-    async prepopulateSearchDate() {
-        if (!document.querySelector('.SearchHotels')) return;
+    // async prepopulateSearchDate() {
+    //     if (!document.querySelector('.SearchHotels')) return;
 
-        await utilities.waitForSelectorInDOM('#theQuickCheckIn');
+    //     await utilities.waitForSelectorInDOM('#theQuickCheckIn');
 
-        const check_in_input = document.querySelector('#theDatePrompt #theQuickCheckIn');
-        const check_out_input = document.querySelector('#theDatePrompt #theQuickCheckOut');
+    //     const check_in_input = document.querySelector('#theDatePrompt #theQuickCheckIn');
+    //     const check_out_input = document.querySelector('#theDatePrompt #theQuickCheckOut');
 
-        if (!check_in_input || !check_out_input) return;
+    //     if (!check_in_input || !check_out_input) return;
 
-        check_in_input.setAttribute('value', dayjs().format('MM/DD/YYYY'));
-        check_out_input.setAttribute('value', dayjs().add(1, 'day').format('MM/DD/YYYY'));
-    }
+    //     check_in_input.setAttribute('value', dayjs().format('MM/DD/YYYY'));
+    //     check_out_input.setAttribute('value', dayjs().add(1, 'day').format('MM/DD/YYYY'));
+    // }
 }
 
 new ChildPortal();
-new Distance();
+// new Distance();

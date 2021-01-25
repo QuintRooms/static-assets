dadb86356b4ea294f1d130f35f877bd9b17ff5fb
Matt Sartain <matthewsartain@me.com>
2021-01-25 09:57:21 -0600

    Barrett Jackson - updates landing page

--- ares/site_configs/bj_landing_page-58196/js/58196-config.js
+++ ares/site_configs/bj_landing_page-58196/js/58196-config.js
@@ -84,7 +84,7 @@ export default function siteConfig() {
             {
                 name: '2021 Scottsdale Auction',
                 display_date: 'March 20 - 27, 2021',
-                end_date: '1/25/2021', // M/D/YYYY
+                end_date: '3/28/2021', // M/D/YYYY
                 portal_url: 'https://events.hotelsforhope.com/group-event?id=46530&utm_campaign=Barrett-Jackson%20Scottsdale%202021%20Rescheduled',
             },
             {

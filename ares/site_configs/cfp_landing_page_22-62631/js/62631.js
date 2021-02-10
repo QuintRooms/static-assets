aa2da896566044374cff6ccf87909db2fe4221ae
Matt Sartain <matthewsartain@me.com>
2021-02-10 10:38:52 -0600

    CFP Landing page - updates 'addBr' func to be async

--- ares/site_configs/cfp_landing_page_22-62631/js/62631.js
+++ ares/site_configs/cfp_landing_page_22-62631/js/62631.js
@@ -1,6 +1,9 @@
 import BasePortal from '../../../js/build';
 import SiteConfig from './62631-config';
 import LandingPage from '../../../js/components/landing_page/landing-page';
+import Utilities from '../../../js/utilities';
+
+const utilities = new Utilities();
 
 const site_config = new SiteConfig();
 
@@ -14,8 +17,9 @@ class ChildPortal extends BasePortal {
 new LandingPage(site_config.landing_page_events, site_config.hide_search_on_landing_page).init();
 new ChildPortal();
 
-function addBr() {
+async function addBr() {
     if (!document.querySelector('.RootBody')) return;
+    await utilities.waitForSelectorInDOM('.event-2 .event-name');
 
     document.querySelector('.event-2 .event-name').innerHTML = '<h2 class="event-name">SEARCH <br> ANYWHERE</h2>';
 }

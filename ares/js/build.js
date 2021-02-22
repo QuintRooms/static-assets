51e7db2cb76b1addd7f1afb679543302c6a89c74
Matt Sartain <matthewsartain@me.com>
2021-02-21 21:05:17 -0600

    adds console log to sort wrapper func

--- ares/js/build.js
+++ ares/js/build.js
@@ -406,6 +406,7 @@ export default class BasePortal {
     // refactor meeeeee
     createMobileSortAndFilter() {
         if (this.site_id === '63712') return;
+        console.log('Running');
         if (!window.matchMedia('(max-width:800px)').matches || !document.querySelector('.SearchHotels')) return;
 
         utilities.updateHTML(

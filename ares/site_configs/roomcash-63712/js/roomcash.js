2cebbbfb674b53fbb87645456373224b9d22b31b
Matt Sartain <matthewsartain@me.com>
2021-03-19 15:39:27 -0500

    RoomCash - swaps textContent for innerHTML

--- ares/site_configs/roomcash-63712/js/roomcash.js
+++ ares/site_configs/roomcash-63712/js/roomcash.js
@@ -537,7 +537,7 @@ export default class Roomcash {
         const error = document.querySelector('#theWBResendOrCancelFormBody.hasErrors .errors li');
         if (!error) return;
         if (error.textContent.includes('with promotional code')) {
-            error.textContent = `Sorry - this reservation cannot be automatically canceled online. Please check the cancellation terms on your confirmation email; you may be outside of the allowable cancellation time. If not, please contact our customer service team at (512) 767 1360 or via our <a href="${
+            error.innerHTML = `Sorry - this reservation cannot be automatically canceled online. Please check the cancellation terms on your confirmation email; you may be outside of the allowable cancellation time. If not, please contact our customer service team at (512) 767 1360 or via our <a href="${
                 document.querySelector('.ARN_ServiceLinks.supportLink').href
             }">support form.</a> Our team is available M-F 8:00 AM - 5:30 PM CST to help. If you require urgent assistance outside our standard hours, please leave us a voicemail, and a member of the team will respond promptly.`;
         }

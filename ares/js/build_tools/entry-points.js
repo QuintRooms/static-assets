db3b94a184d808f6c1216d42443be22435285f8f
Matt Sartain <matthewsartain@me.com>
2021-04-07 14:56:08 -0500

    Builds Moon River Festival files - email, entry points and site_names

--- ares/js/build_tools/entry-points.js
+++ ares/js/build_tools/entry-points.js
@@ -67,6 +67,7 @@ module.exports = () => {
             // 'ares_child-60279': './src/60279.js', // Ares Child Test
             // 'cug_template-39624': './src/39624.js', // CUG Template
             // 'f1_miami_22-65012': './src/65012.js', // Formula 1 Miami Grand Prix 2022
+            // 'moon_river_festival_21-68381': './src/68381.js', // moon river_festival_21
             // 'resbeat_points_demo-62725': './src/62725.js', // Resbeat Points Demo
             // 'resbeat_test-62011': './src/62011.js', // Resbeat Test
             // 'roomcash_develop-63713': './src/63713.js', // RoomCash Develop

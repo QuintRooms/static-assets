e21402378d98d477f65605b5c93a34cb3b92e5bf
Matt Sartain <matthewsartain@me.com>
2021-04-09 14:35:56 -0500

    builds new hof friends and family portal and rebuild of Miami22

--- ares/site_configs/hof_enshrinement_friends_fam_21-66018/js/66018-config.js
+++ ares/site_configs/hof_enshrinement_friends_fam_21-66018/js/66018-config.js
@@ -1,25 +1,23 @@
 import Path from '../../../js/build_tools/path';
 
 const env_path = new Path();
-
-const site_id = 65012;
-const directory_name = `f1_miami_22-${site_id}`;
+const site_id = 66018;
+const directory_name = `hof_enshrinement_friends_fam_21-${site_id}`;
 
 export default function siteConfig() {
     return {
         site_id,
         directory_name,
-        affiliate_id: 16980,
+        affiliate_id: 17464,
         master_id: 920,
         site_type: 'lodging',
         algolia_app_id: 'plCZXR0GZ7J1',
         algolia_api_key: 'b9763a419845b59957b8cc5c9b13440c',
-        use_google_maps: false,
-        currency: 'USD',
+        use_google_autocomplete: true,
         distance_unit: 'useMiles',
         lodging: {
-            event_id: 11111,
-            event_name: 'Miami Grand Prix',
+            event_id: '',
+            event_name: 'Pro Football Hall of Fame',
             event_dates: '',
             redirect_date: '',
             redirect_url: '',
@@ -28,30 +26,30 @@ export default function siteConfig() {
         cug: {
             is_cug: false,
             show_points: false,
-            allow_registration: true,
+            allow_registration: false,
             percent_off_text: 'off',
             show_percent_savings: false,
         },
         exclusive_rate_text: 'Exclusive Rate',
         host_hotel_text: 'Host Hotel',
         partner_hotel_text: 'Partner Hotel',
-        show_tax_inclusive_rates: true,
-        dayjs_date_format: 'D/M/YYYY',
+        show_tax_inclusive_rates: false,
+        dayjs_date_format: 'M/D/YYYY',
         reviews_before_info: true,
         show_stars: false,
         show_property_type: false,
-        show_language_select: true,
-        show_currency_select: true,
+        show_language_select: false,
+        show_currency_select: false,
         show_date_prompt: false,
         has_social_sharing: true,
-        fav_icon_url: `${env_path.path}/clients/formula_1/images/favicon.png`,
+        fav_icon_url: `${env_path.path}/site_configs/${directory_name}/img/favicon.png`,
         header: {
             logo_file_location: `${env_path.path}/site_configs/${directory_name}/img/logo.png`,
-            logo_outbound_url: 'https://bookrooms.formula1.com/v6?siteid=45246&theme=standard',
+            logo_outbound_url: 'https://www.profootballhof.com/',
         },
-        map_marker_image_url: `${env_path.path}/clients/formula_1/images/favicon.png`,
+        map_marker_image_url: `${env_path.path}/site_configs/${directory_name}/img/favicon.png`,
         theme: 'light',
-        google_font_url: 'https://fonts.googleapis.com/css2?family=Titillium+Web:wght@300;400;600&display=swap',
+        google_font_url: '//fonts.googleapis.com/css?family=Montserrat:100,500,700',
         ads: {
             sidebar_ad: {
                 is_active: false,
@@ -59,6 +57,11 @@ export default function siteConfig() {
                 outbound_url: '',
             },
             between_property_ad: {
+                is_active: false,
+                image_url: '',
+                outbound_url: '',
+            },
+            confirmation_page_top: {
                 is_active: false,
                 image_url: '',
                 outbound_url: '',
@@ -73,9 +76,19 @@ export default function siteConfig() {
         root_page_header_text: 'Start Your Search',
         root_page_subheader_text: 'Reservations at 600K+ Hotels at Unbeatable Rates',
         is_resbeat_client: false,
-        has_custom_emails: true,
-        // has_group_booking_banner: true,
-        // group_booking_form_url: 'https://form.jotform.com/203066540331141?bookingPortal=BJAC%20Las%20Vegas',
+        confirmation_email_from: 'Hotels for Hope',
+        has_group_booking_banner: false,
+        group_booking_form_url: '',
+        has_landing_page: false,
+        hide_search_on_landing_page: true,
+        landing_page_events: [
+            {
+                name: '',
+                display_date: '', // August 5-8, 2021
+                end_date: '', // M/D/YYYY
+                portal_url: '',
+            },
+        ],
         uses_hybrid_compensation_model: true,
     };
 }

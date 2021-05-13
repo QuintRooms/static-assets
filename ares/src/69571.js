d88ca705a94a03095575b7205695a6235219437f
Derek Butvin <derek.butvin@quintrooms.com>
2021-05-13 17:29:53 -0400

    TBF & Shaky  Knees - dev build

--- ares/src/69571.js
+++ ares/src/69571.js
@@ -1,2 +1,2 @@
-import '../site_configs/texas_book_festival-69571/js/69571';
-import '../site_configs/texas_book_festival-69571/styles/69571.scss';
+import '../site_configs/texas_book_festival_21-69571/js/69571';
+import '../site_configs/texas_book_festival_21-69571/styles/69571.scss';
---
+++ ares/src/70057.js
@@ -0,0 +1,2 @@
+import '../site_configs/shaky_knees_22-70057/js/70057';
+import '../site_configs/shaky_knees_22-70057/styles/70057.scss';

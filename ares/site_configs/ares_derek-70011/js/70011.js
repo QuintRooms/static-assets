7db57775bdcd9ba6c51cec0555e715485071bd41
Derek Butvin <derek.butvin@quintrooms.com>
2021-05-20 16:41:21 -0400

    70011 file - updates

--- ares/site_configs/ares_derek-70011/js/70011.js
+++ ares/site_configs/ares_derek-70011/js/70011.js
@@ -1,12 +1,106 @@
 import BasePortal from '../../../js/build';
 import SiteConfig from './70011-config';
+import Modal from '../../../js/components/modal/script';
 
 const site_config = new SiteConfig();
+const test_modal = new Modal('test_modal', html_str, 'test-modal', 'Got it!');
 class ChildPortal extends BasePortal {
     constructor() {
         super(site_config);
         super.init();
     }
 }
+
+// const useIconStars = () => {
+//     const star_box_parent = document.querySelector('.V5StarsBox').parentElement;
+//     const star_svg ='<svg height="21" width="20" class="star rating" data-rating="1"><polygon points="9.9, 1.1, 3.3, 21.78, 19.8, 8.58, 0, 8.58, 16.5, 21.78" style="fill: #faaf18"/></svg>';
+
+//     star_box_parent.forEach((starBox) => {
+//         const star_box_classes = starBox.classList;
+//         if(star_box_classes.contains('StarsBox')) {
+//             const stars_text = starBox.forEach((starBoxDiv) => {
+//                 const star_box_div_classes = starBoxDiv.classList;
+//                 if(star_box_div_classes.contains('Stars')){
+//                     return starBoxDiv;
+//                 }
+
+//                 const number_of_stars = stars_text.textContent;
+//                 const num = number_of_stars.replace(/\D/g, '');
+
+//                 stars_text.innerHTML =  star_svg.repeat(num);
+//             });
+//         }
+//     })
+// }
+
+// const useIconStars = () => {
+//     const star_boxes = document.querySelector('.V5StarsBox').parentElement.children;
+
+//     for (star_box of star_boxes){
+//         if (!star_box.classList.contains('StarsBox')) return
+//             const star_box_divs = star_box.children;
+//             for (star_box_div of star_box_divs) {
+//                 if (star_box_div.classList.contains('Stars')) {
+//                     star_box_div.innerHTML = 'fart';
+//                 }
+//             }
+//     }
+// };
+
+// function loadStarDivs(callback) {
+
+//        xhr.onload = function() {
+//          if(xhr.status === 200) {
+//            callback();
+//          }
+//        }
+//     }
+
+//     loadStarDivs(()=>{
+//     const star_texts = document.querySelectorAll('.lbl5stars, .lbl4stars, .lbl3stars, .lbl2stars, .lbl1stars');
+//     star_texts.forEach((star_text)=>{
+//      console.log('fart');
+//     });
+//    });
+
+// const useIconStars = () => {
+//     const star_boxes = document.querySelector('.V5StarsBox').parentElement.children;
+//     const star_svg =
+//         '<svg height="21" width="20" class="star rating" data-rating="1"><polygon points="9.9, 1.1, 3.3, 21.78, 19.8, 8.58, 0, 8.58, 16.5, 21.78" style="fill: #faaf18"/></svg>';
+
+//     for (let i = 1; i < star_boxes.length; i++) {
+//         const star_box_divs = star_boxes[i].children;
+//         const star_div = star_box_divs[star_box_divs.length - 1];
+//         const star_div_text = star_div.textContent;
+//         const star_num = star_div_text.replace(/\D/g, '');
+        
+//         star_div.innerHTML = star_svg.repeat(star_num);
+//     }
+// };
+
+const html_str = `
+<h4>Test</h4>
+<p>this is only a test</p>
+`;
+
+const useIconStarsFromNodes = async () => {
+    // const star_boxes = await document.querySelectorAll('.lbl5stars', '.lbl4stars', '.lbl3stars', '.lbl2stars', '.lbl1stars');
+    const star_boxes = document.querySelector('.V5StarsBox').parentElement.children;
+    star_boxes_arr = Array.from(star_boxes);
+    star_boxes_arr.shift();
+    const star_svg =
+        '<svg height="21" width="20" class="star rating" data-rating="1"><polygon points="9.9, 1.1, 3.3, 21.78, 19.8, 8.58, 0, 8.58, 16.5, 21.78" style="fill: #faaf18"/></svg>';
+
+    star_boxes_arr.forEach((star_box) => {
+        const star_box_divs = star_box.children;
+        const star_div = star_box_divs[star_box_divs.length - 1];
+        const star_num = star_div.textContent.replace(/\D/g, '');
+        star_div.innerHTML = star_svg.repeat(star_num);
+    });
+};
+
+useIconStarsFromNodes();
+
+test_modal.init();
 
 new ChildPortal();

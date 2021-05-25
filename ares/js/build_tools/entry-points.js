c51c8982ddba9825e54c961318cadb95b367096f
Derek Butvin <derek.butvin@quintrooms.com>
2021-05-25 12:52:01 -0400

    Entry Points - Update with Music Mitown

--- ares/js/build_tools/entry-points.js
+++ ares/js/build_tools/entry-points.js
@@ -35,7 +35,7 @@ module.exports = () => {
             // 'f1_miami_22-65012': './src/65012.js', // Formula 1 Miami Grand Prix 2022
             // 'f1_portugal_20-54494': './src/54494.js', // Formula 1 Portugal 2020
             // 'f1_template-46972': './src/46972.js', // Formula 1 Template
-            'firefly_21-64866': './src/64866.js', // firefly 21
+            // 'firefly_21-64866': './src/64866.js', // firefly 21
             // 'hof_canton-62681': './src/62681.js', // Hall Of Fame Canton
             // 'hof_cug-63710': './src/63710.js', // Hall Of Fame cug
             // 'hof_enshrinement_21-60041': './src/60041.js', // Hall Of Fame Enshrinement 2021
@@ -82,6 +82,7 @@ module.exports = () => {
             // 'resbeat_points_demo-62725': './src/62725.js', // Resbeat Points Demo
             // 'resbeat_test-62011': './src/62011.js', // Resbeat Test
             // 'roomcash_develop-63713': './src/63713.js', // RoomCash Develop
+            // 'music_midtown_21-70013': './src/70013.js', // Music Midtown Production 2021
         };
 
         // local environment

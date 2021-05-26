c30c2689a9a6b09db3ecd1bbe514f81f8dfae83b
Derek Butvin <derek.butvin@quintrooms.com>
2021-05-26 12:01:48 -0400

    Modal Files - clean up. need work on logic to continue running listeners

--- ares/js/components/modal/modal.js
+++ ares/js/components/modal/modal.js
@@ -7,7 +7,7 @@ const modal_styles_str = `
         background: white;
         border-radius: 6px;
         box-sizing: border-box;
-        width: 300px;
+        width: 600px;
         max-height: 90%;
         padding: 15px;
         flex-direction: column;
@@ -17,6 +17,7 @@ const modal_styles_str = `
         left: 50%;
         z-index: 995;
         transform: translate(-50%, -50%);
+           display: none;
     }
 
     .overlay {
@@ -26,15 +27,13 @@ const modal_styles_str = `
         width: 100%;
         height: 100%;
         z-index: 995;
-        background: rgba(0,0,0,0.70);
-    }
-
-    .hide {
-        display: none;
+        background: rgba(0,0,0,0.40);
+           display: none;
     }
 
     #x-button {
         border: none;
+        color: #a1a1a1;
         display: inline-block;
         padding: 0px;
         margin-right: 16px;
@@ -53,6 +52,22 @@ const modal_styles_str = `
         right: 0;
         top: 0
     }
+
+    #modal-title {
+        margin-top: 8px;
+        width: 100%;
+        text-align: center;
+        font-size: 20px;
+    }
+
+    hr .solid{
+        border-top: 3px solid $primary_color;
+    }
+
+    .modal-body-container {
+        padding-left: 10px;
+        padding-right: 10px;
+    }
 `;
 
 export default class Modal {
@@ -64,75 +79,70 @@ export default class Modal {
         this.init();
         this.outer_modal = document.querySelector('.outer-modal');
         this.overlay = document.querySelector('.overlay');
-        // this.modal_triggers = document.querySelector(`.${this.modal_id}-trigger`);
+        this.modal_triggers = '';
+        this.modal_closers = document.querySelector(`.button-close-modal`);
     }
 
     init() {
         this.setModalTriggers();
         this.insertModalContainer();
-        this.initiateOpenModal();
+        this.setVars();
+        this.openListeners();
     }
 
-    toggleModalVisibility() {
-        console.log('inside toggleModalVisibility');
-        const modal_state = this.outer_modal.classList;
-        const overlay_state = this.overlay.classList;
-        const modal_triggers = document.querySelector(`.${this.modal_id}-trigger`);
-        console.log('modal_triggers inside toggleModal', modal_triggers);
-
-        if (modal_state.contains('hide')) {
-            modal_state.remove('hide');
-            overlay_state.remove('hide');
-            modal_triggers.removeEventListener('click', this.openModal);
-            document.addEventListener('click', this.closeModal);
-        } else {
-            modal_state.add('hide');
-            overlay_state.add('hide');
-            document.removeEventListener('click', this.closeModal);
-            modal_triggers.addEventListener('click', this.openModal);
-        }
+    async setVars() {
+        await utilities.waitForSelectorInDOM(`.${this.modal_id}-trigger`);
+        this.modal_triggers = document.querySelector(`.${this.modal_id}-trigger`);
     }
 
-    async openModal(event) {
-        const modal_triggers = document.querySelector(`.${this.modal_id}-trigger`);
-        console.log('inside openModal');
-        console.log('openModal event.target', event.target);
-        console.log('modal triggers inside openModal', modal_triggers);
-        // this.toggleModalVisibility();
-
-        event.preventDefault();
-        if (event.target.matches(modal_triggers)) {
-            console.log('inside openModal if statement');
-            this.toggleModalVisibility();
-        }
-    }
-
-    async initiateOpenModal() {
-        console.log('inside initiateopenModal');
+    async openListeners() {
         await utilities.waitForSelectorInDOM(`.${this.modal_id}-trigger`);
         const modal_triggers = document.querySelector(`.${this.modal_id}-trigger`);
-        console.log('modal_triggers inside inititateOpenModal', modal_triggers);
-
-        modal_triggers.addEventListener('click', this.openModal);
-        console.log('initiateOpenModal executed');
+        modal_triggers.addEventListener('click', this.showModal);
     }
 
-    closeModal(event) {
-        console.log('inside closeModal');
-        event.preventDefault();
-        if (event.target.matches('.button-close-modal') || !event.target.closest('.outer-modal')) {
-            this.toggleModalVisibility();
-        }
+    closeListeners() {
+        const modal_closers = document.querySelector('.button-close-modal');
+        modal_closers.addEventListener('click', this.hideModal);
+        console.log('inside eventlistener');
+        document.addEventListener('click', (event) => {
+            console.log('inside second event listener');
+            console.log(this.hideModal);
+            if (!event.target.closest('.outer-modal')) {
+                this.hideModal();
+            }
+        });
     }
+
+    showModal = () => {
+        const modal_triggers = document.querySelector(`.${this.modal_id}-trigger`);
+        const modal_closers = document.querySelector('.button-close-modal');
+
+        this.outer_modal.style.display = 'block';
+        this.overlay.style.display = 'block';
+        modal_triggers.removeEventListener('click', this.showModal);
+        this.closeListeners();
+    };
+
+    hideModal = () => {
+        const modal_triggers = document.querySelector(`.${this.modal_id}-trigger`);
+        const modal_closers = document.querySelector('.button-close-modal');
+
+        this.outer_modal.style.display = 'none';
+        this.overlay.style.display = 'none';
+        modal_closers.removeEventListener('click', this.hideModal);
+        this.openListeners();
+    };
 
     insertModalContainer() {
         document.body.insertAdjacentHTML(
             'beforeend',
             `
-            <div class='overlay hide'></div>
-            <div class='outer-modal hide' id=${this.modal_id}>
+            <div class='overlay'></div>
+            <div class='outer-modal' id=${this.modal_id}>
                 <button id='x-button' class='button-close-modal upper-right'>x</button>
                 <div id='modal-title'>${this.modal_title}</div>
+                <hr class="solid">
                 <div class='modal-body-container'>${this.modal_body}</div>
             </div>
             <style>
@@ -150,6 +160,172 @@ export default class Modal {
         const modal_trigger_setter = document.querySelector('#HotelNameContainer');
         modal_trigger_setter.classList.add(`${this.modal_id}-trigger`);
     }
+}
+
+    // async initiateOpenModal() {
+    //     console.log('inside initiateopenModal');
+    //     await utilities.waitForSelectorInDOM(`.${this.modal_id}-trigger`);
+    //     const modal_triggers = document.querySelector(`.${this.modal_id}-trigger`);
+    //     console.log('modal_triggers inside inititateOpenModal', modal_triggers);
+
+    //     modal_triggers.addEventListener('click', this.openModal);
+    //     console.log('initiateOpenModal executed');
+    // }
+
+
+// const modal_styles_str = `
+//     .outer-modal {
+//         background: white;
+//         border-radius: 6px;
+//         box-sizing: border-box;
+//         width: 300px;
+//         max-height: 90%;
+//         padding: 15px;
+//         flex-direction: column;
+//         align-self: center;
+//         position: fixed;
+//         top: 50%;
+//         left: 50%;
+//         z-index: 995;
+//         transform: translate(-50%, -50%);
+//     }
+
+//     .overlay {
+//         position: fixed;
+//         left: 0;
+//         top: 0;
+//         width: 100%;
+//         height: 100%;
+//         z-index: 995;
+//         background: rgba(0,0,0,0.70);
+//     }
+
+//     .hide {
+//         display: none;
+//     }
+
+//     #x-button {
+//         border: none;
+//         display: inline-block;
+//         padding: 0px;
+//         margin-right: 16px;
+//         margin-top: 8px;
+//         vertical-align: middle;
+//         overflow: hidden;
+//         text-decoration: none;
+//         background-color: inherit;
+//         text-align: center;
+//         cursor: pointer;
+//         white-space: nowrap
+//     }
+
+//     .upper-right {
+//         position: absolute;
+//         right: 0;
+//         top: 0
+//     }
+// `;
+
+// export default class Modal {
+//     constructor(modal_title, modal_body, modal_id, modal_trigger_selector) {
+//         this.modal_title = modal_title;
+//         this.modal_body = modal_body;
+//         this.modal_id = modal_id;
+//         this.modal_trigger_selector = modal_trigger_selector;
+//         this.init();
+//         this.outer_modal = document.querySelector('.outer-modal');
+//         this.overlay = document.querySelector('.overlay');
+//         // this.modal_triggers = document.querySelector(`.${this.modal_id}-trigger`);
+//     }
+
+//     init() {
+//         this.setModalTriggers();
+//         this.insertModalContainer();
+//         this.initiateOpenModal();
+//     }
+
+//     toggleModalVisibility() {
+//         console.log('inside toggleModalVisibility');
+//         const modal_state = this.outer_modal.classList;
+//         const overlay_state = this.overlay.classList;
+//         const modal_triggers = document.querySelector(`.${this.modal_id}-trigger`);
+//         console.log('modal_triggers inside toggleModal', modal_triggers);
+
+//         if (modal_state.contains('hide')) {
+//             modal_state.remove('hide');
+//             overlay_state.remove('hide');
+//             modal_triggers.removeEventListener('click', this.openModal);
+//             document.addEventListener('click', this.closeModal);
+//         } else {
+//             modal_state.add('hide');
+//             overlay_state.add('hide');
+//             document.removeEventListener('click', this.closeModal);
+//             modal_triggers.addEventListener('click', this.openModal);
+//         }
+//     }
+
+//     async openModal(event) {
+//         const modal_triggers = document.querySelector(`.${this.modal_id}-trigger`);
+//         console.log('inside openModal');
+//         console.log('openModal event.target', event.target);
+//         console.log('modal triggers inside openModal', modal_triggers);
+//         // this.toggleModalVisibility();
+
+//         event.preventDefault();
+//         if (event.target.matches(modal_triggers)) {
+//             console.log('inside openModal if statement');
+//             this.toggleModalVisibility();
+//         }
+//     }
+
+//     async initiateOpenModal() {
+//         console.log('inside initiateopenModal');
+//         await utilities.waitForSelectorInDOM(`.${this.modal_id}-trigger`);
+//         const modal_triggers = document.querySelector(`.${this.modal_id}-trigger`);
+//         console.log('modal_triggers inside inititateOpenModal', modal_triggers);
+
+//         modal_triggers.addEventListener('click', this.openModal);
+//         console.log('initiateOpenModal executed');
+//     }
+
+//     closeModal(event) {
+//         console.log('inside closeModal');
+//         event.preventDefault();
+//         if (event.target.matches('.button-close-modal') || !event.target.closest('.outer-modal')) {
+//             this.toggleModalVisibility();
+//         }
+//     }
+
+//     insertModalContainer() {
+//         document.body.insertAdjacentHTML(
+//             'beforeend',
+//             `
+//             <div class='overlay hide'></div>
+//             <div class='outer-modal hide' id=${this.modal_id}>
+//                 <button id='x-button' class='button-close-modal upper-right'>x</button>
+//                 <div id='modal-title'>${this.modal_title}</div>
+//                 <div class='modal-body-container'>${this.modal_body}</div>
+//             </div>
+//             <style>
+//                 ${modal_styles_str}
+//                 .${this.modal_id}-trigger {
+//                     cursor: pointer;
+//                 }
+//             </style>
+//         `
+//         );
+//     }
+
+//     async setModalTriggers() {
+//         await utilities.waitForSelectorInDOM('#HotelNameContainer');
+//         const modal_trigger_setter = document.querySelector('#HotelNameContainer');
+//         modal_trigger_setter.classList.add(`${this.modal_id}-trigger`);
+//     }
+
+
+
+
+
 
     // async openModal() {
     //     await utilities.waitForSelectorInDOM(`.${this.modal_id}-trigger`);
@@ -226,4 +402,4 @@ export default class Modal {
 
     //     }
     // }
-}
+

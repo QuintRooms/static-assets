50f2c2f49168583fb21ecc6b06499fc5ee4b3f7c
Derek Butvin <derek.butvin@quintrooms.com>
2021-05-26 17:59:15 -0400

    Packers experiences - first Dev Build

--- ares/src/61430.js
+++ ares/src/61430.js
@@ -1,4 +1,2 @@
-import '../site_configs/packers_experiences_21-61430/js/61430';
-import '../site_configs/packers_experiences_21-61430/styles/61430.scss';
 import '../site_configs/packers_experiences_retail-61430/js/61430';
 import '../site_configs/packers_experiences_retail-61430/styles/61430.scss';

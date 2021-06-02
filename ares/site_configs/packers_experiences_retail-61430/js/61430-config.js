de52c92260657e0a033562e30474fbc3a7704422
Derek Butvin <derek.butvin@quintrooms.com>
2021-06-02 11:41:58 -0400

    Packers Experiences - updated site type to 'retail'

--- ares/site_configs/packers_experiences_retail-61430/js/61430-config.js
+++ ares/site_configs/packers_experiences_retail-61430/js/61430-config.js
@@ -10,7 +10,7 @@ export default function siteConfig() {
         directory_name,
         affiliate_id: 16999,
         master_id: 920,
-        site_type: 'lodging',
+        site_type: 'retail',
         algolia_app_id: 'plCZXR0GZ7J1',
         algolia_api_key: 'b9763a419845b59957b8cc5c9b13440c',
         use_google_autocomplete: true,

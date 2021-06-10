29bf6800f8bbc68058dfd6f3b1ad9cb9937a6313
Derek Butvin <derek.butvin@quintrooms.com>
2021-06-10 13:23:27 -0500

    Entry-Points - add voodoo 22

--- ares/js/build_tools/entry-points.js
+++ ares/js/build_tools/entry-points.js
@@ -89,13 +89,14 @@ module.exports = () => {
             // 'resbeat_points_demo-62725': './src/62725.js', // Resbeat Points Demo
             // 'resbeat_test-62011': './src/62011.js', // Resbeat Test
             // 'roomcash_develop-63713': './src/63713.js', // RoomCash Develop
-            'voodoo_22-70452': './src/70452.js', // Voodoo Festival 2022
         };
 
         // local environment
     } else if (process.env.NODE_ENV === 'local') {
         entry_points = {
             // new sites populate below
+            'voodoo_22-70452': './src/70452.js', // Voodoo Festival Attendees 2022
+            // 'voodoo_22-70453': './src/70453.js', // Voodoo Festival Production 2022
         };
     }
 

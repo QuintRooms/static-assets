5b0453d3dcaa2f619faca3e10add5119bbc3d5b6
Derek Butvin <derek.butvin@quintrooms.com>
2021-06-17 15:15:29 -0500

    Entry Points - Commented out

--- ares/js/build_tools/entry-points.js
+++ ares/js/build_tools/entry-points.js
@@ -55,6 +55,7 @@ module.exports = () => {
             // 'lolla_20-52975': './src/52975.js', // Lollapalooza 2020
             // 'moon_river_festival_21-68381': './src/68381.js', // moon river_festival_21
             // 'moto_gp_portugal-67854': './src/67854.js', // moto gp_portugal
+            // 'music_midtown_21-70012': './src/70012.js', // Music Midtown Production 2021
             // 'music_midtown_21-70013': './src/70013.js', // Music Midtown Attendees 2021
             // 'packers_experiences_retail-70421': './src/70421.js', // packers experiences_retail
             // 'railbird_21-68382': './src/68382.js', // railbird 21
@@ -91,7 +92,6 @@ module.exports = () => {
             // 'resbeat_points_demo-62725': './src/62725.js', // Resbeat Points Demo
             // 'resbeat_test-62011': './src/62011.js', // Resbeat Test
             // 'roomcash_develop-63713': './src/63713.js', // RoomCash Develop
-            'music_midtown_21-70012': './src/70012.js', // Music Midtown Production 2021
         };
 
         // local environment

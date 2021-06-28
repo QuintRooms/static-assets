b74d1cabac4aeab032e647d3b0b78d3ac80f01a5
Derek Butvin <derek.butvin@quintrooms.com>
2021-06-28 12:43:13 -0500

    F1 Miami GP 22 - #60295 updated to year 2022

--- ares/src/60295.js
+++ ares/src/60295.js
@@ -1,2 +1,2 @@
-import '../site_configs/f1_miami_21-60295/js/60295';
-import '../site_configs/f1_miami_21-60295/styles/60295.scss';
+import '../site_configs/f1_miami_22-60295/js/60295';
+import '../site_configs/f1_miami_22-60295/styles/60295.scss';

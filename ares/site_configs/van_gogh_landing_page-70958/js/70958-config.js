91bc9ec5ced8717d3272808757da0b18ead40e29
Derek Butvin <derek.butvin@quintrooms.com>
2021-07-09 11:24:35 -0500

    Van Gogh Landing Page - Dev - Updates for 3 pages

--- ares/site_configs/van_gogh_landing_page-70958/js/70958-config.js
+++ ares/site_configs/van_gogh_landing_page-70958/js/70958-config.js
@@ -11,6 +11,7 @@ export default function siteConfig() {
         affiliate_id: 17560,
         master_id: 920,
         site_type: 'event-landing-page',
+        use_google_autocomplete: true,
         algolia_app_id: 'plCZXR0GZ7J1',
         algolia_api_key: 'b9763a419845b59957b8cc5c9b13440c',
         currency: 'USD',
@@ -76,23 +77,25 @@ export default function siteConfig() {
         hide_search_on_landing_page: true,
         landing_page_events: [
             {
-                name: '2021 Exhibit Dallas',
-                display_date: 'August 07 - October 03, 2021',
+                name: 'Immersive Exhibit Dallas',
+                display_date: '08/0721 - 10/03/21',
                 end_date: '10/04/2021', // M/D/YYYY
-                portal_url: `https://events.${process.env.domain}/group-event?id=46530&utm_campaign=Barrett-Jackson%20Scottsdale%202021%20Rescheduled`,
+                portal_url: `https://events.quintrooms.com/v6?currency=USD&type=geo&siteid=71011&longitude=-96.7927&latitude=32.7784&checkin=10/02/2021&nights=1&map&locationlabel=Immersive+Van+Gogh+Exhibition&propertytypes=Hotel,Motel,Resort,Hostel,Ext.%20Stay,Boutique`,
             },
             {
-                name: '2021 Exhibit Phoenix',
-                display_date: 'April 08 - 10, 2021',
-                end_date: '10/04/2021', // M/D/YYYY
+                name: 'Immersive Exhibit Phoenix',
+                display_date: '08/26/21 - 11/28/21',
+                end_date: '11/29/2021', // M/D/YYYY
                 portal_url: `http://events.${process.env.domain}/group-event?id=44704&utm_campaign=Barrett-Jackson%20West%20Palm%20Beach%202021`,
             },
             {
-                name: '2021 Las Vegas Auction',
-                display_date: 'June 17 - 19, 2021',
-                end_date: '10/04/2021', // M/D/YYYY
+                name: 'Immersive Exhibit Los Angeles',
+                display_date: '07/31/21 - 01/02/22',
+                end_date: '01/03/2022', // M/D/YYYY
                 portal_url: `https://events.${process.env.domain}/group-event?id=47795&utm_campaign=Barrett-Jackson%20Las%20Vegas%202021`,
             },
         ],
     };
 }
+
+//https://events.quintrooms.com/v6?currency=USD&type=geo&siteid=71011&longitude=-96.79271539517177&latitude=32.77849689607953&radius=100&checkin=10/02/2021&nights=1&map&locationlabel=Immersive+Van+Gogh+Exhibition&cid=GROUP-EVENT-EMAIL&pageSize=15&useMiles&propertytypes=Hotel,Motel,Resort,Hostel,Ext.%20Stay,Boutique

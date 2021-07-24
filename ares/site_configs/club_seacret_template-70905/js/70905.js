675d9f18a9b4900eb34519685796a96561b5677a
Derek Butvin <derek.butvin@quintrooms.com>
2021-07-23 22:11:34 -0500

    seacret - Initial Honeybadger setup

--- ares/site_configs/club_seacret_template-70905/js/70905.js
+++ ares/site_configs/club_seacret_template-70905/js/70905.js
@@ -3,6 +3,15 @@ import BasePortal from '../../../js/build';
 import SiteConfig from './70905-config';
 import Utilities from '../../../js/utilities';
 
+const Honeybadger = require('@honeybadger-io/js');
+
+Honeybadger.configure({
+    apiKey: process.env.HONEYBADGER_API_KEY,
+    environment: 'production',
+});
+
+// Honeybadger.notify('Hello from HoneyBadger');
+
 const site_config = new SiteConfig();
 const utilities = new Utilities();
 
@@ -110,18 +119,32 @@ class ChildPortal extends BasePortal {
     }
 
     async getTrip() {
-        const trip_id = utilities.getUrlParameter('tripId');
-
-        this.trip = this.trips.results.find((obj) => {
-            return obj.id === trip_id;
-        });
-
-        return this.trip;
+        try {
+            const trip_id = utilities.getUrlParameter('tripId');
+            this.trip = this.trips.results.find((obj) => {
+                return obj.id === trip_id;
+            });
+            return this.trip;
+        } catch (error) {
+            console.error('Error in getTrip(): ', error);
+        }
     }
 
     async insertTripDetailsIntoHtml() {
         // Await load and format dates
         await utilities.waitForSelectorInDOM('.bottom-carousel-article');
+
+        if (
+            !this.trip?.data?.trip_name?.[0]?.text ||
+            !this.trip.data.property_name?.[0]?.text ||
+            !this.trip.data.trip_location?.[0]?.text ||
+            !this.trip.data.start_date ||
+            !this.trip.data.end_date
+        ) {
+            window.alert('Sorry, but we cannot find this trip. Please contact support.');
+            return Honeybadger.notify('Trip object, trip data, trip name, property name, trip date, or trip location not found.');
+        }
+
         const start_date = dayjs(this.trip.data.start_date).format('MM/DD/YYYY');
         const end_date = dayjs(this.trip.data.end_date).format('MM/DD/YYYY');
 
@@ -132,7 +155,7 @@ class ChildPortal extends BasePortal {
             <div class='hero-text-container'>
                 <h1 class='trip-title'>${this.trip.data.trip_name[0].text}</h1>
                 <div class='location-date-container'>
-                    <h2 class='trip-location'>${this.trip.data.property_name[0].text}</h2> 
+                    <h2 class='trip-location'>${this.trip.data.property_name[0].text}</h2>
                     <h2 class='event-text-separators'> | </h2>
                     <h2 class='trip-location'>${this.trip.data.trip_location[0].text}</h2>
                     <h2 class='event-text-separators'> | </h2>
@@ -142,9 +165,11 @@ class ChildPortal extends BasePortal {
         `
         );
 
-        // if (!document.querySelector('#trip-name') || !document.querySelector('#trip-date') || !document.querySelector('#itinerary-container-title')) return;
-
         // Create and populate itinerary container from CMS object
+        if (!this.trip.data.itinerary?.[0].day?.[0]?.text || !this.trip.data.itinerary[0].description?.[0]?.text) {
+            window.alert('Sorry, but we cannot find these trip details. Please contact support.');
+            return Honeybadger.notify('Itinerary object, itinerary day, or itinerary description not found.');
+        }
         this.trip.data.itinerary.forEach((i) => {
             document.querySelector('.itinerary-list').insertAdjacentHTML(
                 'beforeEnd',
@@ -173,10 +198,13 @@ class ChildPortal extends BasePortal {
         });
 
         // Pull existing property rooms from DOM and use them to create new room containers
+        if (!document.querySelectorAll('#standardAvail .rateRow')) {
+            window.alert('Sorry, but we cannot find rooms for this trip. Please contact support.');
+            return Honeybadger.notify('ARN property rooms array from DOM is not found.');
+        }
         const room_array = document.querySelectorAll('#standardAvail .rateRow');
 
         room_array.forEach((i) => {
-
             // Insert new rooms container skeleton
             document.querySelector('.trips-list').insertAdjacentHTML(
                 'afterBegin',
@@ -197,6 +225,11 @@ class ChildPortal extends BasePortal {
             );
 
             // Insert price into new containers before removing unwanted divs from DOM
+            if (!i.querySelector('.full-stay')?.innerText) {
+                window.alert('Sorry, but we cannot find prices for this trip. Please contact support.');
+                return Honeybadger.notify('ARN Full-stay price for trip is not found.');
+            }
+
             const full_rate_string = i.querySelector('.full-stay').innerText;
             const trip_rate = Number(full_rate_string.split(' ')[0]).toLocaleString();
             const mobile_price_container = document.querySelector('.trip-price-mobile');
@@ -205,6 +238,11 @@ class ChildPortal extends BasePortal {
             desktop_price_container.innerText = `$${trip_rate}`;
 
             // Move CTAs to new rooms container and format
+            if (!i.querySelector('.bookRoom')?.innerText) {
+                window.alert('Sorry, there was an error. Please contact support.');
+                return Honeybadger.notify('ARN book cta is not found.');
+            }
+
             const cta_container = document.querySelector('.trip-ctas');
             const original_book_cta = i.querySelector('.bookRoom');
             original_book_cta.classList.remove('bookRoom');
@@ -214,7 +252,6 @@ class ChildPortal extends BasePortal {
             cta_container.appendChild(original_book_cta);
 
             const original_hold_cta = i.querySelector('.holdRoom');
-
             if (original_hold_cta) {
                 original_hold_cta.classList.remove('holdRoom');
                 original_hold_cta.classList.add('hold-button');
@@ -224,6 +261,10 @@ class ChildPortal extends BasePortal {
             }
 
             // Move Cancellation policy and alert contianer to new rooms containers
+            if (!i.querySelector('.ArnRateCancelPolicyContainer') || !i.querySelector('.ArnRateCancelAnchor')) {
+                Honeybadger.notify('ARN cancel policy container or anchor are not found.');
+            }
+
             const price_cta_container = document.querySelector('.trip-price-cta-container');
             const cancellation_policy_container = i.querySelector('.ArnRateCancelPolicyContainer');
             const cancellation_policy_link = i.querySelector('.ArnRateCancelAnchor');
@@ -243,17 +284,17 @@ class ChildPortal extends BasePortal {
             calendar_popup.remove();
 
             // Grab trip item title and description divs and content to fill them with
+            if (!i.innerText?.split(':')[0] || !i.innerText.split(':')[1]) {
+                // window.alert('Sorry, there was an error with the trip description. If you need further information, please contact support.');
+                Honeybadger.notify('Room description, Room description colon, room description name, or Room description text is not found');
+            }
             const text_string = i.innerText;
-            console.log(text_string);
             const text_array = i.innerText.split(':');
-            console.log('room text_array', text_array);
             const room_title_container = document.querySelector('.trip-item-name');
             const room_title = text_array[0].trim();
             const room_description_container = document.querySelector('.trip-item-description');
-            // console.log(room_description_container);
             const room_description_splitter = i.innerText.indexOf(':') + 1;
             const room_description = i.innerText.slice(room_description_splitter).trim();
-            // console.log(typeof room_description);
 
             // Check that room description contains correct elements to format
             // if not include entire original string
@@ -263,6 +304,7 @@ class ChildPortal extends BasePortal {
                     return;
                 }
             } catch (error) {
+                Honeybadger.notify(`Room description colon is not found in room index ${i}`);
                 console.error('Error inside room_title and room_description verification: ', error);
             }
 

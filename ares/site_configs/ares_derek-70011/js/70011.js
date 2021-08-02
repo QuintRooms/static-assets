4c7c6c1211a89d3600c375f2683504555c6420fd
Derek Butvin <derek.butvin@quintrooms.com>
2021-08-02 11:52:39 -0500

    Modal_Seacret - seacret modal files and 70011 updates

--- ares/site_configs/ares_derek-70011/js/70011.js
+++ ares/site_configs/ares_derek-70011/js/70011.js
@@ -1,6 +1,6 @@
 import BasePortal from '../../../js/build';
 import SiteConfig from './70011-config';
-import Modal from '../../../js/components/modal/modal';
+import ModalSeacret from '../../../js/components/modal_seacret/modal_seacret';
 
 const html_str = `
 <h4>Test</h4>
@@ -9,32 +9,31 @@ const html_str = `
 const modal_id = 'test-modal';
 
 const site_config = new SiteConfig();
-const test_modal = new Modal('Test Modal', html_str, modal_id, '#HotelNameContainer', 'Got it!');
+const test_modal = new ModalSeacret(modal_id);
 
 class ChildPortal extends BasePortal {
     constructor() {
         super(site_config);
         super.init();
-
-        this.useIconStarsFromNodes();
+        // this.useIconStarsFromNodes();
         test_modal.init();
     }
 
-    async useIconStarsFromNodes() {
-        // const star_boxes = await document.querySelectorAll('.lbl5stars', '.lbl4stars', '.lbl3stars', '.lbl2stars', '.lbl1stars');
-        const star_boxes = document.querySelector('.V5StarsBox').parentElement.children;
-        const star_boxes_arr = Array.from(star_boxes);
-        star_boxes_arr.shift();
-        const star_svg =
-            '<svg height="21" width="20" class="star rating" data-rating="1"><polygon points="9.9, 1.1, 3.3, 21.78, 19.8, 8.58, 0, 8.58, 16.5, 21.78" style="fill: #faaf18"/></svg>';
+    // async useIconStarsFromNodes() {
+    //     // const star_boxes = await document.querySelectorAll('.lbl5stars', '.lbl4stars', '.lbl3stars', '.lbl2stars', '.lbl1stars');
+    //     const star_boxes = document.querySelector('.V5StarsBox').parentElement.children;
+    //     const star_boxes_arr = Array.from(star_boxes);
+    //     star_boxes_arr.shift();
+    //     const star_svg =
+    //         '<svg height="21" width="20" class="star rating" data-rating="1"><polygon points="9.9, 1.1, 3.3, 21.78, 19.8, 8.58, 0, 8.58, 16.5, 21.78" style="fill: #faaf18"/></svg>';
 
-        star_boxes_arr.forEach((star_box) => {
-            const star_box_divs = star_box.children;
-            const star_div = star_box_divs[star_box_divs.length - 1];
-            const star_num = star_div.textContent.replace(/\D/g, '');
-            star_div.innerHTML = star_svg.repeat(star_num);
-        });
-    }
+    //     star_boxes_arr.forEach((star_box) => {
+    //         const star_box_divs = star_box.children;
+    //         const star_div = star_box_divs[star_box_divs.length - 1];
+    //         const star_num = star_div.textContent.replace(/\D/g, '');
+    //         star_div.innerHTML = star_svg.repeat(star_num);
+    //     });
+    // }
 
     // createTriggerElement() {
     //     const trigger_container = document.querySelectorAll(`originalPrice`);

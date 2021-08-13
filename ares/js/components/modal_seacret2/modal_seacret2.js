25474eb82b66d28958093cf7c0d7fecfd5049a79
Jason <jason.kaplan88@gmail.com>
2021-08-13 10:11:53 -0500

    Production build

--- ares/js/components/modal_seacret2/modal_seacret2.js
+++ ares/js/components/modal_seacret2/modal_seacret2.js
@@ -221,6 +221,7 @@ export default class ModalSeacret {
                 </div>
                 <hr class="solid">
                 <div class="modal-body-container">
+                    <h1>Adults</h1>
                     <div class="modal-body-text">
                         How many adults (ages 18+) will attend this trip?
                     </div>

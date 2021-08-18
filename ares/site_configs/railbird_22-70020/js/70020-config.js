7f9fd72d9da988073be63428f6c601c37e1e58b1
Derek Butvin <drockscott123@gmail.com>
2021-08-18 13:46:37 -0500

    Railbird 22 - Update master Id

--- ares/site_configs/railbird_22-70020/js/70020-config.js
+++ ares/site_configs/railbird_22-70020/js/70020-config.js
@@ -9,7 +9,7 @@ export default function siteConfig() {
         site_id,
         directory_name,
         affiliate_id: 16067,
-        master_id: 920,
+        master_id: 1816,
         site_type: 'lodging',
         algolia_app_id: 'plCZXR0GZ7J1',
         algolia_api_key: 'b9763a419845b59957b8cc5c9b13440c',

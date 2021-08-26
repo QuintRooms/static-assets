95d69e45e411b0f90cdb0acbe85c9e4c26f24887
Derek Butvin <drockscott123@gmail.com>
2021-08-26 11:25:17 -0500

    Music Midtown 21 - Add Hilton sponsor text

--- ares/site_configs/music_midtown_21-70013/js/70013.js
+++ ares/site_configs/music_midtown_21-70013/js/70013.js
@@ -1,5 +1,7 @@
 import BasePortal from '../../../js/build';
 import SiteConfig from './70013-config';
+import Utilities from '../../../js/utilities';
+const utilities = new Utilities();
 
 const site_config = new SiteConfig();
 class ChildPortal extends BasePortal {
@@ -10,3 +12,25 @@ class ChildPortal extends BasePortal {
 }
 
 new ChildPortal();
+
+//Insert Sponsor Mention
+const includeSponsorBanner = async () => {
+    await utilities.waitForSelectorInDOM('header');
+    document.querySelector('header').insertAdjacentHTML(
+    'beforeend',
+    `
+    <div class="sponsor-container">
+        <div class='sponsor-text'>
+            Proudly sponsored by
+        </div>
+        <div class='sponsor-text-space'></div>
+        <div class='sponsor-logo'>
+            <img src="https://dev-static.hotelsforhope.com/ares/site_configs/governors_ball_21-70366/img/logo-hilton.png" alt="Hilton logo">
+        </div>
+        
+    </div>
+    `
+    );
+}
+
+includeSponsorBanner();

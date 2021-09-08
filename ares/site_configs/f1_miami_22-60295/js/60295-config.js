ab7f17ac4bd4e066ddf9fcded4f824604f4eba75
Derek Butvin <drockscott123@gmail.com>
2021-09-08 11:30:55 -0500

    Miami GP 22 - prod - map marker hard rock, redirect to miamigp

--- ares/site_configs/f1_miami_22-60295/js/60295-config.js
+++ ares/site_configs/f1_miami_22-60295/js/60295-config.js
@@ -48,7 +48,7 @@ export default function siteConfig() {
         header: {
             logo_file_location: `${env_path.path}/site_configs/${directory_name}/img/logo.png`,
             email_logo_file_location: `${env_path.path}/site_configs/${directory_name}/img/email-logo.png`,
-            logo_outbound_url: 'https://bookrooms.formula1.com/v6?siteid=45246&theme=standard',
+            logo_outbound_url: 'https://f1miamigp.com/',
         },
         map_marker_image_url: `${env_path.path}/site_configs/${directory_name}/img/map_marker.png`,
         theme: 'light',

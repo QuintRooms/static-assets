48d95ca3c9fc3a178c9cf3ad272b898966bd91cc
Derek Butvin <drockscott123@gmail.com>
2021-09-14 11:38:41 -0500

    Miami GP 22 - dev - sort buttons test and emails update

--- ares/site_configs/f1_miami_22-60295/js/60295.js
+++ ares/site_configs/f1_miami_22-60295/js/60295.js
@@ -22,17 +22,102 @@ new Distance();
 
 async function insertFooterText() {
     await utilities.waitForSelectorInDOM('.f1-policy');
-
     const footer_policy = document.querySelector('.f1-policy');
 
     footer_policy.insertAdjacentHTML(
-    'beforebegin',
-    `
-    <img style="max-width: 175px; margin-top: 10px;" src="https://dev-static.hotelsforhope.com/ares/clients/formula_1/images/quintrooms-logo-footer.png" alt="Quintrooms Logo" />
-    <p class="f1-partner-text">Authorized Hotel Booking Partner of F1 Miami Grand Prix</p>
-    `
-);
-}
-
+        'beforebegin',
+        `
+        <img style="max-width: 175px; margin-top: 10px;" src="https://dev-static.hotelsforhope.com/ares/clients/formula_1/images/quintrooms-logo-footer.png" alt="Quintrooms Logo" />
+        <p class="f1-partner-text">Authorized Hotel Booking Partner of F1 Miami Grand Prix</p>
+        `
+    );
+};
 insertFooterText();
+
+async function setActiveItemFromLocalStorage() {
+    await utilities.waitForSelectorInDOM('.active');
+    const activeStorage = window.localStorage;
+    let activeClass = activeStorage.getItem('activeItem');
+    if(!activeClass) return;
+    const activeItemArr = document.querySelectorAll('.active');
+    activeItemArr.forEach((i)=> {
+        if(!i.classList.contains('ArnSortByAvailability')){
+            i.classList.remove('active');
+        }
+    });
+    console.log('activeClass',activeClass);
+    await utilities.waitForSelectorInDOM('.ArnSortByBeachfront');
+    let activeItem = document.querySelector(`.${activeClass}`);
+    activeItem.classList.add('active');
+    console.log('activeItem.classList', activeItem.classList)
+};
+setActiveItemFromLocalStorage();
+
+async function insertBeachSortButton() {
+    await utilities.waitForSelectorInDOM('.sort-wrapper');
+    const sort_wrapper = document.querySelector('.sort-wrapper');
+
+    sort_wrapper.insertAdjacentHTML(
+        'beforeEnd',
+        `
+        <a class="ArnSortByBeachfront Desc" href="#"><div>Beachfront Location</div></a>
+        `
+    );
+};
+insertBeachSortButton();
+
+// async function addListenerToBeachButton() {
+//     await utilities.waitForSelectorInDOM('.ArnSortByBeachfront');
+//     const beachSortButton = document.querySelector('.ArnSortByBeachfront');
+//     beachSortButton.addEventListener('click', (e) => {
+//         e.preventDefault();
+//         changeActiveSortItem('ArnSortByBeachfront')
+//     });
+// };
+// addListenerToBeachButton();
+
+async function addListenerToSortWrapper() {
+    await utilities.waitForSelectorInDOM('.sort-wrapper');
+    const sort_wrapper = document.querySelector('.sort-wrapper');
+    sort_wrapper.addEventListener('click', (e) => {
+        e.preventDefault();
+        const clickedSortATag = e.target.closest('a');
+        // console.log('clickedSortATag', clickedSortATag);
+
+        if (clickedSortATag.classList.contains('ArnSortByBeachfront')){
+            changeActiveSortItem('ArnSortByBeachfront');
+        } else if (clickedSortATag.classList.contains('ArnSortByDistance')) {
+            changeActiveSortItem('ArnSortByDistance');
+        } else if (clickedSortATag.classList.contains('ArnSortByPrice')) {
+            changeActiveSortItem('ArnSortByPrice');
+        } else if (clickedSortATag.classList.contains('ArnSortByClass')) {
+            changeActiveSortItem('ArnSortByClass');
+        }
+    });
+};
+addListenerToSortWrapper();
+
+async function changeActiveSortItem(newActiveClass) {
+    // console.log(e);
+    // console.log(e.target);
+    // e.preventDefault();
+    // if (e.target.querySelector('a>div')){
+    //     const clickedClass = e.target.querySelector('a>div').closest('a').classList;
+    // }
+    const clickedSortATag = document.querySelector(`.${newActiveClass}`);
+    const activeStorage = window.localStorage;
+    activeStorage.setItem('activeItem', newActiveClass);
+    const current_url = new URL(window.location.href);
+    if (newActiveClass === 'ArnSortByBeachfront') {
+        current_url.searchParams.set('properties', 
+        `338522,255939,240375,158522,2681,500825,179461,37003,259911,178994,20735,422794,252829,623490,3374,626284,848867,216347,24437,171408,332706,238362,380849,752193,33569,446028,24622,222338,430088,384604,10181,446322,256124,236435,16286,36053,35663,398338,273231,277689,450804,238764,10280,51206,175591,168273,233505,236708,174544,179936,16529,10116,2324,176956,24618,10117,130326,208368,182430,32656,43658,283572,271200,33831,24620,48449,716976,180475,34301,259007,20681,236428`
+        );
+        window.location.href = current_url;
+    } else {
+        // console.log('clickedSortATag', clickedSortATag);
+        const sortHref = clickedSortATag.href
+        // console.log('sortHref', sortHref)
+        window.location.href = sortHref;
+    }
+}
 

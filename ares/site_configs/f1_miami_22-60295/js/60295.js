ccc639bf0da4bd0b65bd95be60b5f5a7bf70dcb5
Derek Butvin <drockscott123@gmail.com>
2021-09-14 11:50:59 -0500

    Miami GP 22 - dev - sort buttons test 2

--- ares/site_configs/f1_miami_22-60295/js/60295.js
+++ ares/site_configs/f1_miami_22-60295/js/60295.js
@@ -60,21 +60,11 @@ async function insertBeachSortButton() {
     sort_wrapper.insertAdjacentHTML(
         'beforeEnd',
         `
-        <a class="ArnSortByBeachfront Desc" href="#"><div>Beachfront Location</div></a>
+        <a class="ArnSortByBeachfront Desc" href="https://events.quintrooms.com/v6?currency=USD&type=geo&siteid=60295&longitude=-80.23964600000000&latitude=25.95829850000000&radius=100&checkin=5/5/2022&nights=4&properties=338522,255939,240375,158522,2681,500825,179461,37003,259911,178994,20735,422794,252829,623490,3374,626284,848867,216347,24437,171408,332706,238362,380849,752193,33569,446028,24622,222338,430088,384604,10181,446322,256124,236435,16286,36053,35663,398338,273231,277689,450804,238764,10280,51206,175591,168273,233505,236708,174544,179936,16529,10116,2324,176956,24618,10117,130326,208368,182430,32656,43658,283572,271200,33831,24620,48449,716976,180475,34301,259007,20681,236428&map&locationlabel=Hard+Rock+Stadium&cid=GROUP-EVENT-EMAIL&pageSize=10&useMiles&groupid=50425&utm_source=internal"><div>Beachfront Location</div></a>
         `
     );
 };
 insertBeachSortButton();
-
-// async function addListenerToBeachButton() {
-//     await utilities.waitForSelectorInDOM('.ArnSortByBeachfront');
-//     const beachSortButton = document.querySelector('.ArnSortByBeachfront');
-//     beachSortButton.addEventListener('click', (e) => {
-//         e.preventDefault();
-//         changeActiveSortItem('ArnSortByBeachfront')
-//     });
-// };
-// addListenerToBeachButton();
 
 async function addListenerToSortWrapper() {
     await utilities.waitForSelectorInDOM('.sort-wrapper');
@@ -98,26 +88,41 @@ async function addListenerToSortWrapper() {
 addListenerToSortWrapper();
 
 async function changeActiveSortItem(newActiveClass) {
-    // console.log(e);
+    const clickedSortATag = document.querySelector(`.${newActiveClass}`);
+    const beachfrontUrl = `https://events.quintrooms.com/v6?currency=USD&type=geo&siteid=60295&longitude=-80.23964600000000&latitude=25.95829850000000&radius=100&checkin=5/5/2022&nights=4&properties=338522,255939,240375,158522,2681,500825,179461,37003,259911,178994,20735,422794,252829,623490,3374,626284,848867,216347,24437,171408,332706,238362,380849,752193,33569,446028,24622,222338,430088,384604,10181,446322,256124,236435,16286,36053,35663,398338,273231,277689,450804,238764,10280,51206,175591,168273,233505,236708,174544,179936,16529,10116,2324,176956,24618,10117,130326,208368,182430,32656,43658,283572,271200,33831,24620,48449,716976,180475,34301,259007,20681,236428&map&locationlabel=Hard+Rock+Stadium&cid=GROUP-EVENT-EMAIL&pageSize=10&useMiles&groupid=50425&utm_source=internal`
+    const activeStorage = window.localStorage;
+    activeStorage.setItem('activeItem', newActiveClass);
+    const sortHref = clickedSortATag.href
+    // console.log('sortHref', sortHref)
+    window.location.href = sortHref;
+
+    // const current_url = new URL(window.location.href);
+    // if (newActiveClass === 'ArnSortByBeachfront') {
+    //     current_url.searchParams.set('properties', 
+    //     `338522,255939,240375,158522,2681,500825,179461,37003,259911,178994,20735,422794,252829,623490,3374,626284,848867,216347,24437,171408,332706,238362,380849,752193,33569,446028,24622,222338,430088,384604,10181,446322,256124,236435,16286,36053,35663,398338,273231,277689,450804,238764,10280,51206,175591,168273,233505,236708,174544,179936,16529,10116,2324,176956,24618,10117,130326,208368,182430,32656,43658,283572,271200,33831,24620,48449,716976,180475,34301,259007,20681,236428`
+    //     );
+    //     window.location.href = beachfrontUrl;
+    // } else {
+    //     // console.log('clickedSortATag', clickedSortATag);
+    //     const sortHref = clickedSortATag.href
+    //     // console.log('sortHref', sortHref)
+    //     window.location.href = sortHref;
+    // }
+}
+
+// console.log(e);
     // console.log(e.target);
     // e.preventDefault();
     // if (e.target.querySelector('a>div')){
     //     const clickedClass = e.target.querySelector('a>div').closest('a').classList;
     // }
-    const clickedSortATag = document.querySelector(`.${newActiveClass}`);
-    const activeStorage = window.localStorage;
-    activeStorage.setItem('activeItem', newActiveClass);
-    const current_url = new URL(window.location.href);
-    if (newActiveClass === 'ArnSortByBeachfront') {
-        current_url.searchParams.set('properties', 
-        `338522,255939,240375,158522,2681,500825,179461,37003,259911,178994,20735,422794,252829,623490,3374,626284,848867,216347,24437,171408,332706,238362,380849,752193,33569,446028,24622,222338,430088,384604,10181,446322,256124,236435,16286,36053,35663,398338,273231,277689,450804,238764,10280,51206,175591,168273,233505,236708,174544,179936,16529,10116,2324,176956,24618,10117,130326,208368,182430,32656,43658,283572,271200,33831,24620,48449,716976,180475,34301,259007,20681,236428`
-        );
-        window.location.href = current_url;
-    } else {
-        // console.log('clickedSortATag', clickedSortATag);
-        const sortHref = clickedSortATag.href
-        // console.log('sortHref', sortHref)
-        window.location.href = sortHref;
-    }
-}
 
+// async function addListenerToBeachButton() {
+//     await utilities.waitForSelectorInDOM('.ArnSortByBeachfront');
+//     const beachSortButton = document.querySelector('.ArnSortByBeachfront');
+//     beachSortButton.addEventListener('click', (e) => {
+//         e.preventDefault();
+//         changeActiveSortItem('ArnSortByBeachfront')
+//     });
+// };
+// addListenerToBeachButton();

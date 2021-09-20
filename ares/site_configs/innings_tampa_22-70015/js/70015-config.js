215544929bfe2df47f2f5ac32fc75f5de83ed116
Derek Butvin <drockscott123@gmail.com>
2021-09-20 12:39:12 -0500

    Innings Fest - Tempe & Tampa - initial prod build

--- ares/site_configs/innings_tampa_22-70015/js/70015-config.js
+++ ares/site_configs/innings_tampa_22-70015/js/70015-config.js
@@ -70,5 +70,11 @@ export default function siteConfig() {
         root_page_header_text: 'Start Your Search',
         root_page_subheader_text: 'Reservations at 600K+ Hotels at Unbeatable Rates',
         is_resbeat_client: false,
+        confirmation_email_from: 'Quint Rooms',
+        has_group_booking_banner: true,
+        group_booking_form_url: 'https://form.jotform.com/203066540331141?bookingPortal=Innings%20Festival',
+        has_landing_page: false,
+        has_custom_emails: false,
+        hide_search_on_landing_page: true,
     };
 }

af21cf38702b2a1e6c12aeafa5eb24c6d11c8331
Derek Butvin <drockscott123@gmail.com>
2021-09-21 13:59:37 -0500

    BJ Landing - including Scottsdale 22

--- ares/site_configs/bj_landing_page-58196/js/58196-config.js
+++ ares/site_configs/bj_landing_page-58196/js/58196-config.js
@@ -75,30 +75,12 @@ export default function siteConfig() {
         has_landing_page: true,
         hide_search_on_landing_page: true,
         landing_page_events: [
-            // {
-            //     name: 'Fall Auction 2020 Scottsdale',
-            //     display_date: 'October 22 - 24, 2020',
-            //     end_date: '10/25/2020', // M/D/YYYY
-            //     portal_url: 'https://events.hotelsforhope.com/group-event?id=43435',
-            // },
             {
-                name: '2021 Scottsdale Auction',
-                display_date: 'March 20 - 27, 2021',
-                end_date: '3/28/2021', // M/D/YYYY
-                portal_url: `https://events.${process.env.domain}/group-event?id=46530&utm_campaign=Barrett-Jackson%20Scottsdale%202021%20Rescheduled`,
+                name: '2022 Scottsdale Auction',
+                display_date: 'January 22 - 26, 2022',
+                end_date: '1/27/2020', // M/D/YYYY
+                portal_url: 'https://events.quintrooms.com/group-event?id=50694',
             },
-            // {
-            //     name: '2021 West Palm Beach Auction',
-            //     display_date: 'April 08 - 10, 2021',
-            //     end_date: '04/12/2021', // M/D/YYYY
-            //     portal_url: `http://events.${process.env.domain}/group-event?id=44704&utm_campaign=Barrett-Jackson%20West%20Palm%20Beach%202021`,
-            // // },
-            // {
-            //     name: '2021 Las Vegas Auction',
-            //     display_date: 'June 17 - 19, 2021',
-            //     end_date: '06/22/2021', // M/D/YYYY
-            //     portal_url: `https://events.${process.env.domain}/group-event?id=47795&utm_campaign=Barrett-Jackson%20Las%20Vegas%202021`,
-            // },
             {
                 name: '2021 Houston Auction',
                 display_date: 'September 16 - 18, 2021',

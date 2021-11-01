a44718490b68acc6019f11d6810cfc1b68f3594e
Derek Butvin <derek.butvin@quintrooms.com>
2021-11-01 17:35:42 -0500

    CFP landing- further styling changes to lucid banner and buttons

--- ares/site_configs/cfp_landing_page_22-62631/js/62631.js
+++ ares/site_configs/cfp_landing_page_22-62631/js/62631.js
@@ -45,6 +45,7 @@ async function renderLucidBanner() {
 renderLucidBanner();
 
 async function changeButtonText() {
+    await utilities.waitForSelectorInDOM('.view-hotels');
     const view_hotels_buttons = document.querySelectorAll('.view-hotels');
     view_hotels_buttons[1].innerHTML = 'SUBMIT REQUESTS';
     view_hotels_buttons[2].innerHTML = 'SUBMIT REQUESTS';

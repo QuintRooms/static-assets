086cd6fab251dbb8f4af0875b502eb7dac521ebf
Derek Butvin <derek.butvin@quintrooms.com>
2021-11-01 13:55:50 -0500

    CFP landing page - update to include championship waitlists

--- ares/site_configs/cfp_landing_page_22-62631/js/62631.js
+++ ares/site_configs/cfp_landing_page_22-62631/js/62631.js
@@ -17,11 +17,38 @@ class ChildPortal extends BasePortal {
 new LandingPage(site_config.landing_page_events, site_config.hide_search_on_landing_page).init();
 new ChildPortal();
 
-async function addBrs() {
-    if (!document.querySelector('.RootBody')) return;
-    await utilities.waitForSelectorInDOM('.event-name');
-    document.querySelector('.event-1 .event-name').innerHTML = '<h2 class="event-name">SEARCH <br> CFP NATIONAL CHAMPIONSHIP INDIANAPOLIS 2022</h2>';
-    document.querySelector('.event-2 .event-name').innerHTML = '<h2 class="event-name">SEARCH <br> ANYWHERE</h2>';
+async function renderLucidBanner() {
+    await utilities.waitForSelectorInDOM('.events');
+
+    document.querySelector('.events').insertAdjacentHTML(
+        'afterend',
+        `
+        <div class="lucid-banner">
+            <div class="lucid-content">
+                <span id="desktop">Need 10+ rooms for any of the CFP Championship games? We're here to help you secure great rates.</span>     
+                <span id="tablet">Need 10+ rooms for the CFP Championship games?</span>
+                <span id="mobile">Need 10+ rooms?</span>
+            </div>
+            <div class="lucid-button">
+                <a id="lucid-link" target="_blank" href="https://form.jotform.com/203066540331141?bookingPortal=CFP%20Championship">Book 10+ Rooms</a>
+            </div>
+        </div>
+        <style>
+        header {
+            border-bottom: unset !important;
+        }
+        </style>
+        `
+    );
 }
 
-addBrs();
+renderLucidBanner();
+
+// async function addBrs() {
+//     if (!document.querySelector('.RootBody')) return;
+//     await utilities.waitForSelectorInDOM('.event-name');
+//     document.querySelector('.event-1 .event-name').innerHTML = '<h2 class="event-name">SEARCH <br> CFP NATIONAL CHAMPIONSHIP INDIANAPOLIS 2022</h2>';
+//     document.querySelector('.event-2 .event-name').innerHTML = '<h2 class="event-name">SEARCH <br> ANYWHERE</h2>';
+// }
+
+// addBrs();

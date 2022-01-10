c39b7d806c07b97325ed2b76fbfbc6986c7bf696
Derek Butvin <drockscott123@gmail.com>
2022-01-10 17:42:41 -0600

    STOX SA 22 - styles update

---
+++ ares/src/68890.js
@@ -0,0 +1,8 @@
+import '../site_configs/sotx_summer_games_san_antonio_22-68890/js/68890';
+import '../site_configs/sotx_summer_games_san_antonio_22-68890/styles/68890.scss';
+import '../site_configs/sotx_summer_games_san_antonio_22-68890/js/68890';
+import '../site_configs/sotx_summer_games_san_antonio_22-68890/styles/68890.scss';
+import '../site_configs/sotx_summer_games_san_antonio_22-68890/js/68890';
+import '../site_configs/sotx_summer_games_san_antonio_22-68890/styles/68890.scss';
+import '../site_configs/sotx_summer_games_san_antonio_22-68890/js/68890';
+import '../site_configs/sotx_summer_games_san_antonio_22-68890/styles/68890.scss';

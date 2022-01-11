7e87504343161894cccbf1a518a6447d3f22f7b0
Derek Butvin <drockscott123@gmail.com>
2022-01-11 12:34:16 -0600

    Forecastle 22 - Update email logo and setup

--- ares/site_configs/forecastle_22-70018/js/70018-config.js
+++ ares/site_configs/forecastle_22-70018/js/70018-config.js
@@ -44,6 +44,7 @@ export default function siteConfig() {
         fav_icon_url: `${env_path.path}/site_configs/${directory_name}/img/favicon.png`,
         header: {
             logo_file_location: `${env_path.path}/site_configs/${directory_name}/img/logo.png`,
+            email_logo_file_location: `${env_path.path}/site_configs/${directory_name}/img/email-logo.png`,
             logo_outbound_url: 'https://www.forecastlefest.com/',
         },
         map_marker_image_url: `${env_path.path}/site_configs/${directory_name}/img/favicon.png`,

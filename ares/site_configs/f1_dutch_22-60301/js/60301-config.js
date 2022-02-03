3e0030e0d92d567712c992cdeca33f734e8f2fbf
Derek Butvin <drockscott123@gmail.com>
2022-02-03 11:34:56 -0600

    dutch 22 - change email logo

--- ares/site_configs/f1_dutch_22-60301/js/60301-config.js
+++ ares/site_configs/f1_dutch_22-60301/js/60301-config.js
@@ -46,7 +46,7 @@ export default function siteConfig() {
         fav_icon_url: `${env_path.path}/clients/formula_1/images/favicon.png`,
         header: {
             logo_file_location: `${env_path.path}/site_configs/${directory_name}/img/logo.png`,
-            email_logo_file_location: `${env_path.path}/site_configs/${directory_name}/img/email-logo.png`,
+            email_logo_file_location: `${env_path.path}/site_configs/${directory_name}/img/email-logo.jpg`,
             logo_outbound_url: 'https://dutchgp.com/',
         },
         map_marker_image_url: `${env_path.path}/clients/formula_1/images/favicon.png`,

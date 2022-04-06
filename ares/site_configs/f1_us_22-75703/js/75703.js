7b7da0980f0cc16f05347610c202089e501d6d58
Derek Butvin <drockscott123@gmail.com>
2022-04-06 13:00:45 -0500

    F1 USGP - Initial Dev/ prod build

---
+++ ares/site_configs/f1_us_22-75703/js/75703.js
@@ -0,0 +1,21 @@
+import BasePortal from '../../../js/build';
+import SiteConfig from './75703-config';
+import f1Styles from '../../../clients/formula_1/js/f1';
+import Distance from '../../../js/calculate-distance';
+import Utilities from '../../../js/utilities';
+
+const utilities = new Utilities();
+const site_config = new SiteConfig();
+
+class ChildPortal extends BasePortal {
+    constructor() {
+        super(site_config);
+        super.init();
+    }
+}
+
+f1Styles(site_config.site_id, site_config.lodging.event_name, 'Circuit Zandvoort', '#333');
+utilities.replacePropThumbImage(site_config.prop_image_replacement);
+
+new ChildPortal();
+new Distance();
---
+++ ares/src/75703.js
@@ -0,0 +1,2 @@
+import '../site_configs/f1_us_22-75703/js/75703';
+import '../site_configs/f1_us_22-75703/styles/75703.scss';

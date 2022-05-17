d14c7b2e6271e33d306e2c8eb9a5bbd5aba44e7a
Derek Butvin <drockscott123@gmail.com>
2022-05-17 12:16:53 -0500

    Railbird production 23 - complete rebuild with new name

--- ares/src/71773.js
+++ ares/src/71773.js
@@ -1,2 +1,2 @@
-import '../site_configs/railbird_23_prod-71773/js/71773';
-import '../site_configs/railbird_23_prod-71773/styles/71773.scss';
+import '../site_configs/railbird_production_23-71773/js/71773';
+import '../site_configs/railbird_production_23-71773/styles/71773.scss';

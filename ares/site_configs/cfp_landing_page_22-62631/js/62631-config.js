61bbc537fa1b5774354889c1b34128b37a6d33fe
Derek Butvin <drockscott123@gmail.com>
2022-08-18 10:35:02 -0500

    cfp landing page - update la, add atlanta

--- ares/site_configs/cfp_landing_page_22-62631/js/62631-config.js
+++ ares/site_configs/cfp_landing_page_22-62631/js/62631-config.js
@@ -99,13 +99,19 @@ export default function siteConfig() {
                 name: 'CFP NATIONAL CHAMPIONSHIP LOS ANGELES 2023',
                 display_date: 'January 6 - 10, 2023',
                 end_date: '01/10/2023', // M/D/YYYY
-                portal_url: 'https://form.jotform.com/212695796550064',
+                portal_url: 'https://collegefootballplayoffhotels.com/group-event?id=57102',
             },
             {
                 name: 'CFP NATIONAL CHAMPIONSHIP HOUSTON 2024',
                 display_date: 'January 5 - 9, 2024',
                 end_date: '01/09/2024', // M/D/YYYY
                 portal_url: 'https://form.jotform.com/212986027619161',
+            },
+            {
+                name: 'CFP NATIONAL CHAMPIONSHIP ATLANTA 2025',
+                display_date: 'TBD',
+                end_date: '01/09/2025', // M/D/YYYY
+                portal_url: 'https://form.jotform.com/222285410763150',
             },
         ],
     };

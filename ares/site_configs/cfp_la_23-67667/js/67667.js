1a270f77b8e2cdbb3104b728a557720708232cee
Derek Butvin <drockscott123@gmail.com>
2022-08-19 13:27:45 -0500

    CFP la 22 - define utilities variable

--- ares/site_configs/cfp_la_23-67667/js/67667.js
+++ ares/site_configs/cfp_la_23-67667/js/67667.js
@@ -2,6 +2,7 @@ import BasePortal from '../../../js/build';
 import Utilities from '../../../js/utilities';
 import SiteConfig from './67667-config';
 
+const utilities = new Utilities();
 const site_config = new SiteConfig();
 class ChildPortal extends BasePortal {
     constructor() {

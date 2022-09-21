40af139ab8016936000dc41defcb6c37c37c1f0c
Derek Butvin <drockscott123@gmail.com>
2022-09-21 11:59:49 -0500

    BJ Scottsdale 23 - builds

---
+++ ares/src/83130.js
@@ -0,0 +1,4 @@
+import '../site_configs/bj_scottsdale_23-83130/js/83130';
+import '../site_configs/bj_scottsdale_23-83130/styles/83130.scss';
+import '../site_configs/bj_scottsdale_23-83130/js/83130';
+import '../site_configs/bj_scottsdale_23-83130/styles/83130.scss';

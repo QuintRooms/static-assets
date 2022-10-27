79006a933cbdb7cccb4f9e5a850af4592e4cb1d2
Derek Butvin <drockscott123@gmail.com>
2022-10-27 15:30:17 -0500

    initial build

---
+++ ares/site_configs/two_step_inn_festival_23-84537/js/84537.js
@@ -0,0 +1,50 @@
+import BasePortal from '../../../js/build';
+import Utilities from '../../../js/utilities';
+import SiteConfig from './84537-config';
+
+const utilities = new Utilities();
+const site_config = new SiteConfig();
+
+class ChildPortal extends BasePortal {
+    constructor() {
+        super(site_config);
+        super.init();
+    }
+}
+
+new ChildPortal();
+
+// const props = [
+//     33602,
+//     24968,
+//     20578,
+//     3339,
+//     20420,
+//     345484,
+//     30128,
+//     35521,
+//     182581,
+//     39143,
+//     1285879,
+//     21014,
+//     723662,
+//     1286319,
+//     656189,
+//     607674,
+//     177947,
+//     642874,
+//     8863,
+//     21010,
+//     35949,
+//     626122,
+//     35487,
+//     3391313,
+//     3327060,
+//     34049,
+//     14860,
+//     208598,
+//     3388,
+//     614462,
+// ];
+
+// utilities.ratesComingSoon(props);

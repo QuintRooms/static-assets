294b5c2eb3b103d99b74fc4a05d0fd70fbdb9466
Derek Butvin <drockscott123@gmail.com>
2023-01-25 12:27:32 -0600

    Ohana 23 - build

---
+++ ares/site_configs/ohana_festival_23-84337/js/84337.js
@@ -0,0 +1,37 @@
+import BasePortal from '../../../js/build';
+import SiteConfig from './84337-config';
+import Utilities from '../../../js/utilities';
+
+const utilities = new Utilities();
+
+const site_config = new SiteConfig();
+
+class ChildPortal extends BasePortal {
+    constructor() {
+        super(site_config);
+        super.init();
+    }
+}
+
+new ChildPortal();
+
+// Insert Sponsor Banner
+const include_sponsor_banner = async () => {
+    await utilities.waitForSelectorInDOM('header');
+    document.querySelector('header').insertAdjacentHTML(
+        'beforeend',
+        `
+    <div class="sponsor-container">
+        <div class='sponsor-text'>
+            Proudly sponsored by
+        </div>
+        <div class='sponsor-text-space'></div>
+        <div class='sponsor-logo'>
+            <img src="https://dev-static.hotelsforhope.com/ares/site_configs/ohana_festival_22-77149/img/logo-ihg.png" alt="IHG logo">
+        </div>
+    </div>
+    `
+    );
+};
+
+include_sponsor_banner();
---
+++ ares/src/84337.js
@@ -0,0 +1,2 @@
+import '../site_configs/ohana_festival_23-84337/js/84337';
+import '../site_configs/ohana_festival_23-84337/styles/84337.scss';

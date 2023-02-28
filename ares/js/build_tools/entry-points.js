6da8e9d5f96d09e5959f6a2a48652ea76f93191f
Derek Butvin <derek.butvin@quintrooms.com>
2023-02-28 14:14:07 -0600

    Fairwell Fest 23 - build

--- ares/js/build_tools/entry-points.js
+++ ares/js/build_tools/entry-points.js
@@ -16,7 +16,7 @@ module.exports = () => {
             // 'bj_scottsdale_23-83130': './src/83130.js', // bj scottsdale_23
             // 'bj_west_palm_beach_23-82620': './src/82620.js', // bj west_palm_beach_23
             // 'bonnaroo_23-78102': './src/78102.js', // bonnaroo 23
-            'breeders_23-88324': './src/88324.js', // breeders 23
+            // 'breeders_23-88324': './src/88324.js', // breeders 23
             // 'catbird_23-88335': './src/88335.js', // catbird 23
             // 'cfp_la_23-67667': './src/67667.js', // College football Playoff Championship LA 2023
             // 'cfp_la_internal_23-67668': './src/67668.js', // cfp la_internal_23
@@ -25,6 +25,7 @@ module.exports = () => {
             // 'ares_derek-70011': './src/70011.js', // ares derek
             // 'echoland_23-85734': './src/85734.js', // echoland 23
             // 'esap_23-83333': './src/83333.js', // esap 23
+            'fairwell_festival_23-86604': './src/86604.js', // fairwell festival_23
             // 'farm_aid_23-87656': './src/87656.js', // farm aid_23
             // 'f1_canada_gp_23-81399': './src/81399.js', // f1 canada_gp_23
             // 'f1_dutch_23-83219': './src/83219.js', // f1 dutch_23
@@ -104,13 +105,15 @@ module.exports = () => {
             // 'rally_for_resilience_23-87989': './src/87989.js', // rally for_resilience_23
             // 'innings_tempe_24-88139': './src/88139.js', // innings tempe_24
             // 'catbird_23-88335': './src/88335.js', // catbird 23
-            'breeders_23-88324': './src/88324.js', // breeders 23
+            // 'breeders_23-88324': './src/88324.js', // breeders 23
+            'fairwell_festival_23-86604': './src/86604.js', // fairwell festival_23
         };
 
         // local environment
     } else if (process.env.NODE_ENV === 'local') {
         entry_points = {
             // new sites populate below
+            'fairwell_festival_23-86604': './src/86604.js', // fairwell festival_23
             'breeders_23-88324': './src/88324.js', // breeders 23
             'catbird_23-88335': './src/88335.js', // catbird 23
             // 'innings_tempe_24-88139': './src/88139.js', // innings tempe_24
@@ -123,87 +126,87 @@ module.exports = () => {
 
 // OLD PORTALS
 // 'acl_21-60311': './src/60311.js', // Austin City Limits Festival (ACL) 2021
- // 'acl_21_production-60310': './src/60310.js', // acl 21_production
+// 'acl_21_production-60310': './src/60310.js', // acl 21_production
 // 'acl_22_attendees-71761': './src/71761.js', // acl 22_attendees
 // 'arizona_bike_week_21-64812': './src/64812.js', // Arizona Bike week 2021
 // 'arizona_bike_week_22-66931': './src/66931.js', // Arizona Bike Week 2022
 // 'austin_food_and_wine_21-60305': './src/60305.js', // Austin Food And Wine 2021
 // 'austin_food_and_wine_22-71764': './src/71764.js', // austin food_and_wine_22
-            // 'besame_22-71776': './src/71776.js', // besame 22
-            // 'bj_houston_21-64191': './src/64191.js', // Barrett-Jackson Houston 2021
-            // 'bj_houston_22-73526': './src/73526.js', // bj houston_22
-            // 'bj_las_vegas_22-73525': './src/73525.js', // bj las_vegas_22
-            // 'bj_scottsdale_22-68720': './src/68720.js', // bj scottsdale_22
-            // 'bj_west_palm_beach_21-64187': './src/64187.js', // Barrett-Jackson West Palm Beach 2021
-            // 'bj_west_palm_beach_22-68721': './src/68721.js', // bj west_palm_beach_22
-            // 'bonnaroo_22-70019': './src/70019.js', // Bonnaroo 22
-            // 'breeders_cup_21-70372': './src/70372.js', // Breeders Cup 2021
-            // 'breeders_cup_22-77470': './src/77470.js', // breeders cup_22
-            // 'buku_22-66047': './src/66047.js', // buku 22
-            // 'cfp_21-60797': './src/60797.js', // College football Playoff Championship 2021
-            // 'cfp_indianapolis_22-62632': './src/62632.js', // College football Playoff Championship Indianapolis 2022
-            // 'cfp_indianapolis_internal_22-67666': './src/67666.js', // cfp indianapolis_internal_22
-            // 'farm_aid_21-70422': './src/70422.js', // Farm Aid 21
-            // 'farm_aid_22-78568': './src/78568.js', // farm aid_22
-            // 'farm_aid_internal_22-78569': './src/78569.js', // farm aid_internal_22
-            // 'f1_dutch_21-65003': './src/65003.js', // Formula 1 Dutch 2021
-            // 'f1_dutch_22-60301': './src/60301.js', // f1 dutch_22
-            // 'f1_dutch_promoters_22-75705': './src/75705.js', // f1 dutch_promoters_22
-            // 'f1_miami_22-60295': './src/60295.js', // Formula 1 Miami Grand Prix 2022
-            // 'f1_montreal_22-60296': './src/60296.js', // Formula 1 Montreal Grand Prix 2022
-            // 'f1_us_21-65006': './src/65006.js', // Formula 1 United States Grand Prix 2021
-            // 'f1_turkey_21-70474': './src/70474.js', // Formula 1 Turkish Grand Prix 2021
-            // 'f1_us_22-75703': './src/75703.js', // f1 us_22
-            // 'firefly_21-64866': './src/64866.js', // firefly 21
-            // 'forecastle_22-70018': './src/70018.js', // forecastle 22
-            // 'format_festival_22-68383': './src/68383.js', // format festival_22
-            // 'format_festival__prod_22-68384': './src/68384.js', // format festival__prod_22
-            // 'fourleaf_versa_22-73546': './src/73546.js', // fourleaf versa_22
-            // 'governors_ball_21-70366': './src/70366.js', // governors ball_21
-            // 'governors_ball_22-77491': './src/77491.js', // governors ball_22
-            // 'hof_enshrinement_attendees_22-74241': './src/74241.js', // hof enshrinement_attendees_22
-            // 'hof_enshrinement_friends_fam_22-74242': './src/74242.js', // hof enshrinement_friends_fam_22
-            // 'hof_enshrinement_internal_22-74240': './src/74240.js', // hof enshrinement_internal_22
-            // 'hof_marathon_21-60871': './src/60871.js', // Hall Of Fame Marathon 2021
-            // 'innings_tempe_22-70017': './src/70017.js', // innings tempe_22
-            // 'innings_tampa_22-70015': './src/70015.js', // innings tampa_22
-            // 'lib_21-63504': './src/63504.js', // Life Is Beautiful Festival 2021
-            // 'liv_golf_pumpkin_ridge_22-78856': './src/78856.js', // liv golf_pumpkin_ridge_22
-            // 'lolla_21-60828': './src/60828.js', // Lollapalooza 2021
-            // 'lolla_22-62635': './src/62635.js', // lolla 22
-            // 'lolla_22_prod-62636': './src/62636.js', // lolla 22_prod
-            // 'high_water_22-71767': './src/71767.js', // high water_22
-            // 'music_midtown_21-70012': './src/70012.js', // Music Midtown Production 2021
-            // 'music_midtown_21-70013': './src/70013.js', // Music Midtown Attendees 2021
-            // 'music_midtown_22-71768': './src/71768.js', // music midtown_22
-            // 'music_midtown_prod_22-71769': './src/71769.js', // music midtown_prod_22
-            // 'moon_river_festival_21-68381': './src/68381.js', // moon river_festival_21
-            // 'moon_river_22-70021': './src/70021.js', // moon river_22
-            // 'miami_cosmetic_surgery_22-78429': './src/78429.js', // miami cosmetic_surgery_22
-            // 'oceans_calling_22-78095': './src/78095.js', // oceans calling_22
-            // 'ohana_festival_22-77149': './src/77149.js', // ohana festival_22
-            // 'project_glow_22-76307': './src/76307.js', // project glow_22
-            // 'railbird_22-70020': './src/70020.js', // railbird 22
-            // 'sea_hear_now_21-70014': './src/70014.js', // Sea Hear Now 21
-            // 'sea_hear_now_22-71759': './src/71759.js', // sea hear_now_22
-            // 'shaky_knees_21-68379': './src/68379.js', // Shaky Knees 2021
-            // 'shaky_knees_22-70057': './src/70057.js', // Shaky Knees 2022
-            // 'sial_canada_22-75294': './src/75294.js', // sea hear_now_22
-            // 'socal_bike_fest_21-70903': './src/70903.js', // Socal Bike Fest 2021
-            // 'sotx_equestrain_22-79448': './src/79448.js', // sotx equestrain_22
-            // 'sotx_fall_classic_22-79449': './src/79449.js', // sotx fall_classic_22
-            // 'sotx_summer_games_san_antonio_22-68890': './src/68890.js', // sotx summer_games_san_antonio_22
-            // 'sueno_22-71766': './src/71766.js', // sueno 22
-            // 'texas_book_festival_21-69571': './src/69571.js', // texas book_festival 2021
-            // 'texas_book_festival_22-69574': './src/69574.js', // texas book_festival_22
-            // 'hof_hbcu_classic_21-62682': './src/62682.js', // Hall Of Fame HBCU Classic
-            // 'isle_tt_22-78066': './src/78066.js', // isle tt_22
-            // 'tidal_wave_22-74016': './src/74016.js', // tidal wave_22
-            // 'tidal_wave_public_22-76306': './src/76306.js', // tidal wave_public_22
-            // 'tortuga_21-62309': './src/62309.js', // Tortuga 2021
-            // 'tortuga_22-74015': './src/74015.js', // tortuga 22
-            // 'voodoo_22-70452': './src/70452.js', // Voodoo Festival Attendees 2022
-            // 'voodoo_22-70453': './src/70453.js', // Voodoo Festival Production 2022
+// 'besame_22-71776': './src/71776.js', // besame 22
+// 'bj_houston_21-64191': './src/64191.js', // Barrett-Jackson Houston 2021
+// 'bj_houston_22-73526': './src/73526.js', // bj houston_22
+// 'bj_las_vegas_22-73525': './src/73525.js', // bj las_vegas_22
+// 'bj_scottsdale_22-68720': './src/68720.js', // bj scottsdale_22
+// 'bj_west_palm_beach_21-64187': './src/64187.js', // Barrett-Jackson West Palm Beach 2021
+// 'bj_west_palm_beach_22-68721': './src/68721.js', // bj west_palm_beach_22
+// 'bonnaroo_22-70019': './src/70019.js', // Bonnaroo 22
+// 'breeders_cup_21-70372': './src/70372.js', // Breeders Cup 2021
+// 'breeders_cup_22-77470': './src/77470.js', // breeders cup_22
+// 'buku_22-66047': './src/66047.js', // buku 22
+// 'cfp_21-60797': './src/60797.js', // College football Playoff Championship 2021
+// 'cfp_indianapolis_22-62632': './src/62632.js', // College football Playoff Championship Indianapolis 2022
+// 'cfp_indianapolis_internal_22-67666': './src/67666.js', // cfp indianapolis_internal_22
+// 'farm_aid_21-70422': './src/70422.js', // Farm Aid 21
+// 'farm_aid_22-78568': './src/78568.js', // farm aid_22
+// 'farm_aid_internal_22-78569': './src/78569.js', // farm aid_internal_22
+// 'f1_dutch_21-65003': './src/65003.js', // Formula 1 Dutch 2021
+// 'f1_dutch_22-60301': './src/60301.js', // f1 dutch_22
+// 'f1_dutch_promoters_22-75705': './src/75705.js', // f1 dutch_promoters_22
+// 'f1_miami_22-60295': './src/60295.js', // Formula 1 Miami Grand Prix 2022
+// 'f1_montreal_22-60296': './src/60296.js', // Formula 1 Montreal Grand Prix 2022
+// 'f1_us_21-65006': './src/65006.js', // Formula 1 United States Grand Prix 2021
+// 'f1_turkey_21-70474': './src/70474.js', // Formula 1 Turkish Grand Prix 2021
+// 'f1_us_22-75703': './src/75703.js', // f1 us_22
+// 'firefly_21-64866': './src/64866.js', // firefly 21
+// 'forecastle_22-70018': './src/70018.js', // forecastle 22
+// 'format_festival_22-68383': './src/68383.js', // format festival_22
+// 'format_festival__prod_22-68384': './src/68384.js', // format festival__prod_22
+// 'fourleaf_versa_22-73546': './src/73546.js', // fourleaf versa_22
+// 'governors_ball_21-70366': './src/70366.js', // governors ball_21
+// 'governors_ball_22-77491': './src/77491.js', // governors ball_22
+// 'hof_enshrinement_attendees_22-74241': './src/74241.js', // hof enshrinement_attendees_22
+// 'hof_enshrinement_friends_fam_22-74242': './src/74242.js', // hof enshrinement_friends_fam_22
+// 'hof_enshrinement_internal_22-74240': './src/74240.js', // hof enshrinement_internal_22
+// 'hof_marathon_21-60871': './src/60871.js', // Hall Of Fame Marathon 2021
+// 'innings_tempe_22-70017': './src/70017.js', // innings tempe_22
+// 'innings_tampa_22-70015': './src/70015.js', // innings tampa_22
+// 'lib_21-63504': './src/63504.js', // Life Is Beautiful Festival 2021
+// 'liv_golf_pumpkin_ridge_22-78856': './src/78856.js', // liv golf_pumpkin_ridge_22
+// 'lolla_21-60828': './src/60828.js', // Lollapalooza 2021
+// 'lolla_22-62635': './src/62635.js', // lolla 22
+// 'lolla_22_prod-62636': './src/62636.js', // lolla 22_prod
+// 'high_water_22-71767': './src/71767.js', // high water_22
+// 'music_midtown_21-70012': './src/70012.js', // Music Midtown Production 2021
+// 'music_midtown_21-70013': './src/70013.js', // Music Midtown Attendees 2021
+// 'music_midtown_22-71768': './src/71768.js', // music midtown_22
+// 'music_midtown_prod_22-71769': './src/71769.js', // music midtown_prod_22
+// 'moon_river_festival_21-68381': './src/68381.js', // moon river_festival_21
+// 'moon_river_22-70021': './src/70021.js', // moon river_22
+// 'miami_cosmetic_surgery_22-78429': './src/78429.js', // miami cosmetic_surgery_22
+// 'oceans_calling_22-78095': './src/78095.js', // oceans calling_22
+// 'ohana_festival_22-77149': './src/77149.js', // ohana festival_22
+// 'project_glow_22-76307': './src/76307.js', // project glow_22
+// 'railbird_22-70020': './src/70020.js', // railbird 22
+// 'sea_hear_now_21-70014': './src/70014.js', // Sea Hear Now 21
+// 'sea_hear_now_22-71759': './src/71759.js', // sea hear_now_22
+// 'shaky_knees_21-68379': './src/68379.js', // Shaky Knees 2021
+// 'shaky_knees_22-70057': './src/70057.js', // Shaky Knees 2022
+// 'sial_canada_22-75294': './src/75294.js', // sea hear_now_22
+// 'socal_bike_fest_21-70903': './src/70903.js', // Socal Bike Fest 2021
+// 'sotx_equestrain_22-79448': './src/79448.js', // sotx equestrain_22
+// 'sotx_fall_classic_22-79449': './src/79449.js', // sotx fall_classic_22
+// 'sotx_summer_games_san_antonio_22-68890': './src/68890.js', // sotx summer_games_san_antonio_22
+// 'sueno_22-71766': './src/71766.js', // sueno 22
+// 'texas_book_festival_21-69571': './src/69571.js', // texas book_festival 2021
+// 'texas_book_festival_22-69574': './src/69574.js', // texas book_festival_22
+// 'hof_hbcu_classic_21-62682': './src/62682.js', // Hall Of Fame HBCU Classic
+// 'isle_tt_22-78066': './src/78066.js', // isle tt_22
+// 'tidal_wave_22-74016': './src/74016.js', // tidal wave_22
+// 'tidal_wave_public_22-76306': './src/76306.js', // tidal wave_public_22
+// 'tortuga_21-62309': './src/62309.js', // Tortuga 2021
+// 'tortuga_22-74015': './src/74015.js', // tortuga 22
+// 'voodoo_22-70452': './src/70452.js', // Voodoo Festival Attendees 2022
+// 'voodoo_22-70453': './src/70453.js', // Voodoo Festival Production 2022
 
 // 'acl_20-53336': './src/53336.js', // Austin City Limits Festival (ACL) 2020
 // 'akon_21-60284': './src/60284.js', // A-Kon 2021
---
+++ ares/src/86604.js
@@ -0,0 +1,2 @@
+import '../site_configs/fairwell_festival_23-86604/js/86604';
+import '../site_configs/fairwell_festival_23-86604/styles/86604.scss';

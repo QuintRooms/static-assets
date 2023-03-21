ba1a4a880845daafa7269dd295d2b12fc3db9f02
Derek Butvin <drockscott123@gmail.com>
2023-03-21 11:15:04 -0500

    ATX TV 23 - Edit icon for map marker and switch out

--- ares/site_configs/atx_tv_festival_23-88918/js/88918-config.js
+++ ares/site_configs/atx_tv_festival_23-88918/js/88918-config.js
@@ -48,7 +48,7 @@ export default function siteConfig() {
             email_logo_file_location: `${env_path.path}/site_configs/${directory_name}/img/logo.png`,
             logo_outbound_url: 'https://atxfestival.com/',
         },
-        map_marker_image_url: `${env_path.path}/site_configs/${directory_name}/img/favicon.png`,
+        map_marker_image_url: `${env_path.path}/site_configs/${directory_name}/img/map_marker.png`,
         theme: 'light',
         google_font_url: '//fonts.googleapis.com/css?family=Montserrat:100,500,700',
         ads: {

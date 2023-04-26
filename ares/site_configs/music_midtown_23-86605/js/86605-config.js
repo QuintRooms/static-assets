8c4b1e43a97d0d94f0b709b02577d7cec15c3001
Derek Butvin <drockscott123@gmail.com>
2023-04-26 11:43:22 -0600

    Music Midtown 23 - change logo file

--- ares/site_configs/music_midtown_23-86605/js/86605-config.js
+++ ares/site_configs/music_midtown_23-86605/js/86605-config.js
@@ -45,7 +45,7 @@ export default function siteConfig() {
         has_social_sharing: true,
         fav_icon_url: `${env_path.path}/site_configs/${directory_name}/img/favicon.png`,
         header: {
-            logo_file_location: `${env_path.path}/site_configs/${directory_name}/img/logo.svg`,
+            logo_file_location: `${env_path.path}/site_configs/${directory_name}/img/logo.png`,
             email_logo_file_location: `${env_path.path}/site_configs/${directory_name}/img/email-logo.png`,
             logo_outbound_url: 'https://www.musicmidtown.com/',
         },

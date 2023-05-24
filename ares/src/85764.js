5a54570020492b5930e08916a4cbf5d9f13ca17b
Derek Butvin <drockscott123@gmail.com>
2023-05-24 15:27:30 -0600

    LVGP 23 - new files sim to miami 22

--- ares/src/85764.js
+++ ares/src/85764.js
@@ -1,2 +1,2 @@
-import '../site_configs/f1_lasvegas_gp_23-85764/js/85764';
-import '../site_configs/f1_lasvegas_gp_23-85764/styles/85764.scss';
+import '../site_configs/f1_las_vegas2_23-85764/js/85764';
+import '../site_configs/f1_las_vegas2_23-85764/styles/85764.scss';

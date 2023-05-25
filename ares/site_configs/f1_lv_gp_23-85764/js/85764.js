69b5c4dae38ed7925540695d97564f2ffec34b80
Derek Butvin <drockscott123@gmail.com>
2023-05-24 19:21:28 -0600

    LVGP - turn on F1 styles with original event name included

--- ares/site_configs/f1_lv_gp_23-85764/js/85764.js
+++ ares/site_configs/f1_lv_gp_23-85764/js/85764.js
@@ -17,7 +17,7 @@ class ChildPortal extends BasePortal {
     }
 }
 
-// f1Styles(site_config.site_id, site_config.lodging.event_name, 'Las Vegas', '#000');
+f1Styles(site_config.site_id, site_config.lodging.event_name, 'Las Vegas', '#000');
 
 // f1Styles(site_config.site_id);
 

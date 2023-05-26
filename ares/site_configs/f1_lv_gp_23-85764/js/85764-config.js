c5c62ea07671991e92c32cbca1eb44051baf48fd
Derek Butvin <drockscott123@gmail.com>
2023-05-26 11:53:01 -0600

    LVGP 23 - add event_name back to config, test did not work

--- ares/site_configs/f1_lv_gp_23-85764/js/85764-config.js
+++ ares/site_configs/f1_lv_gp_23-85764/js/85764-config.js
@@ -19,6 +19,7 @@ export default function siteConfig() {
         distance_unit: 'useMiles',
         lodging: {
             event_id: '',
+            event_name: 'Las Vegas GP',
             event_dates: '',
             redirect_date: '',
             redirect_url: '',

495922c3eabd75100ed27d70bb5fbed637d84f77
Derek Butvin <drockscott123@gmail.com>
2023-05-30 00:08:17 -0600

    LVGP test 23 - create new files with original build process

--- ares/src/90767.js
+++ ares/src/90767.js
@@ -1,2 +1,2 @@
-import '../site_configs/f1_las_vegas_gp_23-90767/js/90767';
-import '../site_configs/f1_las_vegas_gp_23-90767/styles/90767.scss';
+import '../site_configs/f1_lvgp_23-90767/js/90767';
+import '../site_configs/f1_lvgp_23-90767/styles/90767.scss';

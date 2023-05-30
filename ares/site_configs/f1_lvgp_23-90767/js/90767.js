2cbd560f4849dd70a7f9c85b6878ef53e0bd3248
Derek Butvin <drockscott123@gmail.com>
2023-05-30 00:17:23 -0600

    LVGP test 23 - add footer and policy modal functions to main js

--- ares/site_configs/f1_lvgp_23-90767/js/90767.js
+++ ares/site_configs/f1_lvgp_23-90767/js/90767.js
@@ -1,5 +1,12 @@
 import BasePortal from '../../../js/build';
 import SiteConfig from './90767-config';
+import f1Styles from '../../../clients/formula_1/js/f1';
+import Distance from '../../../js/calculate-distance';
+import Utilities from '../../../js/utilities';
+
+const utilities = new Utilities();
+
+const { domain } = process.env;
 
 const site_config = new SiteConfig();
 class ChildPortal extends BasePortal {
@@ -10,3 +17,29 @@ class ChildPortal extends BasePortal {
 }
 
 new ChildPortal();
+
+async function insertFooterText() {
+    await utilities.waitForSelectorInDOM('.f1-policy');
+    const footer_policy = document.querySelector('.f1-policy');
+
+    footer_policy.insertAdjacentHTML(
+        'beforebegin',
+        `
+        <img style="max-width: 175px; margin-top: 10px;" src="https://dev-static.hotelsforhope.com/ares/clients/formula_1/images/quintrooms-logo-footer.png" alt="Quintrooms Logo" />
+        <p class="f1-partner-text">Authorized Hotel Booking Partner of the F1 Las Vegas Grand Prix</p>
+        `
+    );
+}
+insertFooterText();
+
+async function setListenerForPolicyModal() {
+    await utilities.waitForSelectorInDOM('.span.open-modal');
+
+    const policies_lower = document.querySelector('#policies-fees');
+    policies_lower.addEventListener('click', (e) => {
+        document.querySelector('span.open-modal').click();
+    });
+}
+
+setListenerForPolicyModal();
+

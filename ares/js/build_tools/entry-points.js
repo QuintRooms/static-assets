1b2a8b458a8242bab5db4687c16a493d868a46c2
Derek Butvin <drockscott123@gmail.com>
2024-02-21 12:00:44 -0700

    Portals I-V - update footers

--- ares/js/build_tools/entry-points.js
+++ ares/js/build_tools/entry-points.js
@@ -5,29 +5,29 @@ module.exports = () => {
     if (process.env.NODE_ENV === 'production') {
         entry_points = {
             // 'ares_derek-70011': './src/70011.js', // ares derek
-            'acl_production_24-94608': './src/94608.js', // acl production_24
-            'acl_attendees_24-94605': './src/94605.js', // acl attendees_24
-            'arizona_bike_week_24-90541': './src/90541.js', // arizona bike_week_24
-            'atx_tv_festival_24-95455': './src/95455.js', // atx tv_festival_24
-            'beach_it_24-91129': './src/91129.js', // beach it_24
-            'bj_landing_page-58196': './src/58196.js', // Barrett-Jackson Landing Page
-            'bj_template_21-68719': './src/68719.js', // Barrett-Jackson Template 2021
-            'bonnaroo_24-90957': './src/90957.js', // bonnaroo 24
-            'breeders_24-88325': './src/88325.js', // breeders 24
-            'cfp_landing_page_22-62631': './src/62631.js', // College football Playoff Championship Landing Page
-            'cfp_houston_24-88767': './src/88767.js', // cfp houston_24
-            'cfp_houston_internal_24-92253': './src/92253.js', // cfp houston_internal_24
-            'cfp_houston_media_24-97359': './src/97359.js', // cfp houston_media_24
-            'cfp_atlanta_25-90833': './src/90833.js', // cfp atlanta_25
-            'confidential_template-60276': './src/60276.js', // confidential template
-            'ares_derek-70011': './src/70011.js', // ares derek
-            'fairwell_festival_24-93554': './src/93554.js', // fairwell festival_24
-            'farm_aid_24-98897': './src/98897.js', // farm aid_24
+            // 'acl_production_24-94608': './src/94608.js', // acl production_24
+            // 'acl_attendees_24-94605': './src/94605.js', // acl attendees_24
+            // 'arizona_bike_week_24-90541': './src/90541.js', // arizona bike_week_24
+            // 'atx_tv_festival_24-95455': './src/95455.js', // atx tv_festival_24
+            // 'beach_it_24-91129': './src/91129.js', // beach it_24
+            // 'bj_landing_page-58196': './src/58196.js', // Barrett-Jackson Landing Page
+            // 'bj_template_21-68719': './src/68719.js', // Barrett-Jackson Template 2021
+            // 'bonnaroo_24-90957': './src/90957.js', // bonnaroo 24
+            // 'breeders_24-88325': './src/88325.js', // breeders 24
+            // 'cfp_landing_page_22-62631': './src/62631.js', // College football Playoff Championship Landing Page
+            // 'cfp_houston_24-88767': './src/88767.js', // cfp houston_24
+            // 'cfp_houston_internal_24-92253': './src/92253.js', // cfp houston_internal_24
+            // 'cfp_houston_media_24-97359': './src/97359.js', // cfp houston_media_24
+            // 'cfp_atlanta_25-90833': './src/90833.js', // cfp atlanta_25
+            // 'confidential_template-60276': './src/60276.js', // confidential template
+            // 'ares_derek-70011': './src/70011.js', // ares derek
+            // 'fairwell_festival_24-93554': './src/93554.js', // fairwell festival_24
+            // 'farm_aid_24-98897': './src/98897.js', // farm aid_24
             // 'f1_canada_gp_24-91620': './src/91620.js', // f1 canada_gp_24
             // f1_landing_page: './src/f1_landing_page.js', // Formula 1 landing page (F1 Rooms)
             // 'f1_lvgp_24-91626': './src/91626.js', // f1 lvgp_24
             // 'f1_miami_gp_24-90482': './src/90482.js', // f1 miami_gp_24
-            // 'f1_template-46972': './src/46972.js', // Formula 1 Template
+            // // 'f1_template-46972': './src/46972.js', // Formula 1 Template
             // 'f1e_cug-57182': './src/57182.js', // f1e cug
             // 'f1e_rewards_cug-40598': './src/40598.js', // f1e rewards_cug
             // 'fes_cug-48167': './src/48167.js', // fes cug
@@ -39,67 +39,74 @@ module.exports = () => {
             // 'hook_em_cug-47495': './src/47495.js', // hook em_cug
             // 'hook_em_retail-47496': './src/47496.js', // hook em_retail
             // 'high_water_24-89860': './src/89860.js', // high water_24
-            // 'innings_tempe_24-88139': './src/88139.js', // innings tempe_24
-            // 'innings_tempe_new_id_24-88821': './src/88821.js', // innings tempe_new_id_24
-            // 'lolla_24-90306': './src/90306.js', // lolla 24
-            // 'lolla_prod_24-90312': './src/90312.js', // lolla prod_24
-            // 'oceans_calling_24-96197': './src/96197.js', // oceans calling_24
-            // 'ohana_festival_c3_24-94904': './src/94904.js', // ohana festival_c3_24
-            // 'packers_experiences_retail-70421': './src/70421.js', // packers experiences_retail
-            // 'railbird_production_24-90538': './src/90538.js', // railbird production_24
-            // 'railbird_24-90535': './src/90535.js', // railbird 24
-            // 'resbeat-60831': './src/60831.js', // Resbeat
-            // 'retail_resbeat_public-51474': './src/51474.js', // Retail Resbeat Public
-            // 'resbeat_squad_test-62686': './src/62686.js', // Resbeat Service Squad Test
-            // 'roomcash-63712': './src/63712.js', // RoomCash
+            'innings_tempe_24-88139': './src/88139.js', // innings tempe_24
+            'innings_tempe_new_id_24-88821': './src/88821.js', // innings tempe_new_id_24
+            'lolla_24-90306': './src/90306.js', // lolla 24
+            'lolla_prod_24-90312': './src/90312.js', // lolla prod_24
+            'oceans_calling_24-96197': './src/96197.js', // oceans calling_24
+            'ohana_festival_c3_24-94904': './src/94904.js', // ohana festival_c3_24
+            'packers_experiences_retail-70421': './src/70421.js', // packers experiences_retail
+            'railbird_production_24-90538': './src/90538.js', // railbird production_24
+            'railbird_24-90535': './src/90535.js', // railbird 24
+            'resbeat-60831': './src/60831.js', // Resbeat
+            'retail_resbeat_public-51474': './src/51474.js', // Retail Resbeat Public
+            'resbeat_squad_test-62686': './src/62686.js', // Resbeat Service Squad Test
+            'roomcash-63712': './src/63712.js', // RoomCash
             // 'room_steals-52342': './src/52342.js', // Room Steals
-            // 'sea_hear_now_24-94600': './src/94600.js', // sea hear_now_24
-            // 'shaky_knees_attendees_24-90249': './src/90249.js', // shaky knees_attendees_24
-            // 'sotx_winter_games_24-92088': './src/92088.js', // sotx winter_games_24
-            // 'sotx_summer_games_24-94660': './src/94660.js', // sotx summer_games_24
+            'sea_hear_now_24-94600': './src/94600.js', // sea hear_now_24
+            'shaky_knees_attendees_24-90249': './src/90249.js', // shaky knees_attendees_24
+            'sotx_winter_games_24-92088': './src/92088.js', // sotx winter_games_24
+            'sotx_summer_games_24-94660': './src/94660.js', // sotx summer_games_24
             // 'sportsnet_holidays_cug-00001': './src/00001.js', // sportsnet holidays_cug
-            // 'suenos_24-90956': './src/90956.js', // suenos 24
-            // 'suenos_new_dates_24-93376': './src/93376.js', // suenos new_dates_24
-            // 'tejas_trails_retail-45007': './src/45007.js', // tejas trails_retail
-            // 'texas_book_festival_24-98724': './src/98724.js', // texas book_festival_24
-            // 'ticket_manager-70009': './src/70009.js', // ticket manager
-            // 'tortuga_24_public-88778': './src/88778.js', // tortuga 24_public
-            // 'transportation_short_course_20-56735': './src/56735.js', // Transportation Short Course 2020
-            // 'two_step_inn_festival_24-90132': './src/90132.js', // two step_inn_festival_24
-            // 'van_gogh_dallas-71011': './src/71011.js', // Van Gogh Dallas
-            // 'van_gogh_phoenix-71012': './src/71012.js', // Van Gogh Phoenix
-            // 'van_gogh_los_angeles-71013': './src/71013.js', // Van Gogh Los Angeles
-            // 'van_gogh_landing_page-70958': './src/70958.js', // Van Gogh Los Angeles
+            'suenos_24-90956': './src/90956.js', // suenos 24
+            'suenos_new_dates_24-93376': './src/93376.js', // suenos new_dates_24
+            'tejas_trails_retail-45007': './src/45007.js', // tejas trails_retail
+            'texas_book_festival_24-98724': './src/98724.js', // texas book_festival_24
+            'ticket_manager-70009': './src/70009.js', // ticket manager
+            'tortuga_24_public-88778': './src/88778.js', // tortuga 24_public
+            'transportation_short_course_20-56735': './src/56735.js', // Transportation Short Course 2020
+            'two_step_inn_festival_24-90132': './src/90132.js', // two step_inn_festival_24
+            'van_gogh_dallas-71011': './src/71011.js', // Van Gogh Dallas
+            'van_gogh_phoenix-71012': './src/71012.js', // Van Gogh Phoenix
+            'van_gogh_los_angeles-71013': './src/71013.js', // Van Gogh Los Angeles
+            'van_gogh_landing_page-70958': './src/70958.js', // Van Gogh Los Angeles
             
         };
 
         // develop environment
     } else if (process.env.NODE_ENV === 'develop') {
         entry_points = {
-            // 'ares_child-60278': './src/60278.js', // Ares Child Test
-            // 'ares_child-60279': './src/60279.js', // Ares Child Test
-            // 'cug_template-39624': './src/39624.js', // CUG Template
-            // 'resbeat_points_demo-62725': './src/62725.js', // Resbeat Points Demo
-            // 'resbeat_test-62011': './src/62011.js', // Resbeat Test
-            // 'roomcash_develop-63713': './src/63713.js', // RoomCash Develop
-            'acl_production_24-94608': './src/94608.js', // acl production_24
-            'acl_attendees_24-94605': './src/94605.js', // acl attendees_24
-            'arizona_bike_week_24-90541': './src/90541.js', // arizona bike_week_24
-            'atx_tv_festival_24-95455': './src/95455.js', // atx tv_festival_24
-            'beach_it_24-91129': './src/91129.js', // beach it_24
-            'bj_landing_page-58196': './src/58196.js', // Barrett-Jackson Landing Page
-            'bj_template_21-68719': './src/68719.js', // Barrett-Jackson Template 2021
-            'bonnaroo_24-90957': './src/90957.js', // bonnaroo 24
-            'breeders_24-88325': './src/88325.js', // breeders 24
-            'cfp_landing_page_22-62631': './src/62631.js', // College football Playoff Championship Landing Page
-            'cfp_houston_24-88767': './src/88767.js', // cfp houston_24
-            'cfp_houston_internal_24-92253': './src/92253.js', // cfp houston_internal_24
-            'cfp_houston_media_24-97359': './src/97359.js', // cfp houston_media_24
-            'cfp_atlanta_25-90833': './src/90833.js', // cfp atlanta_25
-            'confidential_template-60276': './src/60276.js', // confidential template
-            'ares_derek-70011': './src/70011.js', // ares derek
-            'fairwell_festival_24-93554': './src/93554.js', // fairwell festival_24
-            'farm_aid_24-98897': './src/98897.js', // farm aid_24
+            'innings_tempe_24-88139': './src/88139.js', // innings tempe_24
+            'innings_tempe_new_id_24-88821': './src/88821.js', // innings tempe_new_id_24
+            'lolla_24-90306': './src/90306.js', // lolla 24
+            'lolla_prod_24-90312': './src/90312.js', // lolla prod_24
+            'oceans_calling_24-96197': './src/96197.js', // oceans calling_24
+            'ohana_festival_c3_24-94904': './src/94904.js', // ohana festival_c3_24
+            'packers_experiences_retail-70421': './src/70421.js', // packers experiences_retail
+            'railbird_production_24-90538': './src/90538.js', // railbird production_24
+            'railbird_24-90535': './src/90535.js', // railbird 24
+            'resbeat-60831': './src/60831.js', // Resbeat
+            'retail_resbeat_public-51474': './src/51474.js', // Retail Resbeat Public
+            'resbeat_squad_test-62686': './src/62686.js', // Resbeat Service Squad Test
+            'roomcash-63712': './src/63712.js', // RoomCash
+            // 'room_steals-52342': './src/52342.js', // Room Steals
+            'sea_hear_now_24-94600': './src/94600.js', // sea hear_now_24
+            'shaky_knees_attendees_24-90249': './src/90249.js', // shaky knees_attendees_24
+            'sotx_winter_games_24-92088': './src/92088.js', // sotx winter_games_24
+            'sotx_summer_games_24-94660': './src/94660.js', // sotx summer_games_24
+            // 'sportsnet_holidays_cug-00001': './src/00001.js', // sportsnet holidays_cug
+            'suenos_24-90956': './src/90956.js', // suenos 24
+            'suenos_new_dates_24-93376': './src/93376.js', // suenos new_dates_24
+            'tejas_trails_retail-45007': './src/45007.js', // tejas trails_retail
+            'texas_book_festival_24-98724': './src/98724.js', // texas book_festival_24
+            'ticket_manager-70009': './src/70009.js', // ticket manager
+            'tortuga_24_public-88778': './src/88778.js', // tortuga 24_public
+            'transportation_short_course_20-56735': './src/56735.js', // Transportation Short Course 2020
+            'two_step_inn_festival_24-90132': './src/90132.js', // two step_inn_festival_24
+            'van_gogh_dallas-71011': './src/71011.js', // Van Gogh Dallas
+            'van_gogh_phoenix-71012': './src/71012.js', // Van Gogh Phoenix
+            'van_gogh_los_angeles-71013': './src/71013.js', // Van Gogh Los Angeles
+            'van_gogh_landing_page-70958': './src/70958.js', // Van Gogh Los Angeles
         };
 
         // local environment

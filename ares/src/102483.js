5f86926de5384a9b16afb1f4b00e31392b471e58
Derek Butvin <drockscott123@gmail.com>
2024-07-09 13:18:20 -0600

    Farm Aid Internal 24 - new build

--- ares/src/102483.js
+++ ares/src/102483.js
@@ -1,2 +1,2 @@
-import '../site_configs/farm_aid_internal_updated_24-102483/js/102483';
-import '../site_configs/farm_aid_internal_updated_24-102483/styles/102483.scss';
+import '../site_configs/farm_aid_prevclone_internal_24-102483/js/102483';
+import '../site_configs/farm_aid_prevclone_internal_24-102483/styles/102483.scss';

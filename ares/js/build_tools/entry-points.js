6bea77e6e5bdb9a3983b2b54c8e1091ee5b01be7
Derek Butvin <drockscott123@gmail.com>
2024-08-05 10:25:48 -0600

    LVGP Marshal Program 24 - Initial Build

--- ares/js/build_tools/entry-points.js
+++ ares/js/build_tools/entry-points.js
@@ -32,7 +32,8 @@ module.exports = () => {
             // f1_landing_page: './src/f1_landing_page.js', // Formula 1 landing page (F1 Rooms)
             // 'f1_lvgp_24-91626': './src/91626.js', // f1 lvgp_24
             // 'f1_lvgp_public_24-102817': './src/102817.js', // f1 lvgp_public_24
-            'f1_lvgp_public_updated_24-102894': './src/102894.js', // f1 lvgp_public_updated_24
+            // 'f1_lvgp_public_updated_24-102894': './src/102894.js', // f1 lvgp_public_updated_24
+            'f1_lvgp_marshal_program_24-103622': './src/103622.js', // f1 lvgp_marshal_program_24
             // 'f1_miami_gp_24-90482': './src/90482.js', // f1 miami_gp_24
             // 'f1_miami_gp_25-101993': './src/101993.js', // f1 miami gp_25
             // 'f1_lvgp_retail-100609': './src/100609.js', // f1 lvgp_retail
@@ -106,7 +107,8 @@ module.exports = () => {
             // 'cfp_landing_page_22-62631': './src/62631.js', // College football Playoff Championship Landing Page
             // 'cfp_retail_22-67665': './src/67665.js', // cfp retail_22
             // 'cfp_atlanta_internal_25-103193': './src/103193.js', // cfp atlanta_internal_25
-            'f1_lvgp_public_updated_24-102894': './src/102894.js', // f1 lvgp_public_updated_24
+            // 'f1_lvgp_public_updated_24-102894': './src/102894.js', // f1 lvgp_public_updated_24
+            'f1_lvgp_marshal_program_24-103622': './src/103622.js', // f1 lvgp_marshal_program_24
 
         };
 
@@ -114,6 +116,7 @@ module.exports = () => {
     } else if (process.env.NODE_ENV === 'local') {
         entry_points = {
             // new sites populate below
+            'f1_lvgp_marshal_program_24-103622': './src/103622.js', // f1 lvgp_marshal_program_24
             // 'cfp_atlanta_internal_25-103193': './src/103193.js', // cfp atlanta_internal_25
             // 'cfp_retail_22-67665': './src/67665.js', // cfp retail_22
             // 'farm_aid_prevclone_internal_24-102483': './src/102483.js', // farm aid_prevclone_internal_24
---
+++ ares/src/103622.js
@@ -0,0 +1,2 @@
+import '../site_configs/f1_lvgp_marshal_program_24-103622/js/103622';
+import '../site_configs/f1_lvgp_marshal_program_24-103622/styles/103622.scss';

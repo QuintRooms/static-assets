35b3b1c3f093fc54b9075ad2c3888dd5e6aa24a2
Derek Butvin <drockscott123@gmail.com>
2024-08-07 14:16:52 -0600

    LVGP 24 Marshal - update checkout room description

--- ares/site_configs/f1_lvgp_marshal_program_24-103622/js/103622.js
+++ ares/site_configs/f1_lvgp_marshal_program_24-103622/js/103622.js
@@ -145,6 +145,23 @@ async function updatePropDetailPricing() {
     }
 }
 
+async function updateCheckoutDescription() {
+    await utilities.waitForSelectorInDOM('#theBody');
+    if (document.querySelector('.CheckOutForm')) {
+        await utilities.waitForSelectorInDOM('#theRateDescription');
+  
+        //Get Room Descriptions
+        const roomDescriptionElement = room.querySelector("#theRateDescription p: nth - child(2)");
+        const roomDescriptionText = roomDescriptionElement.innerText;
+        // const roomDescriptionSeparator = roomDescriptionUpdatedText.indexOf("y");
+        const roomTitleText = roomDescriptionText.substring(0, roomDescriptionText.indexOf("y") + 1) + ":";
+        console.log("roomTitleText = " + roomTitleText);
+        const roomDescriptionUpdatedText = roomDescriptionText.substring(roomDescriptionText.indexOf("y") + 1).replace('Hotel requires a 5-night minimum stay for this reservation.', '').replace('undefined', '');
+        console.log("roomDescriptionUpdatedText = " + roomDescriptionUpdatedText);
+        roomDescriptionElement.innerHTML = `<span style="font-weight: 700; font-size: 17px !important;">${roomTitleText} <span style="font-weight: 300; font-size: 14px !important;">${roomDescriptionUpdatedText}</span></span>`;
+        };
+    }
+
 // async function updatePropDetailDescriptions() {
 //     await utilities.waitForSelectorInDOM('#theBody');
 //     if (document.querySelector('.SinglePropDetail')) {
@@ -161,6 +178,7 @@ async function updatePropDetailPricing() {
 
 updateSearchHotelsPagePricingAndMapMarker();
 updatePropDetailPricing();
+updateCheckoutDescription();
 // updatePropDetailDescriptions();
 hideFirstAmenity();
 insertFooterText();

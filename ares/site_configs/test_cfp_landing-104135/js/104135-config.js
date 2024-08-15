910501b339dadc408ed1dc70e3f75d76542f6b46
Derek Butvin <drockscott123@gmail.com>
2024-08-15 10:50:37 -0600

    Test CFP Landing - Add Retail Link

--- ares/site_configs/test_cfp_landing-104135/js/104135-config.js
+++ ares/site_configs/test_cfp_landing-104135/js/104135-config.js
@@ -102,11 +102,17 @@ export default function siteConfig() {
                 portal_url: 'https://collegefootballplayoffhotels.com/group-event?id=78359',
             },
             {
+                name: 'CFP BOWL GAMES 2025',
+                display_date: 'VARIOUS DATES',
+                end_date: '01/20/2025', // M/D/YYYY
+                portal_url: 'https://collegefootballplayoffhotels.com/group-event?id=80509',
+            },
+            {
                 name: 'CFP NATIONAL CHAMPIONSHIP MIAMI 2026',
                 display_date: 'January 16 - 19, 2026',
                 end_date: '01/19/2026', // M/D/YYYY
                 portal_url: 'https://form.jotform.com/222285562425153',
-            },
+            }
         ],
     };
 }

cce4b6d3298e853ba69731b981f5cd03d4b69189
Derek Butvin <drockscott123@gmail.com>
2024-09-22 15:22:57 -0600

    Test CFP Landing - add back event group headers

--- ares/site_configs/test_cfp_landing-104135/js/104135.js
+++ ares/site_configs/test_cfp_landing-104135/js/104135.js
@@ -67,31 +67,31 @@ async function renderEventGroupHeaders() {
     const lastChampionshipEvent = championshipEventsArray[championshipEventsArray.length - 1];
     const firstChampionshipEvent = championshipEventsArray[0];
 
-    console.log('championshipEventsArray' + championshipEventsArray);
-    console.log('lastChampionshipEvent' + lastChampionshipEvent);
-    console.log('firstChampionshipEvent' + firstChampionshipEvent);
+    console.log('championshipEventsArray ' + championshipEventsArray);
+    console.log('lastChampionshipEvent ' + lastChampionshipEvent);
+    console.log('firstChampionshipEvent ' + firstChampionshipEvent);
 
-    // firstChampionshipEvent.insertAdjacentHTML(
-    //     'beforebegin',
-    //     `
-    //     <div class="event-group-header-container">
-    //         <div class="event-group-header-left-box"></div>
-    //         <div class="event-group-header">NATIONAL CHAMPIONSHIP GAMES</div>
-    //         <div class="event-group-header-right-box"></div>
-    //     </div>
-    //     `
-    // ); 
+    firstChampionshipEvent.insertAdjacentHTML(
+        'beforebegin',
+        `
+        <div class="event-group-header-container">
+            <div class="event-group-header-left-box"></div>
+            <div class="event-group-header">NATIONAL CHAMPIONSHIP GAMES</div>
+            <div class="event-group-header-right-box"></div>
+        </div>
+        `
+    ); 
 
-    // lastChampionshipEvent.insertAdjacentHTML(
-    //     'afterend',
-    //     `
-    //     <div class="event-group-header-container">
-    //         <div class="event-group-header-left-box"></div>
-    //         <div class="event-group-header">PLAYOFF GAMES</div>
-    //         <div class="event-group-header-right-box"></div>
-    //     </div>
-    //     `
-    // ); 
+    lastChampionshipEvent.insertAdjacentHTML(
+        'afterend',
+        `
+        <div class="event-group-header-container">
+            <div class="event-group-header-left-box"></div>
+            <div class="event-group-header">PLAYOFF GAMES</div>
+            <div class="event-group-header-right-box"></div>
+        </div>
+        `
+    ); 
 
 }
 

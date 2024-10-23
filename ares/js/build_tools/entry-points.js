348e798702d04f9fa90c2ea44dff0194fa261a49
Derek Butvin <drockscott123@gmail.com>
2024-10-23 14:20:37 -0600

    CFP Fiesta Bowl 25 - build

--- ares/js/build_tools/entry-points.js
+++ ares/js/build_tools/entry-points.js
@@ -15,13 +15,14 @@ module.exports = () => {
             // 'bonnaroo_24-90957': './src/90957.js', // bonnaroo 24
             // 'breeders_24-88325': './src/88325.js', // breeders 24
             // 'cfp_atlanta_public_25-101560': './src/101560.js', // cfp atlanta_public_25
+            // 'cfp_fiesta_bowl_25-106128': './src/106128.js', // cfp fiesta_bowl_25
             // 'cfp_landing_page_22-62631': './src/62631.js', // College football Playoff Championship Landing Page
             // 'cfp_houston_24-88767': './src/88767.js', // cfp houston_24
             // 'cfp_houston_internal_24-92253': './src/92253.js', // cfp houston_internal_24
             // 'cfp_houston_media_24-97359': './src/97359.js', // cfp houston_media_24
             // 'cfp_atlanta_25-90833': './src/90833.js', // cfp atlanta_25
             // 'cfp_atlanta_internal_25-103193': './src/103193.js', // cfp atlanta_internal_25
-            'cfp_atlanta_media2_25-105604': './src/105604.js', // cfp atlanta_media2_25
+            // 'cfp_atlanta_media2_25-105604': './src/105604.js', // cfp atlanta_media2_25
             // 'cfp_retail_22-67665': './src/67665.js', // cfp retail_22
             // 'cfp_retail_24-103769': './src/103769.js', // cfp retail_24
             // 'confidential_template-60276': './src/60276.js', // confidential template
@@ -38,7 +39,7 @@ module.exports = () => {
             // 'f1_lvgp_public_updated_24-102894': './src/102894.js', // f1 lvgp_public_updated_24
             // 'f1_lvgp_marshal_program_24-103622': './src/103622.js', // f1 lvgp_marshal_program_24
             // 'f1_miami_gp_24-90482': './src/90482.js', // f1 miami_gp_24
-            // 'f1_miami_gp_25-101993': './src/101993.js', // f1 miami gp_25
+            'f1_miami_gp_25-101993': './src/101993.js', // f1 miami gp_25
             // 'f1_lvgp_retail-100609': './src/100609.js', // f1 lvgp_retail
             // // 'f1_template-46972': './src/46972.js', // Formula 1 Template
             // 'f1e_cug-57182': './src/57182.js', // f1e cug
@@ -116,18 +117,21 @@ module.exports = () => {
             // 'f1_lvgp_marshal_program_24-103622': './src/103622.js', // f1 lvgp_marshal_program_24
             // 'cfp_retail_24-103769': './src/103769.js', // cfp retail_24
             // 'test_cfp_landing-104135': './src/104135.js', // test cfp_landing
+            'f1_miami_gp_25-101993': './src/101993.js', // f1 miami gp_25
             // 'farm_aid_24-98897': './src/98897.js', // farm aid_24
             // 'derek_test_2-101088': './src/101088.js', // derek test_2
             // 'sotx_summer_games_25-105438': './src/105438.js', // sotx summer_games_25
-            'cfp_atlanta_media2_25-105604': './src/105604.js', // cfp atlanta_media2_25
-
+            // 'cfp_atlanta_media2_25-105604': './src/105604.js', // cfp atlanta_media2_25
+            // 'tortuga_25-101623': './src/101623.js', // tortuga 25
+            // 'cfp_fiesta_bowl_25-106128': './src/106128.js', // cfp fiesta_bowl_25
         };
 
         // local environment
     } else if (process.env.NODE_ENV === 'local') {
         entry_points = {
             // new sites populate below
-            'cfp_atlanta_media2_25-105604': './src/105604.js', // cfp atlanta_media2_25
+            // 'cfp_fiesta_bowl_25-106128': './src/106128.js', // cfp fiesta_bowl_25
+            // 'cfp_atlanta_media2_25-105604': './src/105604.js', // cfp atlanta_media2_25
             // 'derek_test_2-101088': './src/101088.js', // derek test_2
             // 'test_cfp_landing-104135': './src/104135.js', // test cfp_landing
             // 'cfp_retail_24-103769': './src/103769.js', // cfp retail_24
@@ -140,7 +144,7 @@ module.exports = () => {
             // 'f1_lvgp_public_24-102817': './src/102817.js', // f1 lvgp_public_24
             // 'farm_aid_public_24-102806': './src/102806.js', // farm aid_public_24
             // 'veronica_test_site-102126': './src/102126.js', // veronica test_site
-            // 'f1_miami_gp_25-101993': './src/101993.js', // f1 miami gp_25
+            'f1_miami_gp_25-101993': './src/101993.js', // f1 miami gp_25
             // 'tortuga_25-101623': './src/101623.js', // tortuga 25
             // 'cfp_atlanta_public_25-101560': './src/101560.js', // cfp atlanta_public_25
             // 'sotx_winter_games_25-101018': './src/101018.js', // sotx winter_games_25

d352dd8ee4438a394f1b5c5b4bcbe481bf94862a
Derek Butvin <drockscott123@gmail.com>
2025-09-17 13:13:05 -0500

    BJ WPB 26 public - build

---
+++ ares/site_configs/bj_west_palm_beach_public_26-114319/js/114319-config.js
@@ -0,0 +1,81 @@
+import Path from '../../../js/build_tools/path';
+
+const env_path = new Path();
+const site_id = 114319;
+const directory_name = `bj_west_palm_beach_public_26-${site_id}`;
+
+export default function siteConfig() {
+    return {
+        site_id,
+        directory_name,
+        affiliate_id: 16659,
+        master_id: 920,
+        site_type: 'lodging',
+        algolia_app_id: 'plCZXR0GZ7J1',
+        algolia_api_key: 'b9763a419845b59957b8cc5c9b13440c',
+        currency: 'USD',
+        distance_unit: 'useMiles',
+        map_size: '12',
+        radius: '50',
+        lodging: {
+            event_id: '',
+            event_name: 'Barrett-Jackson Scottsdale 2023',
+            event_dates: '',
+            redirect_date: '',
+            redirect_url: '',
+            is_lrg: false,
+        },
+        cug: {
+            is_cug: false,
+            show_points: false,
+            allow_registration: true,
+            percent_off_text: 'off',
+            show_percent_savings: false,
+        },
+        exclusive_rate_text: 'Exclusive Rates',
+        host_hotel_text: 'Host Hotel',
+        partner_hotel_text: 'Partner Hotel',
+        show_tax_inclusive_rates: false,
+        dayjs_date_format: 'M/D/YYYY',
+        reviews_before_info: true,
+        show_stars: false,
+        show_property_type: false,
+        show_language_select: false,
+        show_currency_select: false,
+        show_date_prompt: false,
+        has_social_sharing: true,
+        fav_icon_url: `${env_path.path}/site_configs/${directory_name}/img/favicon.png`,
+        header: {
+            logo_file_location: `${env_path.path}/site_configs/${directory_name}/img/logo.png`,
+            logo_outbound_url: 'https://www.barrett-jackson.com/',
+        },
+        map_marker_image_url: `${env_path.path}/site_configs/${directory_name}/img/favicon.png`,
+        theme: 'light',
+        google_font_url: '//fonts.googleapis.com/css?family=Montserrat:100,500,700',
+        ads: {
+            sidebar_ad: {
+                is_active: true,
+                image_url: `${env_path.path}/site_configs/${directory_name}/img/left_banner.jpg?v1`,
+                outbound_url: 'https://events.quintrooms.com/v6?type=property&property=587&checkIn=01/25/2023&nights=4&siteid=83130',
+            },
+            between_property_ad: {
+                is_active: false,
+                image_url: '',
+                outbound_url: '',
+            },
+            confirmation_page_bottom: {
+                is_active: false,
+                image_url: '',
+                outbound_url: '',
+            },
+        },
+        // new options
+        has_test_emails: true,
+        root_page_header_text: 'Start Your Search',
+        root_page_subheader_text: 'Reservations at 600K+ Hotels at Unbeatable Rates',
+        is_resbeat_client: false,
+        confirmation_email_from: 'Quint Rooms',
+        has_group_booking_banner: true,
+        group_booking_form_url: 'https://form.jotform.com/203066540331141?bookingPortal=BJAC%20Scottsdale',
+    };
+}
